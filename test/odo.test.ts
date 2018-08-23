721a4f059dd6fdb4f15ab33f961a3065243521ba
Denis Golovin <dgolovin@redhat.com>
2018-08-23 12:08:48 -0700

    Add tests for odo catalog, project and app

--- test/odo.test.ts
+++ test/odo.test.ts
@@ -5,17 +5,13 @@
 
 // The module 'assert' provides assertion methods from node
 import * as odo from '../src/odo';
+import { OpenShiftObject } from '../src/odo';
 import  { Cli, CliExitData, create } from '../src/cli';
 import * as assert from 'assert';
+import { RSA_PKCS1_OAEP_PADDING } from 'constants';
+import { FunctionBreakpoint } from 'vscode';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
-
-suite("Extension Tests", function () {
+suite("odo integration tests", function () {
 
     function create(stdout: string){
         return {
@@ -29,28 +25,95 @@ suite("Extension Tests", function () {
         };
     }
 
-    const componentName = 'httpd';
-    const c: Cli = create(
-`NAME                                    TAGS
-fis-java-openshift                      1.0,2.0
-fis-karaf-openshift                     1.0,2.0
-${componentName}                        2.2,2.3,2.4,latest`);
+    suite("odo catalog integration", function() {
+        const http = 'httpd';    
+        const nodejs = 'nodejs';    
+        const python = 'python';    
+    
+        const odoCatalogCli: Cli = create([
+            `NAME                           TAGS`,
+            `${nodejs}                      1.0`,
+            `${python}                      1.0,2.0`,
+            `${http}                        2.2,2.3,latest`
+        ].join('\n'));
+        let result: string[];
 
-    suite("odo getComponentTypes()", function() {
-        test("returns correct number of component types", function() {
-            const o: odo.Odo = odo.create(c);
-            return o.getComponentTypes().then((result)=> {
-                assert(result.length === 3, 'getComponentTypes() returned wrong number of component types');
+        suiteSetup(async function() {
+            result = await odo.create(odoCatalogCli).getComponentTypes();
+        });
+
+        test("Odo->getComponentTypes() returns correct number of component types", function() {
+            assert(result.length === 3);
+        });
+
+        test("Odo->getComponentTypes() returns correct component type names", function() {
+            const resultArray = result.filter(function(element:string) {
+                return element === http || element === nodejs || element === python;
             });
+            assert(resultArray.length === 3);
+        });
+
+        test("Odo->getComponentTypeVersions() returns correct number of tags for component type", function() {
+            return Promise.all([
+                odo.create(odoCatalogCli).getComponentTypeVersions(nodejs).then((result)=> {
+                    assert(result.length === 1);
+                }),
+                odo.create(odoCatalogCli).getComponentTypeVersions(python).then((result)=> {
+                    assert(result.length === 2);
+                }),
+                odo.create(odoCatalogCli).getComponentTypeVersions(http).then((result)=> {
+                    assert(result.length === 3);
+                })
+            ]);
         });
     });
 
-    suite("odo getComponentTypeVersions()", function() {
-        test("returns correct number of component types", function() {
-            const o: odo.Odo = odo.create(c);
-            return o.getComponentTypeVersions(componentName).then((result)=> {
-                assert(result.length === 4, 'getComponentTypes() returned wrong number of component types');
-            });
+    suite("odo app integration", function() {
+        const app1 = 'app1';    
+        const app2 = 'app2';  
+        const app3 = 'app3';    
+    
+        const odoAppCli: Cli = create([
+            `ACTIVE  NAME`,
+            `  *     ${app1}`,
+            `        ${app2}`,
+            `        ${app3}`
+        ].join('\n'));
+        
+        let result: OpenShiftObject[];
+        const osProj = <OpenShiftObject> {
+            getName: () => "project1"
+        };
+
+        suiteSetup(async function() {
+            result = await odo.create(odoAppCli).getApplications(osProj);
+        });
+
+        test("Odo->getApplications(project) returns correct number of applications", function() {
+            assert(result.length === 3);
+        });
+    });
+
+    suite("odo project integration", function() {
+        const proj1 = 'proj1';    
+        const proj2 = 'proj2';  
+        const proj3 = 'proj3';    
+    
+        const odoProjCli: Cli = create([
+            `ACTIVE  NAME`,
+            `  *     ${proj1}`,
+            `        ${proj2}`,
+            `        ${proj3}`
+        ].join('\n'));
+        
+        let result: OpenShiftObject[];
+
+        suiteSetup(async function() {
+            result = await odo.create(odoProjCli).getProjects();
+        });
+
+        test("Odo->getProjects() returns correct number of projects", function() {
+            assert(result.length === 3);
         });
     });
 });
