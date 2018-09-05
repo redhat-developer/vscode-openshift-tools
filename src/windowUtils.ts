ef77a40195eedbc00230311fe92e7917ba8cc83a
Denis Golovin <dgolovin@redhat.com>
2018-09-04 23:12:32 -0700

    Execute command in terminal works only om macOS/Linux
    Fix #33.

--- src/windowUtils.ts
+++ src/windowUtils.ts
@@ -1,5 +1,5 @@
 import { window, Terminal } from 'vscode';
  
 export function createTerminal(name: string, cwd: string, env: NodeJS.ProcessEnv = process.env ): Terminal {
-    return window.createTerminal(name, "/bin/bash");
+    return window.createTerminal(name, process.platform === 'win32'? undefined : '/bin/bash');
 }
