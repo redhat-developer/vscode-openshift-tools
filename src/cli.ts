1ca68f3a0b279d710d5edac08d127ecc7a679d67
Denis Golovin <dgolovin@gmail.com>
2018-09-10 07:26:08 -0700

    [WIP] Download odo

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,9 @@
 import * as childProcess from 'child_process';
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as download from './download';
+import * as fs from 'fs-extra';
+
 
 export interface CliExitData {
     readonly error: Error;
@@ -9,10 +12,31 @@ export interface CliExitData {
 }
 
 class Cli implements ICli {
-    execute(cmd: string, opts: any = {}): Promise<CliExitData> {
+
+    async execute(cmd: string, opts: any = {}): Promise<CliExitData> {
+
+        const installed = await isToolInstalled(cmd.split(' ')[1]);
+        let finalCommand = cmd;
+        
+        if(!installed) {
+            const toFile = path.resolve('c:\\Users\\Eskimo\\.vs-openshift\\odo.exe');
+            await fs.ensureDir(path.dirname(toFile));
+            download.downloadFile(
+                "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-windows-amd64.exe", 
+                toFile, 
+                function(error){
+                    console.log(error);
+                }, 
+                function(progress){
+                    console.log(progress);
+                }
+            );
+            finalCommand = cmd.replace("odo", toFile);
+        }
+
         return new Promise<CliExitData>((resolve, reject) => {
-            odoChannel.print(cmd);
-            childProcess.exec(cmd, opts, (error: Error, stdout: string, stderr: string) => {
+            odoChannel.print(finalCommand);
+            childProcess.exec(finalCommand, opts, (error: Error, stdout: string, stderr: string) => {
                 odoChannel.print(stdout);
                 odoChannel.print(stderr);
                 resolve({error, stdout, stderr});
@@ -43,6 +67,11 @@ class OdoChannelImpl implements OdoChannel {
         }
         this.channel.show();
     }
+}
+
+
+ async function isToolInstalled(cmdName: string) {
+    return false;
 }
 
 export const odoChannel = new OdoChannelImpl();
