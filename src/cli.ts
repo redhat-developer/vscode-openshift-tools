b8f36efe265481cb1dc7d3e077ceaec8984671f5
Denis Golovin <dgolovin@gmail.com>
2018-09-10 23:02:13 -0700

    Add multiplatfrom download support for used cli tools

--- src/cli.ts
+++ src/cli.ts
@@ -3,10 +3,9 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as download from './download';
 import * as fsex from 'fs-extra';
-import { platform } from 'os';
 import { which } from 'shelljs';
 import * as fs from 'fs';
-
+import { Platform } from './platform';
 
 export interface CliExitData {
     readonly error: Error;
@@ -14,19 +13,66 @@ export interface CliExitData {
     readonly stderr: string;
 }
 
-class Cli implements ICli {
+const toolsConfig = {
+    "odo": {
+        "description": "OpenShift Do CLI client",
+        "vendor": "Red Hat, Inc.",
+        "name": "odo",
+        "version": "0.0.10",
+        "fileName": "odo",
+        "platform": {
+            "win32": {
+                "url": "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-windows-amd64.exe",
+                "sha256sum": "",
+                "fileName": "odo.exe"
+            },
+            "darwin": {
+                "url": "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-darwin-amd64",
+                "sha256sum": ""
+            },
+            "linux": {
+                "url": "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-linux-amd64",
+                "sha256sum": ""
+            }
+        }
+    },
+    "oc": {
+        "description": "OpenShift CLI Client",
+        "vendor": "Red Hat, Inc.",
+        "name": "oc",
+        "fileName": "oc",
+        "version": "0.0.10",
+        "platform": {
+            "win32": {
+                "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-windows.zip",
+                "sha256sum": "",
+                "fileName": "oc.exe"
+            },
+            "darwin": {
+                "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-mac.zip",
+                "sha256sum": ""
+            },
+            "linux": {
+                "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz",
+                "sha256sum": ""
+            }
+        }
+    }
+};
 
+const tools = loadMetadata(toolsConfig, process.platform);
+
+class Cli implements ICli {
     async execute(cmd: string, opts: any = {}): Promise<CliExitData> {
         const cmdName = cmd.split(' ')[0];
         const odoLocation = await getToolLocation(cmdName);
-        let finalCommand = cmd.replace("odo", odoLocation);
-
+        let finalCommand = cmd.replace(cmdName, odoLocation);
         return new Promise<CliExitData>((resolve, reject) => {
             odoChannel.print(finalCommand);
             childProcess.exec(finalCommand, opts, (error: Error, stdout: string, stderr: string) => {
                 odoChannel.print(stdout);
                 odoChannel.print(stderr);
-                resolve({error, stdout, stderr});
+                resolve({ error, stdout, stderr });
             });
         });
     }
@@ -46,43 +92,51 @@ export interface OdoChannel {
 
 class OdoChannelImpl implements OdoChannel {
     private readonly channel: vscode.OutputChannel = vscode.window.createOutputChannel("OpenShift Do");
-
     print(text: string) {
         this.channel.append(text);
-        if(text.charAt(text.length-1) !== '\n') {
+        if (text.charAt(text.length - 1) !== '\n') {
             this.channel.append('\n');
         }
         this.channel.show();
     }
 }
 
-
 async function getToolLocation(cmd): Promise<string> {
-    
-    let toolLocation = getDownloadedLocation(cmd);
+    let toolLocation = path.resolve(Platform.getUserHomePath(), '.vs-openshift', tools[cmd].fileName);
     try {
         fs.accessSync(toolLocation);
-    } catch(error) {
+    } catch (error) {
         let pathTool = which(cmd);
-        if(pathTool === null) {
+        if (pathTool === null) {
+            odoChannel.print(`Downloading '${cmd}' CLI tool`)
             await fsex.ensureDir(path.dirname(toolLocation));
             await download.downloadFile(
-                "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-windows-amd64.exe", 
-                toolLocation, 
-                function(progress){
-                    console.log(progress + '%');
+                tools[cmd].url,
+                toolLocation,
+                function (progress) {
+                    odoChannel.print(progress + '%');
                 }
             );
         } else {
             toolLocation = cmd;
         }
-        
     }
     return toolLocation;
 }
 
-function getDownloadedLocation(cmd) {
-    return path.resolve('c:\\Users\\Eskimo\\.vs-openshift\\odo' + (platform()==='win32'?'.exe':'') );
+function loadMetadata(requirements, platform) {
+    let reqs = JSON.parse(JSON.stringify(requirements));
+    for (var object in requirements) {
+        if (reqs[object].platform) {
+            if (reqs[object].platform[platform]) {
+                Object.assign(reqs[object], reqs[object].platform[platform]);
+                delete reqs[object].platform;
+            } else {
+                delete reqs[object];
+            }
+        }
+    }
+    return reqs;
 }
 
 export const odoChannel = new OdoChannelImpl();
