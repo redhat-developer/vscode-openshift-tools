0a8b3575f8a5976bce95318bcf217f1ad91bae35
Denis Golovin <dgolovin@gmail.com>
2018-09-11 01:28:14 -0700

    Support for downloading zipped cli tools

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,9 @@ import * as fsex from 'fs-extra';
 import { which } from 'shelljs';
 import * as fs from 'fs';
 import { Platform } from './platform';
+import targz = require('targz');
+import unzipm = require('unzip-stream');
+import mkdirp = require('mkdirp');
 
 export interface CliExitData {
     readonly error: Error;
@@ -19,12 +22,15 @@ const toolsConfig = {
         "vendor": "Red Hat, Inc.",
         "name": "odo",
         "version": "0.0.10",
-        "fileName": "odo",
+        "dlFileName": "odo",
+        "cmdFileName": "odo",
+        "filePrefix": "",
         "platform": {
             "win32": {
                 "url": "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-windows-amd64.exe",
                 "sha256sum": "",
-                "fileName": "odo.exe"
+                "dlFileName": "odo.exe",
+                "cmdFileName": "odo.exe"
             },
             "darwin": {
                 "url": "https://github.com/redhat-developer/odo/releases/download/v0.0.10/odo-darwin-amd64",
@@ -42,19 +48,26 @@ const toolsConfig = {
         "name": "oc",
         "fileName": "oc",
         "version": "0.0.10",
+        "filePrefix": "",
         "platform": {
             "win32": {
                 "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-windows.zip",
                 "sha256sum": "",
-                "fileName": "oc.exe"
+                "dlFileName": "oc.zip",
+                "cmdFileName": "oc.exe"
             },
             "darwin": {
                 "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-mac.zip",
-                "sha256sum": ""
+                "sha256sum": "",
+                "dlFileName": "oc.zip",
+                "cmdFileName": "oc"
             },
             "linux": {
                 "url": "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz",
-                "sha256sum": ""
+                "sha256sum": "",
+                "fileName": "oc.tar.gz",
+                "dlFileName": "oc.zip",
+                "cmdFileName": "oc"
             }
         }
     }
@@ -102,21 +115,23 @@ class OdoChannelImpl implements OdoChannel {
 }
 
 async function getToolLocation(cmd): Promise<string> {
-    let toolLocation = path.resolve(Platform.getUserHomePath(), '.vs-openshift', tools[cmd].fileName);
+    let toolLocation = path.resolve(Platform.getUserHomePath(), '.vs-openshift', tools[cmd].cmdFileName);
+    let toolDlLocation = path.resolve(Platform.getUserHomePath(), '.vs-openshift', tools[cmd].dlFileName);
     try {
         fs.accessSync(toolLocation);
     } catch (error) {
         let pathTool = which(cmd);
         if (pathTool === null) {
-            odoChannel.print(`Downloading '${cmd}' CLI tool`)
+            odoChannel.print(`Downloading '${cmd}' CLI tool`);
             await fsex.ensureDir(path.dirname(toolLocation));
             await download.downloadFile(
                 tools[cmd].url,
-                toolLocation,
+                toolDlLocation,
                 function (progress) {
                     odoChannel.print(progress + '%');
                 }
             );
+            await unzip(toolDlLocation, path.resolve(Platform.getUserHomePath(), '.vs-openshift'), tools[cmd].prefix);
         } else {
             toolLocation = cmd;
         }
@@ -138,5 +153,39 @@ function loadMetadata(requirements, platform) {
     }
     return reqs;
 }
+
+function unzip(zipFile, extractTo, prefix): Promise<any> {
+    return new Promise((resolve, reject) => {
+
+      if(zipFile.endsWith('.tar.gz')) {
+        targz.decompress({
+          src: zipFile,
+          dest: extractTo,
+          tar: {
+            map: function(header) {
+              if (prefix && header.name.startsWith(prefix)) {
+                header.name = header.name.substring(prefix.length);
+              }
+              return header;
+            }
+          }
+        }, (err)=> {
+          if(err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        });
+      } else if(zipFile.endsWith('.zip')) {
+        fs.createReadStream(zipFile).pipe(unzipm.Extract({ path: extractTo })).on('error', (error) => {
+            reject(error);
+          }).on('close', () => {
+            resolve();
+          });
+      } else {
+        reject(`unsupported extension for ${zipFile}`);
+      }
+    });
+  }
 
 export const odoChannel = new OdoChannelImpl();
