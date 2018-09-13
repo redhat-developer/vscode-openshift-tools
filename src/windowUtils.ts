2a1a59b39f24a427afbee84b915118b8d4ab503f
Jan Richter <jrichter@redhat.com>
2018-09-13 19:23:34 +0200

    Make terminal calls work with tools not on path (#58)

--- src/windowUtils.ts
+++ src/windowUtils.ts
@@ -1,5 +1,20 @@
-import { window, Terminal } from 'vscode';
+import { window, Terminal, TerminalOptions } from 'vscode';
+import * as path from 'path';
+import { Platform } from './platform';
+
+const toolsLocation = path.resolve(Platform.getUserHomePath(), '.vs-openshift');
  
-export function createTerminal(name: string, cwd: string, env: NodeJS.ProcessEnv = process.env ): Terminal {
-    return window.createTerminal(name, process.platform === 'win32'? undefined : '/bin/bash');
+export function createTerminal(name: string, cwd: string, env: NodeJS.ProcessEnv = process.env): Terminal {
+    let finalEnv: NodeJS.ProcessEnv = {};
+    Object.assign(finalEnv, env);
+    const key = process.platform === 'win32' ? 'Path' : 'PATH';
+    finalEnv[key] = `${toolsLocation}${path.delimiter}${env[key]}`;
+
+    const options: TerminalOptions = {
+        cwd: cwd,
+        name: name,
+        env: finalEnv,
+        shellPath: process.platform === 'win32'? undefined : '/bin/bash'
+    };
+    return window.createTerminal(options);
 }
