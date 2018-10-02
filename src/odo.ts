c65ad00b42e26c0e00c3003e01c3ffc1f057104c
Denis Golovin <dgolovin@redhat.com>
2018-10-01 22:04:52 -0700

    Remove oc api-versions call becuase it works only with oc 9.10+

--- src/odo.ts
+++ src/odo.ts
@@ -208,12 +208,13 @@ class OdoImpl implements Odo {
         const projName: string = application.getParent().getName();
         const apis: cliInstance.CliExitData = await this.cli.execute(`oc api-versions`, {});
         let services: OpenShiftObject[] = [];
-        if(apis.stdout.indexOf('servicecatalog.k8s.io')>-1) {
+        try {
             const result: cliInstance.CliExitData = await this.cli.execute(
                 `oc get ServiceInstance -o jsonpath="{range .items[?(.metadata.labels.app == '${appName}')]}{.metadata.labels.app\\.kubernetes\\.io/component-name}{'\\n'}{end}" --namespace ${projName}`, {}
             );
-            
             services = result.stdout.trim().split('\n').filter((value)=>value!=='').map((value) => new OpenShiftObjectImpl(application, value, 'service', this, TreeItemCollapsibleState.None));
+        } catch (e) {
+            // ignore error in case service catalog is not configured
         }
         return services;
     }
