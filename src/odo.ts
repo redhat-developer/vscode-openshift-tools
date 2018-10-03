963590d30ecbed9b4e7455788fc1ad68f23c45cb
Denis Golovin <dgolovin@redhat.com>
2018-10-02 21:55:36 -0700

    Add copyright notice header in *.ts files

--- src/odo.ts
+++ src/odo.ts
@@ -1,3 +1,8 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
 import * as cliInstance from './cli';
 import { TreeItem, ProviderResult, TreeItemCollapsibleState, OutputChannel, window, Terminal, Uri, commands } from 'vscode';
 import * as windowUtils from './windowUtils';
--- test/odo.test.ts
+++ test/odo.test.ts
@@ -1,3 +1,8 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
