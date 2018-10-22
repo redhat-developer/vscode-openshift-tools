a7e88f66dfd426fe686f3997dac91b75373af979
sudhirverma <sudhirvvermaa@gmail.com>
2018-10-22 23:27:37 +0530

    Unit test for extension.ts file

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -8,20 +8,91 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import * as vscode from 'vscode';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import { activate } from '../src/extension';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+const expect = chai.expect;
+chai.use(sinonChai);
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite('openshift connector Extension', () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    let sandbox: sinon.SinonSandbox;
+
+    class DummyMemento implements vscode.Memento {
+        get<T>(key: string): Promise<T|undefined> {
+          return Promise.resolve(undefined);
+        }
+
+        update(key: string, value: any): Promise<void> {
+          return Promise.resolve();
+        }
+    }
+
+    const context: vscode.ExtensionContext = {
+        extensionPath: 'path',
+        storagePath: 'string',
+        subscriptions: [],
+        workspaceState: new DummyMemento(),
+        globalState: new DummyMemento(),
+        asAbsolutePath(relativePath: string): string {
+            return '';
+          }
+    };
+
+    setup(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    test('Extension should be present', () => {
+		assert.ok(vscode.extensions.getExtension('redhat.vscode-openshift-connector'));
+	});
+
+    test('should activate extension', async () => {
+        const registerTreeDataProviderStub = sandbox.stub(vscode.window, 'registerTreeDataProvider');
+        await activate(context);
+        expect(registerTreeDataProviderStub).calledOnce;
+	});
+    test('should register all server commands', async () => {
+        return await vscode.commands.getCommands(true).then((commands) => {
+            const SERVER_COMMANDS = [
+                'openshift.about',
+                'openshift.explorer.login',
+                'openshift.explorer.logout',
+                'openshift.explorer.refresh',
+                'openshift.catalog.list.components',
+                'openshift.catalog.list.services',
+                'openshift.project.create',
+                'openshift.project.delete',
+                'openshift.app.describe',
+                'openshift.app.create',
+                'openshift.app.delete',
+                'openshift.component.describe',
+                'openshift.component.create',
+                'openshift.component.push',
+                'openshift.component.watch',
+                'openshift.component.log',
+                'openshift.component.followLog',
+                'openshift.component.openUrl',
+                'openshift.component.openshiftConsole',
+                'openshift.component.delete',
+                'openshift.storage.create',
+                'openshift.storage.delete',
+                'openshift.url.create',
+                'openshift.service.create',
+                'openshift.service.delete'
+            ];
+            const foundServerCommands = commands.filter((value) => {
+                return SERVER_COMMANDS.indexOf(value) >= 0 || value.startsWith('openshift.');
+            });
+            assert.equal(foundServerCommands.length , SERVER_COMMANDS.length, 'Some openshift commands are not registered properly or a new command is not added to the test');
+        });
     });
 });
