35aae0f5c261d56f23cb8d44a44cdee1733d3d5a
Jan Richter <jrichter@redhat.com>
2018-10-31 13:36:54 +0100

    Cleanup for cli

---
+++ src/toolsConfig.ts
@@ -0,0 +1,57 @@
+export const toolsConfig = {
+    odo: {
+        description: "OpenShift Do CLI tool",
+        vendor: "Red Hat, Inc.",
+        name: "odo",
+        version: "0.0.14",
+        dlFileName: "odo",
+        cmdFileName: "odo",
+        filePrefix: "",
+        platform: {
+            win32: {
+                url: "https://github.com/redhat-developer/odo/releases/download/v0.0.14/odo-windows-amd64.exe.gz",
+                sha256sum: "ccd8a929e6585a48f79d144ec871b6e5684252fcfa91f67e6e5e8ebbac91f7d7",
+                dlFileName: "odo-windows-amd64.exe.gz",
+                cmdFileName: "odo.exe"
+            },
+            darwin: {
+                url: "https://github.com/redhat-developer/odo/releases/download/v0.0.14/odo-darwin-amd64.gz",
+                sha256sum: "43812cc02f42819db6f00b325374d6e8992e2484bb0e805a32146bbd169842ab",
+                dlFileName: "odo-darwin-amd64.gz"
+            },
+            linux: {
+                url: "https://github.com/redhat-developer/odo/releases/download/v0.0.14/odo-linux-amd64.gz",
+                sha256sum: "743c066fb772eb86231a78abd494618c802180fd9666b191e1ccd54fa1ee80ac",
+                dlFileName: "odo-linux-amd64.gz"
+            }
+        }
+    },
+    oc: {
+        description: "OKD CLI client tool",
+        vendor: "Red Hat, Inc.",
+        name: "oc",
+        cmdFileName: "oc",
+        version: "0.0.10",
+        filePrefix: "",
+        platform: {
+            win32: {
+                url: "https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-windows.zip",
+                sha256sum: "705eb110587fdbd244fbb0f93146a643b24295cfe2410ff9fe67a0e880912663",
+                dlFileName: "oc.zip",
+                cmdFileName: "oc.exe"
+            },
+            darwin: {
+                url: "https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-mac.zip",
+                sha256sum: "32bdd9464866c8e93d8cf4a3a7718b0bc9fa0f2881f045b97997fa014b52a40b",
+                dlFileName: "oc.zip",
+            },
+            linux: {
+                url: "https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz",
+                sha256sum: "6ed2fb1579b14b4557e4450a807c97cd1b68a6c727cd1e12deedc5512907222e",
+                fileName: "oc.tar.gz",
+                dlFileName: "oc.tar.gz",
+                filePrefix: "openshift-origin-client-tools-v3.9.0-191fece-linux-64bit"
+            }
+        }
+    }
+};
