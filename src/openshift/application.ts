fd04634d5bc75736a74a1ba8509e58c2ebc01efe
Jan Richter <jrichter@redhat.com>
2018-11-02 10:09:33 +0100

    Unit tests for OS/Application

--- src/openshift/application.ts
+++ src/openshift/application.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 import * as validator from 'validator';
 
 export class Application extends OpenShiftItem {
-    static async create(context: OpenShiftObject): Promise<String> {
+    static async create(project: OpenShiftObject): Promise<String> {
         const applicationName = await vscode.window.showInputBox({
             prompt: "Application name",
             validateInput: (value: string) => {
@@ -15,28 +15,28 @@ export class Application extends OpenShiftItem {
         });
         if (!applicationName) return Promise.resolve(null);
         return Promise.resolve()
-            .then(() => Application.odo.execute(`odo project set ${context.getName()} && odo app create ${applicationName.trim()}`))
+            .then(() => Application.odo.execute(`odo project set ${project.getName()} && odo app create ${applicationName.trim()}`))
             .then(() => {
-                Application.explorer.refresh(context);
+                Application.explorer.refresh(project);
                 return `Application '${applicationName}' successfully created`;
             })
             .catch((error) => { return Promise.reject(`Failed to create application with error '${error}'`); });
     }
 
-    static describe(context: OpenShiftObject): void {
-        const project: OpenShiftObject = context.getParent();
-        Application.odo.executeInTerminal(`odo project set ${project.getName()}; odo app describe ${context.getName()}`, process.cwd());
+    static describe(treeItem: OpenShiftObject): void {
+        const project: OpenShiftObject = treeItem.getParent();
+        Application.odo.executeInTerminal(`odo project set ${project.getName()}; odo app describe ${treeItem.getName()}`, process.cwd());
     }
 
-    static async del(context: OpenShiftObject): Promise<string> {
-        const project: OpenShiftObject = context.getParent();
-        const value = await vscode.window.showWarningMessage(`Are you sure you want to delete application '${context.getName()}?`, 'Yes', 'Cancel');
+    static async del(treeItem: OpenShiftObject): Promise<string> {
+        const project: OpenShiftObject = treeItem.getParent();
+        const value = await vscode.window.showWarningMessage(`Are you sure you want to delete application '${treeItem.getName()}?`, 'Yes', 'Cancel');
         if (value === 'Yes') {
             return Promise.resolve()
-            .then(() => Application.odo.execute(`odo project set ${project.getName()} && odo app delete ${context.getName()} -f`))
+            .then(() => Application.odo.execute(`odo project set ${project.getName()} && odo app delete ${treeItem.getName()} -f`))
             .then(() => {
-                Application.explorer.refresh(context.getParent());
-                return `Application '${context.getName()}' successfully deleted`;
+                Application.explorer.refresh(treeItem.getParent());
+                return `Application '${treeItem.getName()}' successfully deleted`;
             })
             .catch((err)=> { return Promise.reject(`Failed to delete application with error '${err}'`); });
         }
---
+++ test/openshift/application.test.ts
@@ -0,0 +1,134 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import { OdoImpl } from '../../src/odo';
+import { Application } from '../../src/openshift/application';
+import { TestItem } from './testOSItem';
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+suite('Openshift/Application', () => {
+    let sandbox: sinon.SinonSandbox;
+    let execStub: sinon.SinonStub;
+    const projectItem = new TestItem(null, 'project');
+    const appItem = new TestItem(projectItem, 'app');
+
+    setup(() => {
+        sandbox = sinon.createSandbox();
+        execStub = sandbox.stub(OdoImpl.prototype, 'execute');
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    suite('create', () => {
+        let inputStub: sinon.SinonStub;
+
+        setup(() => {
+            inputStub = sandbox.stub(vscode.window, 'showInputBox');
+        });
+
+        test('calls the appropriate odo command', async () => {
+            inputStub.resolves('name');
+            execStub.resolves();
+
+            await Application.create(projectItem);
+
+            expect(execStub).calledOnceWith(`odo project set ${projectItem.getName()} && odo app create name`);
+        });
+
+        test('returns status when successful', async () => {
+            inputStub.resolves('name1');
+            execStub.resolves();
+
+            const result = await Application.create(projectItem);
+
+            expect(result).equals(`Application 'name1' successfully created`);
+        });
+
+        test('returns null with no input', async() => {
+            inputStub.resolves();
+
+            const result = await Application.create(projectItem);
+
+            expect(result).null;
+        });
+
+        test('wraps error messages with additional info', async () => {
+            inputStub.resolves('name1');
+            execStub.rejects('ERROR');
+
+            try {
+                await Application.create(projectItem);
+                expect.fail();
+            } catch (err) {
+                expect(err).equals(`Failed to create application with error 'ERROR'`);
+            }
+        });
+    });
+
+    suite('describe', () => {
+        let termStub: sinon.SinonStub;
+
+        setup(() => {
+            termStub = sandbox.stub(OdoImpl.prototype, 'executeInTerminal').resolves();
+        });
+
+        test('calls the appropriate odo command in terminal', () => {
+            Application.describe(appItem);
+
+            expect(termStub).calledOnceWith(`odo project set ${projectItem.getName()}; odo app describe ${appItem.getName()}`, process.cwd());
+        });
+    });
+
+    suite('del', () => {
+        let warnStub: sinon.SinonStub;
+
+        setup(() => {
+            warnStub = sandbox.stub(vscode.window, 'showWarningMessage');
+        });
+
+        test('calls the appropriate odo command if confirmed', async () => {
+            warnStub.resolves('Yes');
+            execStub.resolves();
+
+            await Application.del(appItem);
+
+            expect(execStub).calledOnceWith(`odo project set ${projectItem.getName()} && odo app delete ${appItem.getName()} -f`);
+        });
+
+        test('returns status when successful', async () => {
+            warnStub.resolves('Yes');
+            execStub.resolves();
+
+            const result = await Application.del(appItem);
+
+            expect(result).equals(`Application '${appItem.getName()}' successfully deleted`);
+        });
+
+        test('returns null when cancelled', async() => {
+            warnStub.resolves('Cancel');
+
+            const result = await Application.del(appItem);
+
+            expect(result).null;
+        });
+
+        test('wraps error messages with additional info', async () => {
+            warnStub.resolves('Yes');
+            execStub.rejects('ERROR');
+
+            try {
+                await Application.del(appItem);
+                expect.fail();
+            } catch (err) {
+                expect(err).equals(`Failed to delete application with error 'ERROR'`);
+            }
+        });
+    });
+});
