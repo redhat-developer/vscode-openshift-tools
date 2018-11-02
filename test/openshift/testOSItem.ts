32a56971333177c5f4f8f839fc9cf6a92ffe165e
Jan Richter <jrichter@redhat.com>
2018-11-02 10:09:33 +0100

    Unit tests for OS/Application

---
+++ test/openshift/testOSItem.ts
@@ -0,0 +1,24 @@
+import { OpenShiftObject } from "../../src/odo";
+
+export class TestItem implements OpenShiftObject {
+    constructor(
+        private parent: OpenShiftObject,
+        private name) {
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getTreeItem() {
+        return null;
+    }
+
+    getChildren() {
+        return [];
+    }
+
+    getParent(): OpenShiftObject {
+        return this.parent;
+    }
+}
