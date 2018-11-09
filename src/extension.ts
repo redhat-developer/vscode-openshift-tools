39c1a5271e97573ddf7570a3bf844bb46eda16b6
sudhirverma <sudhirvvermaa@gmail.com>
2018-11-09 18:26:01 +0530

    'OpenShift: Delete Service' command to command palette

--- src/extension.ts
+++ src/extension.ts
@@ -47,6 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.commands.registerCommand('openshift.url.create', (context) => execute(Url.create, context)),
         vscode.commands.registerCommand('openshift.service.create', (context) => execute(Service.create, context)),
         vscode.commands.registerCommand('openshift.service.delete', (context) => execute(Service.del, context)),
+        vscode.commands.registerCommand('openshift.service.delete.palette', (context) => execute(Service.del, context)),
         vscode.window.registerTreeDataProvider('openshiftProjectExplorer', explorer),
         explorer
     ];
