363cd7178356a8f6b232a3bb8f6228cf9015fcf7
Denis Golovin <dgolovin@redhat.com>
2018-11-09 10:00:05 -0800

    Fix service commands errors

--- src/openshift/service.ts
+++ src/openshift/service.ts
@@ -67,7 +67,7 @@ export class Service extends OpenShiftItem {
         if (service) {
             const answer = await vscode.window.showWarningMessage(`Are you sure you want to delete service '${service.getName()}'`, 'Yes', 'Cancel');
             if (answer === 'Yes') {
-                return Service.odo.execute(`odo project set ${service.getParent().getParent().getName()} && odo app set ${service.getParent().getName()} && odo service delete ${service.getName()} -f`)
+                return Service.odo.execute(`odo service delete ${service.getName()} -f --project ${project.getName()} --app ${application.getName()}`)
                     .then(() => Service.explorer.refresh(treeItem ? treeItem.getParent() : undefined))
                     .then(() => `Service '${service.getName()} successfully deleted'`)
                     .catch((err) => Promise.reject(`Failed to delete service with error '${err}'`));
