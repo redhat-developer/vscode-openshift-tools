3ff5d528ad4be10092d171c1e11fddf2dfd6f706
sudhirverma <sudhirvvermaa@gmail.com>
2018-11-09 18:26:01 +0530

    'OpenShift: Delete Service' command to command palette

--- src/openshift/service.ts
+++ src/openshift/service.ts
@@ -6,19 +6,17 @@ import { Progress } from '../util/progress';
 export class Service extends OpenShiftItem {
     static async create(context: OpenShiftObject): Promise<string>  {
         try {
-            const serviceTemplateNames: string[] = await Service.odo.getServiceTemplates();
-            const serviceTemplateName = await vscode.window.showQuickPick(serviceTemplateNames, {
+            const serviceTemplateName = await vscode.window.showQuickPick(Service.odo.getServiceTemplates(), {
                 placeHolder: "Service Template Name"
             });
 
-            if (serviceTemplateName === undefined) return Promise.resolve(null);
+            if (serviceTemplateName === undefined) return null;
 
-            const serviceTemplatePlanNames: string[] = await Service.odo.getServiceTemplatePlans(serviceTemplateName);
-            const serviceTemplatePlanName = await vscode.window.showQuickPick(serviceTemplatePlanNames, {
+            const serviceTemplatePlanName = await vscode.window.showQuickPick(Service.odo.getServiceTemplatePlans(serviceTemplateName), {
                 placeHolder: "Service Template Plan Name"
             });
 
-            if (serviceTemplatePlanName === undefined) return Promise.resolve(null);
+            if (serviceTemplatePlanName === undefined) return null;
 
             const serviceName = await vscode.window.showInputBox({
                 value: serviceTemplateName,
@@ -34,31 +32,40 @@ export class Service extends OpenShiftItem {
             });
             if (serviceName) {
                 return Progress.execWithProgress({
-                    cancellable: false,
-                    location: vscode.ProgressLocation.Notification,
-                    title: `Creating new service '${serviceName}'`
-                }, [{command: `odo project set ${context.getParent().getName()} && odo app set ${context.getName()} && odo service create ${serviceTemplateName} --plan ${serviceTemplatePlanName} ${serviceName.trim()}`, increment: 100}
-                ], Service.odo).then(() => {
-                    Service.explorer.refresh(context);
-                    return `Service '${serviceName}' successfully created`;
-                });
+                        cancellable: false,
+                        location: vscode.ProgressLocation.Notification,
+                        title: `Creating new service '${serviceName}'`
+                    }, [{command: `odo project set ${context.getParent().getName()} && odo app set ${context.getName()} && odo service create ${serviceTemplateName} --plan ${serviceTemplatePlanName} ${serviceName.trim()}`, increment: 100}
+                    ], Service.odo)
+                    .then(() => Service.explorer.refresh(context))
+                    .then(() => `Service '${serviceName}' successfully created`);
             }
-            return Promise.resolve(null);
+            return null;
         } catch (e) {
             return Promise.reject(e.message.replace(/\w/, (c) => c.toUpperCase()));
         }
     }
 
-    static async del(service: OpenShiftObject): Promise<string> {
-        const value = await vscode.window.showWarningMessage(`Are you sure you want to delete service '${service.getName()}'`, 'Yes', 'Cancel');
-        if (value === 'Yes') {
-            return Service.odo.execute(`odo project set ${service.getParent().getParent().getName()} && odo app set ${service.getParent().getName()} && odo service delete ${service.getName()} -f`)
-            .then(() => {
-                Service.explorer.refresh(service.getParent());
-                return `Service '${service.getName()} successfully deleted'`;
-            })
-            .catch((err) => { return Promise.reject(`Failed to delete service with error '${err}'`); });
+    static async del(treeItem: OpenShiftObject): Promise<string> {
+        let project: OpenShiftObject;
+        let application: OpenShiftObject;
+        let service: OpenShiftObject = treeItem;
+        if (!service) {
+            project = await vscode.window.showQuickPick(Service.odo.getProjects(), {placeHolder: "From which project you want to delete service"});
+            application = await vscode.window.showQuickPick(Service.odo.getApplications(project), {placeHolder: "From which application you want to delete service"});
+            if (application) {
+                service = await vscode.window.showQuickPick(Service.odo.getServices(application), {placeHolder: "Select service to delete"});
+            }
         }
-        return Promise.resolve(null);
+        if (service) {
+            const answer = await vscode.window.showWarningMessage(`Are you sure you want to delete service '${service.getName()}'`, 'Yes', 'Cancel');
+            if (answer === 'Yes') {
+                return Service.odo.execute(`odo project set ${service.getParent().getParent().getName()} && odo app set ${service.getParent().getName()} && odo service delete ${service.getName()} -f`)
+                    .then(() => Service.explorer.refresh(treeItem ? treeItem.getParent() : undefined))
+                    .then(() => `Service '${service.getName()} successfully deleted'`)
+                    .catch((err) => Promise.reject(`Failed to delete service with error '${err}'`));
+            }
+            return null;
+        }
     }
 }
