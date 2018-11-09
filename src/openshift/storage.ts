18c2bc3374c5f4643191eed556edb560d06931ee
Denis Golovin <dgolovin@redhat.com>
2018-11-09 11:13:07 -0800

    Cleanup code

--- src/openshift/storage.ts
+++ src/openshift/storage.ts
@@ -12,36 +12,36 @@ export class Storage extends OpenShiftItem {
                 return 'Invalid storage name';
             }
         }});
-        if (!storageName) return Promise.resolve(null);
 
+        if (!storageName) return null;
         const mountPath = await vscode.window.showInputBox({prompt: "Specify the mount path", validateInput: (value: string) => {
             if (validator.isEmpty(value.trim())) {
                 return 'Invalid mount path';
             }
         }});
-        if (!mountPath) return Promise.resolve(null);
 
+        if (!mountPath) return null;
         const storageSize = await vscode.window.showQuickPick(['1Gi', '1.5Gi', '2Gi'], {placeHolder: 'Select the storage size'});
         return Promise.resolve()
             .then(() => Storage.odo.execute(`odo project set ${project.getName()} && odo app set ${app.getName()} && odo component set ${context.getName()} && odo storage create ${storageName} --path=${mountPath} --size=${storageSize}`))
-            .then(() => {
-                Storage.explorer.refresh(context);
-                return `Storage '${storageName}' successfully created for component '${context.getName()}'`;
-            })
-            .catch((e: Error) => { return Promise.reject(`New Storage command failed with error: '${e}'!`); });
+            .then(() => Storage.explorer.refresh(context))
+            .then(() => `Storage '${storageName}' successfully created for component '${context.getName()}'`)
+            .catch((err) => Promise.reject(`New Storage command failed with error: '${err}'!`));
     }
 
     static async del(treeItem: OpenShiftObject): Promise<string> {
         let project: OpenShiftObject;
         let component: OpenShiftObject;
         let application: OpenShiftObject;
-        let storage: OpenShiftObject;
-        if (treeItem) {
-            storage = treeItem;
-        } else {
+        let storage: OpenShiftObject = treeItem;
+        if (!storage) {
             project = await vscode.window.showQuickPick(Storage.odo.getProjects(), {placeHolder: "From which project you want to delete storage"});
-            application = await vscode.window.showQuickPick(Storage.odo.getApplications(project), {placeHolder: "From which application you want to delete storage"});
-            component = await vscode.window.showQuickPick(Storage.odo.getComponents(application), {placeHolder: "From which component you want ot delete storage"});
+            if (project) {
+                application = await vscode.window.showQuickPick(Storage.odo.getApplications(project), {placeHolder: "From which application you want to delete storage"});
+            }
+            if (application) {
+                component = await vscode.window.showQuickPick(Storage.odo.getComponents(application), {placeHolder: "From which component you want ot delete storage"});
+            }
             if (component) {
                 storage = await vscode.window.showQuickPick(Storage.odo.getStorageNames(component), {placeHolder: "Select storage to delete"});
             }
@@ -54,12 +54,11 @@ export class Storage extends OpenShiftItem {
             if (value === 'Yes') {
                 return Promise.resolve()
                     .then(() => Storage.odo.execute(`odo project set ${project.getName()} && odo app set ${app.getName()} && odo component set ${component.getName()} && odo storage delete ${storage.getName()} -f`))
-                    .then(() => Storage.explorer.refresh(project))
-                    .then(() => {
-                        return `Storage '${storage.getName()}' from component '${component.getName()}' successfully deleted`;
-                    }).catch((err) => { return Promise.reject(`Failed to delete storage with error '${err}'`); });
+                    .then(() => Storage.explorer.refresh(treeItem ? component : undefined))
+                    .then(() => `Storage '${storage.getName()}' from component '${component.getName()}' successfully deleted`)
+                    .catch((err) => Promise.reject(`Failed to delete storage with error '${err}'`));
             }
-            return Promise.resolve(null);
+            return null;
         }
     }
 }
