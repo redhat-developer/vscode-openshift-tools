7cbd8370dab7f590a480b0d39907fa5c6b5870be
Jan Richter <jrichter@redhat.com>
2018-11-15 11:36:31 +0100

    Unit tests for OS/Project

--- src/openshift/project.ts
+++ src/openshift/project.ts
@@ -6,7 +6,6 @@
 import { OpenShiftItem } from './openshiftItem';
 import { OpenShiftObject } from '../odo';
 import * as vscode from 'vscode';
-import { OpenShiftExplorer } from '../explorer';
 import * as validator from 'validator';
 
 export class Project extends OpenShiftItem {
@@ -18,14 +17,14 @@ export class Project extends OpenShiftItem {
                 return Project.validateName(value);
             }
         });
-        if (!projectName) return Promise.resolve(null);
+        if (!projectName) return null;
         return Promise.resolve()
             .then(() => Project.odo.execute(`odo project create ${projectName.trim()}`))
             .then(() => {
                 Project.explorer.refresh();
                 return `Project '${projectName}' successfully created`;
             })
-            .catch((error) => { return Promise.reject(`Failed to create project with error '${error}'`); });
+            .catch((error) => Promise.reject(`Failed to create project with error '${error}'`));
     }
 
     static async del(context: OpenShiftObject): Promise<string> {
@@ -34,6 +33,7 @@ export class Project extends OpenShiftItem {
             const projects: OpenShiftObject[] = await Project.odo.getProjects();
             project = await vscode.window.showQuickPick(projects, {placeHolder: "Select Project to delete"});
         }
+        if (!project) return null;
 
         const value = await vscode.window.showWarningMessage(`Are you sure you want to delete project '${project.getName()}'?`, 'Yes', 'Cancel');
         if (value === 'Yes') {
@@ -43,7 +43,7 @@ export class Project extends OpenShiftItem {
                     Project.explorer.refresh();
                     return `Project '${project.getName()}' successfully deleted`;
                 })
-                .catch((err) => { return Promise.reject(`Failed to delete project with error '${err}'`); });
+                .catch((err) => Promise.reject(`Failed to delete project with error '${err}'`));
         }
         return null;
     }
