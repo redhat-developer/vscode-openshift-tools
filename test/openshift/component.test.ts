70e96487c7eb5562e35ee9f582e9ecc5937d18f5
Mohit Suman <mohit.skn@gmail.com>
2018-11-17 05:45:37 +0530

    add support for binary file to create component

--- test/openshift/component.test.ts
+++ test/openshift/component.test.ts
@@ -89,6 +89,13 @@ suite('Openshift/Component', () => {
                 expect(result).null;
             });
 
+            test('returns null when no component name selected', async () => {
+                inputStub.resolves();
+                const result = await Component.create(appItem);
+
+                expect(result).null;
+            });
+
             test('returns null when no component type selected', async () => {
                 quickPickStub.onSecondCall().resolves();
                 const result = await Component.create(appItem);
@@ -161,6 +168,55 @@ suite('Openshift/Component', () => {
                 expect(commandStub).calledOnceWith('git.clone', uri);
             });
         });
+
+        suite('from binary file', () => {
+            let fileStub: sinon.SinonStub;
+            const filePath = { fsPath: 'path/path' };
+
+            setup(() => {
+                quickPickStub.onFirstCall().resolves({ label: 'Binary File' });
+                fileStub = sandbox.stub(vscode.window, 'showOpenDialog').resolves([filePath]);
+                inputStub.resolves(componentItem.getName());
+            });
+
+            test('happy path works', async () => {
+                const steps = [
+                    {command: `odo create ${componentType}:${version} ${componentItem.getName()} --binary ${filePath.fsPath} --app ${appItem.getName()} --project ${projectItem.getName()}`, increment: 100}
+                ];
+                const result = await Component.create(appItem);
+
+                expect(result).equals(`Component '${componentItem.getName()}' successfully created`);
+                expect(progressStub).calledOnceWith(sinon.match.object, steps);
+            });
+
+            test('returns null when no binary file selected', async () => {
+                fileStub.resolves();
+                const result = await Component.create(appItem);
+
+                expect(result).null;
+            });
+
+            test('returns null when no component name selected', async () => {
+                inputStub.resolves();
+                const result = await Component.create(appItem);
+
+                expect(result).null;
+            });
+
+            test('returns null when no component type selected', async () => {
+                quickPickStub.onSecondCall().resolves();
+                const result = await Component.create(appItem);
+
+                expect(result).null;
+            });
+
+            test('returns null when no component type version selected', async () => {
+                quickPickStub.onThirdCall().resolves();
+                const result = await Component.create(appItem);
+
+                expect(result).null;
+            });
+        })
     });
 
     suite('del', () => {
