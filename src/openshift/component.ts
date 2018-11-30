a7d8822ffd352698d3fb95544739a9e674363fef
sudhirverma <sudhirvvermaa@gmail.com>
2018-11-30 18:32:25 +0530

    Fix minor changes

--- src/openshift/component.ts
+++ src/openshift/component.ts
@@ -134,6 +134,7 @@ export class Component extends OpenShiftItem {
     }
 
     static push(context: OpenShiftObject): void {
+
         const app: OpenShiftObject = context.getParent();
         const project: OpenShiftObject = app.getParent();
         Component.odo.executeInTerminal(Command.pushComponent(project.getName(), app.getName(), context.getName()));
