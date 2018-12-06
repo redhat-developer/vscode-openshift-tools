3a9a8c86e34d0a6bff50112dea20073cdb12a44d
Denis Golovin <dgolovin@redhat.com>
2018-12-06 14:11:03 -0800

    Copyright header fix

---
+++ build/check-copyright.ts
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+'use strict'
+
+import walker = require('walker');
+import { WatchUtil } from '../src/util/watch';
+
+walker('.').filterDir((dir) => dir !== 'node_modules' && dir !== '.vscode-test').on('file', async (file:string) => {
+  if(file.endsWith('.ts')) {
+    await WatchUtil.grep(file, /Copyright \(c\) Red Hat\, Inc\. All rights reserved/).catch((err) => console.log(`${file}`));
+  }
+});
