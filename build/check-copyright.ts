deec291d71a5c8c4e3600fe9081863f2d17dc7ab
Denis Golovin <dgolovin@users.noreply.github.com>
2018-12-07 21:58:09 -0800

    Cleanup tslint errors (#473)

--- build/check-copyright.ts
+++ build/check-copyright.ts
@@ -3,13 +3,13 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-'use strict'
+'use strict';
 
 import walker = require('walker');
 import { WatchUtil } from '../src/util/watch';
 
-walker('.').filterDir((dir) => dir !== 'node_modules' && dir !== '.vscode-test').on('file', async (file:string) => {
-  if(file.endsWith('.ts')) {
-    await WatchUtil.grep(file, /Copyright \(c\) Red Hat\, Inc\. All rights reserved/).catch((err) => console.log(`${file}`));
+walker('.').filterDir((dir) => dir !== 'node_modules' && dir !== '.vscode-test').on('file', async (file: string) => {
+  if (file.endsWith('.ts')) {
+    await WatchUtil.grep(file, /Copyright \(c\) Red Hat\, Inc\. All rights reserved/).catch((err) => console.log(file));
   }
 });
