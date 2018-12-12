d01e41a097843789d638660eb473306cbad9cb92
Sudhir Verma <sudhirvvermaa@gmail.com>
2018-12-12 17:09:17 +0530

    Added unittest

--- src/openshift/url.ts
+++ src/openshift/url.ts
@@ -7,11 +7,11 @@ import { OpenShiftObject, Odo, OdoImpl, Command } from '../odo';
 import * as vscode from 'vscode';
 import { CliExitData } from '../cli';
 
-export class Url {
-    private static odo: Odo = OdoImpl.getInstance();
+export class Url extends OpenShiftItem  {
 
     static async create(context: OpenShiftObject): Promise<string> {
-        const app: OpenShiftObject = context.getParent();
+        const data: OpenShiftObject = await Url.getUrlData(context);
+        const app: OpenShiftObject = data.getParent();
         const project: OpenShiftObject = app.getParent();
         const portsResult: CliExitData = await Url.odo.execute(Command.listComponentPorts(project.getName(), app.getName(), context.getName()));
         let ports: string[] = portsResult.stdout.trim().split(',');
@@ -22,11 +22,60 @@ export class Url {
         } else if (ports.length > 1) {
             port = await vscode.window.showQuickPick(ports, {placeHolder: "Select port to expose"});
         } else {
-            return Promise.reject(`Component '${context.getName()}' has no ports decalred.`);
+            return Promise.reject(`Component '${data.getName()}' has no ports decalred.`);
         }
         return port === undefined ? undefined : Promise.resolve()
-            .then(async () => Url.odo.execute(Command.createCompontentUrl(project.getName(), app.getName(), context.getName(), port)))
-            .then(() => `URL for component '${context.getName()}' successfully created`)
-            .catch((err) => Promise.reject(`Failed to create URL for component '${context.getName()}'`));
+            .then(async () => Url.odo.execute(`odo url create --port ${port} --project ${project.getName()} --app ${app.getName()} --component ${data.getName()}`))
+            .then(() => `URL for component '${data.getName()}' successfully created`)
+            .catch((err) => Promise.reject(`Failed to create URL for component '${data.getName()}'`));
+    }
+
+    static async getUrlData(url) {
+        let urlName: OpenShiftObject  = url ? url : undefined;
+        let name: OpenShiftObject;
+        if (!urlName) {
+            name = await Url.getProjectName();
+        }
+        if (urlName) {
+            urlName = url;
+        }
+        return name || urlName ? name || urlName : undefined;
+    }
+
+    static async getProjectName() {
+        let application: OpenShiftObject;
+        let component: OpenShiftObject;
+        const project: OpenShiftObject = await vscode.window.showQuickPick(Url.getProjectNames(), {placeHolder: "In which Project you want to create an Url"});
+        if (project) {
+            application = await vscode.window.showQuickPick(Url.getApplicationNames(project), {placeHolder: "In which Application you want to create an Url"});
+        }
+        if (application) {
+            component = await vscode.window.showQuickPick(Url.getComponentNames(application), {placeHolder: "In which Component you want to create an Url"});
+        }
+        return component ? component: undefined;
+    }
+
+    static async getProjectNames() {
+        const projectList: Array<OpenShiftObject> = await Url.odo.getProjects();
+        if (projectList.length === 0) {
+           throw Error('You need at least one Project available to create an Url. Please create new OpenShift Project and try again.');
+        }
+        return projectList;
+    }
+
+    static async getApplicationNames(project) {
+        const applicationList: Array<OpenShiftObject> = await Url.odo.getApplications(project);
+        if (applicationList.length === 0) {
+            throw Error('You need at least one Application available to create an Url. Please create new OpenShift Project and try again.');
+         }
+         return applicationList;
+    }
+
+    static async getComponentNames(application) {
+        const applicationList: Array<OpenShiftObject> = await Url.odo.getComponents(application);
+        if (applicationList.length === 0) {
+            throw Error('You need at least one Component available to create an Url. Please create new OpenShift Project and try again.');
+        }
+         return applicationList;
     }
 }
--- test/openshift/url.test.ts
+++ test/openshift/url.test.ts
@@ -18,6 +18,7 @@ chai.use(sinonChai);
 
 suite('Openshift/URL', () => {
     let sandbox: sinon.SinonSandbox;
+    let quickPickStub: sinon.SinonStub;
     let execStub: sinon.SinonStub;
     const projectItem = new TestItem(null, 'project');
     const appItem = new TestItem(projectItem, 'app');
@@ -25,10 +26,58 @@ suite('Openshift/URL', () => {
 
     setup(() => {
         sandbox = sinon.createSandbox();
+        quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
     });
 
     teardown(() => {
         sandbox.restore();
+    });
+
+    suite('create Url with no context', () => {
+
+        setup(() => {
+            quickPickStub.onFirstCall().resolves(projectItem);
+            quickPickStub.onSecondCall().resolves(appItem);
+            quickPickStub.onThirdCall().resolves(componentItem);
+        });
+
+        teardown(() => {
+            sandbox.restore();
+        });
+
+        test('calls the appropriate error message if no project found', async () => {
+            quickPickStub.restore();
+            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([]);
+            sandbox.stub(vscode.window, 'showErrorMessage');
+            try {
+                await Url.create(null);
+            } catch (err) {
+                expect(err.message).equals('You need at least one Project available to create an Url. Please create new OpenShift Project and try again.');
+                return;
+            }
+            expect.fail();
+        });
+
+        test('asks to select port if more that one exposed and returns message', async () => {
+            execStub = sandbox.stub(OdoImpl.prototype, 'execute');
+            execStub.resolves({error: null, stdout: 'port1,', stderr: ''});
+            quickPickStub.resolves('port1');
+            const result = await Url.create(null);
+
+            expect(result).equals(`URL for component '${componentItem.getName()}' successfully created`);
+            expect(execStub).callCount(4);
+        });
+
+        test('rejects when fails to create Url', () => {
+            execStub = sandbox.stub(OdoImpl.prototype, 'execute');
+            execStub.resolves({error: null, stdout: 'port1,', stderr: ''});
+            execStub.onFirstCall().rejects();
+
+            return Url.create(null).catch((err) => {
+                expect(err).equals(`Failed to create URL for component '${componentItem.getName()}'`);
+            });
+        });
+
     });
 
     suite('create', () => {
@@ -37,7 +86,7 @@ suite('Openshift/URL', () => {
             execStub = sandbox.stub(OdoImpl.prototype, 'execute');
             execStub.onFirstCall().resolves({error: null, stdout: 'port1,port2,port3,', stderr: ''});
             execStub.onSecondCall().resolves();
-            sandbox.stub(vscode.window, 'showQuickPick').resolves('port1');
+            quickPickStub.resolves('port1');
             const result = await Url.create(componentItem);
 
             expect(result).equals(`URL for component '${componentItem.getName()}' successfully created`);
