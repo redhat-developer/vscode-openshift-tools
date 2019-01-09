6cca557fbbb6fe48bfca287cad03beab0660d679
Jan Richter <jrichter@redhat.com>
2019-01-09 11:59:18 +0100

    Add login handling for when ~/.kube is empty

--- src/odo.ts
+++ src/odo.ts
@@ -207,6 +207,12 @@ export function getInstance(): Odo {
 export class OdoImpl implements Odo {
     private static cli: cliInstance.ICli = cliInstance.Cli.getInstance();
     private static instance: OdoImpl;
+    private readonly odoLoginMessages = [
+        'Please log in to the cluster',
+        'the server has asked for the client to provide credentials',
+        'Please login to your server'
+    ];
+
     private constructor() {}
 
     public static getInstance(): OdoImpl {
@@ -298,7 +304,7 @@ export class OdoImpl implements Odo {
         const result: cliInstance.CliExitData = await this.execute(
             Command.printOdoVersionAndProjects(), process.cwd(), false
         );
-        if (result.stdout.indexOf('Please log in to the cluster') > -1 || result.stdout.indexOf('the server has asked for the client to provide credentials') > -1) {
+        if (this.odoLoginMessages.some((element) => { return result.stdout.indexOf(element) > -1; })) {
             const loginErrorMsg: string = 'Please log in to the cluster';
             return[new OpenShiftObjectImpl(null, loginErrorMsg, ContextType.LOGIN_REQUIRED, this, TreeItemCollapsibleState.None)];
         }
@@ -375,6 +381,6 @@ export class OdoImpl implements Odo {
 
     public async requireLogin(): Promise<boolean> {
         const result: cliInstance.CliExitData = await this.execute(Command.printOdoVersionAndProjects(), process.cwd(), false);
-        return result.stdout.indexOf("Please log in to the cluster") > -1 || result.stdout.indexOf('the server has asked for the client to provide credentials') > -1;
+        return this.odoLoginMessages.some((element) => { return result.stdout.indexOf(element) > -1; });
     }
 }
