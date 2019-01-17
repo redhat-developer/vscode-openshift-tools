126730f0b0632da8cf92e8edf11bfc04e14584fd
Denis Golovin <dgolovin@redhat.com>
2019-01-16 23:58:28 -0800

    Fix messages for component storage

--- src/openshift/storage.ts
+++ src/openshift/storage.ts
@@ -11,11 +11,11 @@ import * as validator from 'validator';
 export class Storage extends OpenShiftItem {
     static async create(context: OpenShiftObject): Promise<string> {
         const component = await Storage.getOpenShiftCmdData(context,
-            "In which Project you want to create an Storage",
-            "In which Application you want to create an Storage",
-            "In which Component you want to create an Storage");
+            "In which Project you want to create a Storage",
+            "In which Application you want to create a Storage",
+            "In which Component you want to create a Storage");
         if (component) {
-            const storageName = await vscode.window.showInputBox({prompt: "Specify the storage name", validateInput: (value: string) => {
+            const storageName = await vscode.window.showInputBox({prompt: "Specify the Storage name", validateInput: (value: string) => {
                 if (validator.isEmpty(value.trim())) {
                     return 'Invalid storage name';
                 }
@@ -29,13 +29,13 @@ export class Storage extends OpenShiftItem {
             }});
             if (!mountPath) return null;
 
-            const storageSize = await vscode.window.showQuickPick(['1Gi', '1.5Gi', '2Gi'], {placeHolder: 'Select the storage size'});
+            const storageSize = await vscode.window.showQuickPick(['1Gi', '1.5Gi', '2Gi'], {placeHolder: 'Select the Storage size'});
             if (!storageSize) return null;
 
             return Promise.resolve()
                 .then(() => Storage.odo.execute(Command.createStorage(component.getParent().getParent().getName(), component.getParent().getName(), component.getName(), storageName, mountPath, storageSize)))
                 .then(() => Storage.explorer.refresh())
-                .then(() => `Storage '${storageName}' successfully created for component '${component.getName()}'`)
+                .then(() => `Storage '${storageName}' successfully created for Component '${component.getName()}'`)
                 .catch((err) => Promise.reject(`New Storage command failed with error: '${err}'!`));
 
         }
@@ -47,28 +47,28 @@ export class Storage extends OpenShiftItem {
         let application: OpenShiftObject;
         let storage: OpenShiftObject = treeItem;
         if (!storage) {
-            project = await vscode.window.showQuickPick(Storage.odo.getProjects(), {placeHolder: "From which project you want to delete storage"});
+            project = await vscode.window.showQuickPick(Storage.odo.getProjects(), {placeHolder: "From which Project you want to delete Storage"});
             if (project) {
-                application = await vscode.window.showQuickPick(Storage.odo.getApplications(project), {placeHolder: "From which application you want to delete storage"});
+                application = await vscode.window.showQuickPick(Storage.odo.getApplications(project), {placeHolder: "From which Application you want to delete Storage"});
             }
             if (application) {
-                component = await vscode.window.showQuickPick(Storage.odo.getComponents(application), {placeHolder: "From which component you want ot delete storage"});
+                component = await vscode.window.showQuickPick(Storage.odo.getComponents(application), {placeHolder: "From which Component you want ot delete Storage"});
             }
             if (component) {
-                storage = await vscode.window.showQuickPick(Storage.odo.getStorageNames(component), {placeHolder: "Select storage to delete"});
+                storage = await vscode.window.showQuickPick(Storage.odo.getStorageNames(component), {placeHolder: "Select Storage to delete"});
             }
         }
         if (storage) {
             const component: OpenShiftObject = storage.getParent();
             const app: OpenShiftObject = component.getParent();
             project = app.getParent();
-            const value = await vscode.window.showWarningMessage(`Do you want to delete storage '${storage.getName()}' from component '${component.getName()}'?`, 'Yes', 'Cancel');
+            const value = await vscode.window.showWarningMessage(`Do you want to delete Storage '${storage.getName()}' from Component '${component.getName()}'?`, 'Yes', 'Cancel');
             if (value === 'Yes') {
                 return Promise.resolve()
                     .then(() => Storage.odo.execute(Command.deleteStorage(project.getName(), app.getName(), component.getName(), storage.getName())))
                     .then(() => Storage.explorer.refresh(treeItem ? component : undefined))
-                    .then(() => `Storage '${storage.getName()}' from component '${component.getName()}' successfully deleted`)
-                    .catch((err) => Promise.reject(`Failed to delete storage with error '${err}'`));
+                    .then(() => `Storage '${storage.getName()}' from Component '${component.getName()}' successfully deleted`)
+                    .catch((err) => Promise.reject(`Failed to delete Storage with error '${err}'`));
             }
         }
         return null;
