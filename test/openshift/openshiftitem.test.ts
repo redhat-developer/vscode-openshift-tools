1023d2bfbb835f5be620a5d08e7c89908e8cbd76
Denis Golovin <dgolovin@redhat.com>
2019-01-27 20:31:17 -0800

    Fix copyright for openshiftitem.test.ts file

--- test/openshift/openshiftitem.test.ts
+++ test/openshift/openshiftitem.test.ts
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+'use strict';
+
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
 import sinon = require('sinon');
