7de2ece32cc6c4ff3f54297588ef1c3091efa050
Denis Golovin <dgolovin@redhat.com>
2019-02-09 21:26:47 -0800

    Cleanup application tests

--- test/openshift/application.test.ts
+++ test/openshift/application.test.ts
@@ -22,6 +22,7 @@ suite('Openshift/Application', () => {
     let sandbox: sinon.SinonSandbox;
     let execStub: sinon.SinonStub;
     let getProjectNamesStub: sinon.SinonStub;
+    let inputStub: sinon.SinonStub;
     const projectItem = new TestItem(null, 'project');
     const appItem = new TestItem(projectItem, 'app');
 
@@ -30,183 +31,144 @@ suite('Openshift/Application', () => {
         execStub = sandbox.stub(OdoImpl.prototype, 'execute').resolves({error: null, stdout: '', stderr: ''});
         getProjectNamesStub = sandbox.stub(OpenShiftItem, 'getProjectNames').resolves([projectItem]);
         sandbox.stub(OpenShiftItem, 'getApplicationNames').resolves([appItem]);
+        inputStub = sandbox.stub(vscode.window, 'showInputBox');
     });
 
     teardown(() => {
         sandbox.restore();
     });
 
-    suite('create', () => {
-        let inputStub: sinon.SinonStub;
+    suite('create command', () => {
 
-        setup(() => {
-            inputStub = sandbox.stub(vscode.window, 'showInputBox');
-        });
+        suite('called form \'OpenShift Application Explorer\'', () => {
 
-        test('calls the appropriate odo command', async () => {
-            inputStub.resolves('name');
+            test('executes the appropriate odo command', async () => {
+                inputStub.resolves('name');
+                execStub.resolves();
 
-            await Application.create(projectItem);
-
-            expect(execStub).calledOnceWith(Command.createApplication(projectItem.getName(), 'name'));
-        });
-
-        test('returns status when successful', async () => {
-            inputStub.resolves('name1');
-
-            const result = await Application.create(projectItem);
-
-            expect(result).equals(`Application 'name1' successfully created`);
-        });
-
-        test('returns null with no input', async() => {
-            inputStub.resolves();
-
-            const result = await Application.create(projectItem);
-
-            expect(result).null;
-        });
-
-        test('wraps error messages with additional info', async () => {
-            inputStub.resolves('name1');
-            execStub.rejects('ERROR');
-
-            try {
                 await Application.create(projectItem);
-            } catch (err) {
-                expect(err).equals(`Failed to create Application with error 'ERROR'`);
-                return;
-            }
-            expect.fail();
+
+                expect(execStub).calledOnceWith(Command.createApplication(projectItem.getName(), 'name'));
+            });
+
+            test('returns message text when successful', async () => {
+                inputStub.resolves('name1');
+                execStub.resolves();
+
+                const result = await Application.create(projectItem);
+
+                expect(result).equals(`Application 'name1' successfully created`);
+            });
+
+            test('returns null when input was canceled', async() => {
+                inputStub.resolves();
+
+                const result = await Application.create(projectItem);
+
+                expect(result).null;
+            });
+
+            test('trows error messages', async () => {
+                inputStub.resolves('name1');
+                execStub.rejects('ERROR');
+
+                try {
+                    await Application.create(projectItem);
+                } catch (err) {
+                    expect(err).equals(`Failed to create Application with error 'ERROR'`);
+                    return;
+                }
+                expect.fail();
+            });
+        });
+
+        suite('called from command bar', () => {
+
+            setup(() => {
+                quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
+                quickPickStub.onFirstCall().resolves(projectItem);
+            });
+
+            test('returns the appropriate error message when no projects available', async () => {
+                quickPickStub.restore();
+                getProjectNamesStub.restore();
+                sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([]);
+                sandbox.stub(vscode.window, 'showErrorMessage');
+                try {
+                    await Application.create(null);
+                } catch (err) {
+                    expect(err.message).equals('You need at least one Project available. Please create new OpenShift Project and try again.');
+                    return;
+                }
+                expect.fail();
+
+            });
+
+            test('asks to select project first', async () => {
+                const projects = Promise.resolve([projectItem, projectItem]);
+                inputStub.resolves('name');
+                execStub.resolves();
+
+                await Application.create(null);
+
+                expect(quickPickStub).calledWith(projects, { placeHolder: "In which Project you want to create an Application" });
+            });
         });
     });
 
-    suite('create application with no context', () => {
-        let inputStub: sinon.SinonStub;
-
-        setup(() => {
-            inputStub = sandbox.stub(vscode.window, 'showInputBox');
-            quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
-            quickPickStub.onFirstCall().resolves(projectItem);
-        });
-
-        test('calls the appropriate error message', async () => {
-            quickPickStub.restore();
-            getProjectNamesStub.restore();
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([]);
-            sandbox.stub(vscode.window, 'showErrorMessage');
-            try {
-                await Application.create(null);
-            } catch (err) {
-                expect(err.message).equals('You need at least one Project available. Please create new OpenShift Project and try again.');
-                return;
-            }
-            expect.fail();
-
-        });
-
-        test('calls the appropriate odo command', async () => {
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem]);
-            inputStub.resolves('name');
-
-            await Application.create(null);
-
-            expect(execStub).calledOnceWith(Command.createApplication(projectItem.getName(), 'name'));
-        });
-
-        test('calls the appropriate odo command when there are more then one project', async () => {
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem, projectItem]);
-            inputStub.resolves('name');
-
-            await Application.create(null);
-
-            expect(execStub).calledOnceWith(Command.createApplication(projectItem.getName(), 'name'));
-        });
-
-        test('returns status when successful', async () => {
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem]);
-            inputStub.resolves('name1');
-
-            const result = await Application.create(null);
-
-            expect(result).equals(`Application 'name1' successfully created`);
-        });
-
-        test('returns null with no input', async() => {
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem]);
-            inputStub.resolves();
-
-            const result = await Application.create(null);
-
-            expect(result).null;
-        });
-
-        test('wraps error messages with additional info', async () => {
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem]);
-            inputStub.resolves('name1');
-            execStub.rejects('ERROR');
-
-            try {
-                await Application.create(null);
-            } catch (err) {
-                expect(err).equals(`Failed to create Application with error 'ERROR'`);
-                return;
-            }
-            expect.fail();
-        });
-    });
-
-    suite('describe', () => {
+    suite('describe command', () => {
         let termStub: sinon.SinonStub;
 
         setup(() => {
             termStub = sandbox.stub(OdoImpl.prototype, 'executeInTerminal').resolves();
-            quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
-            quickPickStub.onFirstCall().resolves(projectItem);
-            quickPickStub.onSecondCall().resolves(appItem);
         });
 
-        test('calls the appropriate odo command in terminal', async () => {
-            await Application.describe(appItem);
+        suite('called form \'OpenShift Application Explorer\'', () => {
 
-            expect(termStub).calledOnceWith(Command.describeApplication(projectItem.getName(), appItem.getName()));
-        });
-    });
+            test('executes the appropriate odo command in terminal', async () => {
+                await Application.describe(appItem);
 
-    suite('describe with no context', () => {
-        let termStub: sinon.SinonStub;
-
-        setup(() => {
-            termStub = sandbox.stub(OdoImpl.prototype, 'executeInTerminal').resolves();
-            quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
+                expect(termStub).calledOnceWith(Command.describeApplication(appItem.getParent().getName(), appItem.getName()));
+            });
         });
 
-        test('calls the appropriate odo command in terminal', async () => {
-            quickPickStub.onFirstCall().resolves(projectItem);
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([projectItem]);
-            quickPickStub.onSecondCall().resolves(appItem);
-            sandbox.stub(OdoImpl.prototype, 'getApplications').resolves([appItem]);
-            await Application.describe(null);
+        suite('called from command palette', () => {
 
-            expect(termStub).calledOnceWith(Command.describeApplication(projectItem.getName(), appItem.getName()));
-        });
+            test('calls the appropriate error message when no project found', async () => {
+                getProjectNamesStub.restore();
+                sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([]);
+                try {
+                    await Application.describe(null);
+                } catch (err) {
+                    expect(err.message).equals('You need at least one Project available. Please create new OpenShift Project and try again.');
+                    return;
+                }
+                expect.fail();
 
-        test('calls the appropriate error message when no project found', async () => {
-            quickPickStub.restore();
-            getProjectNamesStub.restore();
-            sandbox.stub(OdoImpl.prototype, 'getProjects').resolves([]);
-            try {
+            });
+
+            test('asks to select a project and an application', async () => {
+                const projects = Promise.resolve([projectItem]);
+                const apps = Promise.resolve([appItem]);
+                quickPickStub = sandbox.stub(vscode.window, 'showQuickPick');
+                quickPickStub.onFirstCall().resolves(projectItem);
+                quickPickStub.onSecondCall().resolves(appItem);
+
                 await Application.describe(null);
-            } catch (err) {
-                expect(err.message).equals('You need at least one Project available. Please create new OpenShift Project and try again.');
-                return;
-            }
-            expect.fail();
 
+                expect(quickPickStub).calledWith(projects, { placeHolder: "From which project you want to describe Application" });
+                expect(quickPickStub).calledWith(apps, { placeHolder: "Select Application you want to describe" });
+            });
+
+            test('skips odo command execution if canceled by user', async () => {
+                quickPickStub = sandbox.stub(vscode.window, 'showQuickPick').resolves(null);
+                await Application.describe(null);
+                expect(termStub).not.called;
+            });
         });
     });
 
-    suite('del', () => {
+    suite('delete command', () => {
         let warnStub: sinon.SinonStub;
 
         setup(() => {
@@ -221,7 +183,7 @@ suite('Openshift/Application', () => {
             expect(execStub).calledOnceWith(Command.deleteApplication(projectItem.getName(), appItem.getName()));
         });
 
-        test('returns status when successful', async () => {
+        test('returns a confirmation message text when successful', async () => {
             warnStub.resolves('Yes');
 
             const result = await Application.del(appItem);
@@ -237,7 +199,7 @@ suite('Openshift/Application', () => {
             expect(result).null;
         });
 
-        test('wraps error messages with additional info', async () => {
+        test('trows an error message when odo command failed', async () => {
             warnStub.resolves('Yes');
             execStub.rejects('ERROR');
 
@@ -250,7 +212,7 @@ suite('Openshift/Application', () => {
             expect.fail();
         });
 
-        test('requests for project and exits if not provided', async () => {
+        test('requests for a project name and exits if not provided', async () => {
             const stub = sandbox.stub(vscode.window, 'showQuickPick');
             stub.onFirstCall().resolves();
             warnStub.resolves('Yes');
@@ -259,7 +221,7 @@ suite('Openshift/Application', () => {
             expect(warnStub).is.not.called;
         });
 
-        test('requests for project and application and exit if application is not provided', async () => {
+        test('requests for a project and an application and exit if application is not provided', async () => {
             const stub = sandbox.stub(vscode.window, 'showQuickPick');
             stub.onFirstCall().resolves(appItem);
             stub.onSecondCall().resolves();
