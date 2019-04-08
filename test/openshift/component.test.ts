68b4d06ed7c9bf37ccba347d42b4b5526679e0be
Denis Golovin <dgolovin@redhat.com>
2019-04-08 11:10:21 -0700

    Fix test error

--- test/openshift/component.test.ts
+++ test/openshift/component.test.ts
@@ -367,7 +367,7 @@ suite('OpenShift/Component', () => {
             const result = await Component.del(null);
 
             expect(result).equals(`Component '${componentItem.getName()}' successfully deleted`);
-            expect(execStub).calledOnceWith(Command.deleteComponent(projectItem.getName(), appItem.getName(), componentItem.getName()));
+            expect(execStub).calledWith(Command.deleteComponent(projectItem.getName(), appItem.getName(), componentItem.getName()));
         });
 
         test('wraps errors in additional info', async () => {
