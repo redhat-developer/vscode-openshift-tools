a1fb40f4a56e20a3f90d3f878aaba76956290daa
Mohit Suman <mohit.skn@gmail.com>
2019-04-26 06:35:47 +0530

    1. Add url to the tree view 2. Perform delete url action to palette and tree view

--- src/openshift/url.ts
+++ src/openshift/url.ts
@@ -42,11 +42,31 @@ export class Url extends OpenShiftItem{
             }
 
             if (port) {
-                return Progress.execFunctionWithProgress(`Creating a URL '${urlName}' for the Component '${component.getName()}'`, 
+                return Progress.execFunctionWithProgress(`Creating a URL '${urlName}' for the Component '${component.getName()}'`,
                     () => Url.odo.execute(Command.createComponentCustomUrl(project.getName(), app.getName(), component.getName(), `${urlName}`, `${port['port']}`))
                         .then(() => `URL '${urlName}' for component '${component.getName()}' successfully created`)
                         .catch((err) => Promise.reject(`Failed to create URL '${urlName}' for component '${component.getName()}'. ${err.message}`))
                 );
+            }
+        }
+        return null;
+    }
+
+    static async del(treeItem: OpenShiftObject): Promise<string> {
+        let url = treeItem;
+        const component = await Url.getOpenShiftCmdData(url,
+            "From which Project you want to delete Route",
+            "From which Application you want to delete Route",
+            "From which Component you want to delete Route");
+        if (!url && component) {
+            url = await window.showQuickPick(Url.odo.getRoutes(component), {placeHolder: `Select the desired URL to delete from the component ${component.getName()}`});
+        }
+        if (url) {
+            const value = await window.showWarningMessage(`Do you want to delete Route '${url.getName()}' from Component '${url.getParent().getName()}'?`, 'Yes', 'Cancel');
+            if (value === 'Yes') {
+                return Progress.execFunctionWithProgress(`Deleting Route ${url.getName()} from Component ${component.getName()}`, () => Url.odo.deleteRoute(url))
+                    .then(() => `Route '${url.getName()}' from Component '${url.getParent().getName()}' successfully deleted`)
+                    .catch((err) => Promise.reject(`Failed to delete Route with error '${err}'`));
             }
         }
         return null;
