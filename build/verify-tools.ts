a9efcf71f0c14cbe392622c5acb916b43f17830f
Denis Golovin <dgolovin@redhat.com>
2019-05-30 09:59:43 -0700

    Reuse download util from project

--- build/verify-tools.ts
+++ build/verify-tools.ts
@@ -5,89 +5,50 @@
 
 'use strict';
 
-const hasha = require('hasha');
-const mkdirp = require('mkdirp');
-const request = require('request');
-const fs = require('fs-extra');
-const path = require("path");
-const { exec } = require('child_process');
-const progress = require('request-progress');
-const configData = require('../src/tools.json');
-const os = require('os');
+import hasha = require('hasha');
+import mkdirp = require('mkdirp');
+import fs = require('fs-extra');
+import path = require("path");
+import cp = require('child_process');
+import configData = require('../src/tools.json');
+import os = require('os');
+import { DownloadUtil } from '../src/util/download';
 
 async function verifyTools() {
   for (const key in configData) {
     for (const OS in configData[key].platform) {
       const targetFolder =  path.resolve(os.tmpdir(), OS);
-      console.log(`[INFO] ${configData[key].platform[OS].dlFileName} is not downloaded yet`);
       await downloadFileAndCreateSha256(targetFolder, configData[key].platform[OS].dlFileName, configData[key].platform[OS].url, configData[key].platform[OS].sha256sum);
     }
   }
 }
 
-async function downloadFileAndCreateSha256(targetFolder, fileName, reqURL, sha256sum) {
+async function downloadFileAndCreateSha256(targetFolder: string, fileName: string, reqURL: string, sha256sum: string) {
   if (!fs.existsSync(targetFolder)) {
     await mkdirp.sync(targetFolder);
   }
   const currentFile = path.join(targetFolder, fileName);
-  await downloadFile(reqURL, currentFile);
-  const currentSHA256 = await getSHA256(currentFile);
+  console.log(`${currentFile} download started from ${reqURL}`)
+  await DownloadUtil.downloadFile(reqURL, currentFile, (current) => console.log(current + '%'));
+  const currentSHA256 = await hasha.fromFile(currentFile, {algorithm: 'sha256'});;
   if (currentSHA256 === sha256sum) {
     console.log( `[INFO] ${currentFile} is downloaded and sha256 is correct`);
   } else {
     throw Error(`${currentFile} is downloaded and sha256 is not correct`);
   }
-
 }
 
-// for a given filename, return the sha256sum
-async function getSHA256(filename) {
-  return hasha.fromFile(filename, {algorithm: 'sha256'});
-}
-
-async function downloadFile(fromUrl, toFile) {
-  return new Promise(async (resolve, reject) => {
-    let previous = -1;
-    await progress(request(fromUrl), {
-      throttle: 250,
-      delay: 0,
-      lengthHeader: 'content-length'
-    }).on('progress', (state) => {
-      if (previous === -1) {
-        console.log(`[INFO] ${toFile} download started from ${fromUrl}`);
-        console.log('0%');
-        previous = 0;
-      }
-      const current = Math.round(state.percent*100);
-      if (current!==100 && current!==0 && previous!==current) {
-        console.log(current + '%');
-      }
-      previous = current;
-    }).on('error', (error)=> {
-      Promise.reject(error);
-    }).on('end', () => {
-      if (previous === -1) {
-        console.log(`[INFO] ${toFile} download started from ${fromUrl}`);
-        console.log('0%');
-      }
-      console.log('100%');
-    })
-    .pipe(fs.createWriteStream(toFile))
-    .on('close', resolve)
-    .on('error', reject);
-  });
-}
-
-function checkToolFile() {
-  const fileCheckRegex = /\w*tools.json/;
-  exec('git diff --name-only master..HEAD', async (error, stdout, stderr) => {
-    if (error) {
-      throw Error(error);
-    }
-    if (fileCheckRegex.test(stdout)) {
-      await verifyTools();
-    }
-  });
-}
-
-checkToolFile();
+const fileCheckRegex = /\w*tools.json/;
+cp.exec('git diff --name-only master..HEAD', async (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log('The changed files:');
+  console.log(stdout);
+  if (fileCheckRegex.test(stdout)) {
+    console.log('tools.json is changed, starting download verification')
+    await verifyTools();
+  } else {
+    console.log('tools.json is not changed, skipping download verification')
+  }
+});
