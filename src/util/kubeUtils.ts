215ab2f81ad386957d88b2e638d121b82cd9ae48
Mohit Suman <mohit.skn@gmail.com>
2019-06-26 10:10:32 +0530

    use getUsers() function for user list

--- src/util/kubeUtils.ts
+++ src/util/kubeUtils.ts
@@ -4,9 +4,6 @@ import { KubeConfig } from '@kubernetes/client-node';
 export async function getClusters(): Promise<QuickPickItem[]> {
     const kubectlConfig = new KubeConfig();
     kubectlConfig.loadFromDefault();
-    if (!kubectlConfig) {
-        return [];
-    }
     const currentContextServer = kubectlConfig.currentContext.split('/')[1];
     const clusters = kubectlConfig.clusters || [];
     return clusters.map((c) => {
@@ -20,11 +17,8 @@ export async function getClusters(): Promise<QuickPickItem[]> {
 export async function getUsers(clusterUrl: string): Promise<QuickPickItem[]> {
     const kubectlConfig = new KubeConfig();
     kubectlConfig.loadFromDefault();
-    if (!kubectlConfig) {
-        return [];
-    }
     const currentContextUser = kubectlConfig.currentContext.split('/')[2];
-    return kubectlConfig.users
+    return kubectlConfig.getUsers()
             .filter((item) => {
                 return item.name.split('/')[1] === clusterUrl;
             })
