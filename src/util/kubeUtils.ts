1427d919062e7f546a3af66a5bbfd0b558ce022b
Mohit Suman <mohit.skn@gmail.com>
2019-06-26 09:38:16 +0530

    Use kubernetes client API instead of kubectl

--- src/util/kubeUtils.ts
+++ src/util/kubeUtils.ts
@@ -1,49 +1,13 @@
-import { QuickPickItem, window } from "vscode";
-import * as k8s from 'vscode-kubernetes-tools-api';
-
-interface Kubeconfig {
-    readonly apiVersion: string;
-    readonly 'current-context': string;
-    readonly clusters: {
-        readonly name: string;
-        readonly cluster: {
-            readonly server: string;
-            readonly 'certificate-authority'?: string;
-            readonly 'certificate-authority-data'?: string;
-        };
-    }[] | undefined;
-    readonly contexts: {
-        readonly name: string;
-        readonly context: {
-            readonly cluster: string;
-            readonly user: string;
-            readonly namespace?: string;
-        };
-    }[] | undefined;
-    readonly users: {
-        readonly name: string;
-        readonly user: {};
-    }[] | undefined;
-}
-
-async function getKubeconfig(): Promise<Kubeconfig | null> {
-    const kubectl = await k8s.extension.kubectl.v1;
-    if (!kubectl.available) {
-        window.showWarningMessage(`Failed to fetch '~/.kube/config' file.`);
-        return null;
-    }
-    if (kubectl.available) {
-        const command = await kubectl.api.invokeCommand(`config view -o json`);
-        return JSON.parse(command.stdout);
-    }
-}
+import { QuickPickItem } from "vscode";
+import { KubeConfig } from '@kubernetes/client-node';
 
 export async function getClusters(): Promise<QuickPickItem[]> {
-    const kubectlConfig = await getKubeconfig();
+    const kubectlConfig = new KubeConfig();
+    kubectlConfig.loadFromDefault();
     if (!kubectlConfig) {
         return [];
     }
-    const currentContextServer = kubectlConfig["current-context"].split('/')[1];
+    const currentContextServer = kubectlConfig.currentContext.split('/')[1];
     const clusters = kubectlConfig.clusters || [];
     return clusters.map((c) => {
         return {
@@ -54,11 +18,12 @@ export async function getClusters(): Promise<QuickPickItem[]> {
 }
 
 export async function getUsers(clusterUrl: string): Promise<QuickPickItem[]> {
-    const kubectlConfig = await getKubeconfig();
+    const kubectlConfig = new KubeConfig();
+    kubectlConfig.loadFromDefault();
     if (!kubectlConfig) {
         return [];
     }
-    const currentContextUser = kubectlConfig["current-context"].split('/')[2];
+    const currentContextUser = kubectlConfig.currentContext.split('/')[2];
     return kubectlConfig.users
             .filter((item) => {
                 return item.name.split('/')[1] === clusterUrl;
