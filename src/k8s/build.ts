6d15502065242394eb165cd7aaa764eac0ebeaee
Sudhir Verma <sudhirvvermaa@gmail.com>
2019-07-19 15:54:26 +0530

    Add commands `Show Log` and 'Rebuild' for Build resources in Clusters view

--- src/k8s/build.ts
+++ src/k8s/build.ts
@@ -1,9 +1,38 @@
 import { window } from "vscode";
+import { OdoImpl, Odo } from "../odo";
 
-export function showLog(context: any) {
-    window.showInformationMessage(`oc log ${context.impl.id}`);
+export class Command {
+
+    static getBuild(build: string) {
+        return `oc get build -l buildconfig=${build} -o json`;
+    }
+    static showLog(text: string, build: string) {
+        return `oc logs ${text}${build}`;
+    }
+
+    static rebuild(build, build1) {
+        return `oc start-build ${build} --from-build ${build1}`;
+    }
+
+    static followLog(text: string) {
+        return `oc logs -f ${text}`;
+    }
+
+    static delete(build: String) {
+        return `oc delete ${this.delete}`;
+    }
 }
 
-export function followLog(context: any) {
-    window.showInformationMessage(`oc log -f ${context.impl.id}`);
+export class Build {
+    protected static readonly odo: Odo = OdoImpl.Instance;
+
+    static showLog(context: any) {
+        let buildName: string;
+        if (context) {
+            buildName = context.impl.name;
+        } else {
+
+        }
+        Build.odo.executeInTerminal(Command.showLog(buildName, '-build'));
+    }
 }
