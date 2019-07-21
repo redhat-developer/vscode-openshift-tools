a8eac570a209c8c3dbb1cbe2c771884cb19c35b3
Sudhir Verma <sudhirvvermaa@gmail.com>
2019-07-22 01:24:23 +0530

    Added License for build.ts and  deployment.ts (#948)

--- src/k8s/deployment.ts
+++ src/k8s/deployment.ts
@@ -1,3 +1,8 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
 import { ClusterExplorerV1 } from 'vscode-kubernetes-tools-api';
 import * as k8s from 'vscode-kubernetes-tools-api';
