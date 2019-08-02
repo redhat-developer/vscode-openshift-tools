ae1c64ce18256d4ce05789a0c55f1df9808e7b9d
Denis Golovin <dgolovin@redhat.com>
2019-08-02 01:30:52 -0700

    Fix Delete Component command for components with context
    Fix #998.

--- src/openshift/component.ts
+++ src/openshift/component.ts
@@ -81,15 +81,8 @@ export class Component extends OpenShiftItem {
         const value = await window.showWarningMessage(`Do you want to delete Component '${name}\'?`, 'Yes', 'Cancel');
         if (value === 'Yes') {
             return Progress.execFunctionWithProgress(`Deleting the Component '${component.getName()} '`, async () => {
-                if (component.contextValue === ContextType.COMPONENT_NO_CONTEXT || component.contextValue === ContextType.COMPONENT_PUSHED) {
-                    await Component.unlinkAllComponents(component);
-                    await Component.odo.deleteComponent(component);
-                }
-                if (component.contextPath) {
-                    const wsFolder = workspace.getWorkspaceFolder(component.contextPath);
-                    workspace.updateWorkspaceFolders(wsFolder.index, 1);
-                }
-
+                await Component.unlinkAllComponents(component);
+                await Component.odo.deleteComponent(component);
             }).then(() => `Component '${name}' successfully deleted`)
             .catch((err) => Promise.reject(`Failed to delete Component with error '${err}'`));
         }
