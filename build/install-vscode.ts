1f245dabe83dd03a2a399ee465c052db925f2f24
Denis Golovin <dgolovin@redhat.com>
2019-08-07 22:52:57 -0700

    Attempt 88888

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -4,7 +4,7 @@ import { platform } from 'os';
 const downloadAndUnzipVSCode = require('vscode-test').downloadAndUnzipVSCode;
 downloadAndUnzipVSCode().then((executable: string) => {
     if (platform() === 'darwin') {
-        executable = path.join(executable.substring(0, executable.indexOf('.app')+4), 'Contents', 'Resources', 'app', 'bin');
+        executable = path.join(executable.substring(0, executable.indexOf('.app')+4), 'Contents', 'Resources', 'app', 'bin', 'code');
     } else {
         executable = path.join(path.dirname(executable), 'bin', 'code');
     }
