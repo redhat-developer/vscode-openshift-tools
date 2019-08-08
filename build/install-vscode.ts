65c7aa15e399f2a1c49e58da5e81b6257ca2cc9b
Denis Golovin <dgolovin@redhat.com>
2019-08-07 22:31:39 -0700

    Fix command for names with spaces

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -1,12 +1,7 @@
 import cp = require('child_process');
 import path = require('path');
-import { platform } from 'os';
 const downloadAndUnzipVSCode = require('vscode-test').downloadAndUnzipVSCode;
 downloadAndUnzipVSCode().then((executable) => {
-    if (platform() === 'darwin') {
-        executable = path.join(path.dirname(executable), 'Contents', 'Resources', 'app', 'bin', 'code');
-    } else {
-        executable = path.join(path.dirname(executable), 'bin', 'code');
-    }
+    executable = path.join(path.dirname(executable), 'bin', 'code');
     cp.execSync(`'${executable}' --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
 });
