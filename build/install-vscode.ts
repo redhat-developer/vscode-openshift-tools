9552cc21e4282869a02299c6a2a6227ca9411231
Denis Golovin <dgolovin@redhat.com>
2019-08-07 22:49:48 -0700

    Mac fix attempt #####

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -1,7 +1,12 @@
 import cp = require('child_process');
 import path = require('path');
+import { platform } from 'os';
 const downloadAndUnzipVSCode = require('vscode-test').downloadAndUnzipVSCode;
-downloadAndUnzipVSCode().then((executable) => {
-    executable = path.join(path.dirname(executable), 'bin', 'code');
+downloadAndUnzipVSCode().then((executable: string) => {
+    if (platform() === 'darwin') {
+        executable = path.join(executable.substring(0, executable.indexOf('.app')+4), 'Contents', 'Resources', 'app', 'bin');
+    } else {
+        executable = path.join(path.dirname(executable), 'bin', 'code');
+    }
     cp.execSync(`'${executable}' --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
 });
