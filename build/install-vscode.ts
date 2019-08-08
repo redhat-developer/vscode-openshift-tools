36c374614e77b25685da4c3b6743479ea97f003b
Denis Golovin <dgolovin@redhat.com>
2019-08-07 23:10:33 -0700

    Attempt infinity.

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -4,9 +4,9 @@ import { platform } from 'os';
 const downloadAndUnzipVSCode = require('vscode-test').downloadAndUnzipVSCode;
 downloadAndUnzipVSCode().then((executable: string) => {
     if (platform() === 'darwin') {
-        executable = path.join(executable.substring(0, executable.indexOf('.app')+4), 'Contents', 'Resources', 'app', 'bin', 'code');
+        executable = `'${path.join(executable.substring(0, executable.indexOf('.app')+4), 'Contents', 'Resources', 'app', 'bin', 'code')}'`;
     } else {
-        executable = path.join(path.dirname(executable), 'bin', 'code.cmd');
+        executable = path.join(path.dirname(executable), 'bin', 'code');
     }
-    cp.execSync(`'${executable}' --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
+    cp.execSync(`${executable} --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
 });
