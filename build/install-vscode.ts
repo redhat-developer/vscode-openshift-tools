4361806fc9edc819a7cc7af07fa13c3d464ba54e
Denis Golovin <dgolovin@redhat.com>
2019-08-07 22:23:52 -0700

    Fix macOS build errors

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -1,7 +1,12 @@
 import cp = require('child_process');
 import path = require('path');
+import { platform } from 'os';
 const downloadAndUnzipVSCode = require('vscode-test').downloadAndUnzipVSCode;
 downloadAndUnzipVSCode().then((executable) => {
-    executable = path.join(path.dirname(executable), 'bin', 'code');
+    if (platform() === 'darwin') {
+        executable = path.join(path.dirname(executable), 'Contents', 'Resources', 'app', 'bin', 'code');
+    } else {
+        executable = path.join(path.dirname(executable), 'bin', 'code');
+    }
     cp.execSync(`${executable} --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
 });
