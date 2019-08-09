eaf00f11162f54be6f92a4a46f21d79c06765676
Denis Golovin <dgolovin@users.noreply.github.com>
2019-08-08 17:32:41 -0700

    Update odo version to 1.0.0-beta4 (#1028)

--- src/openshift/cluster.ts
+++ src/openshift/cluster.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { OpenShiftObject, Command } from "../odo";
+import { Command } from "../odo";
 import { OpenShiftItem } from './openshiftItem';
 import { window, commands, env, QuickPickItem } from 'vscode';
 import { CliExitData, Cli } from "../cli";
