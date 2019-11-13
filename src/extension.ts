dc9b8fd8bb4913ef11919e76fb0b36d2cf5e46b1
Denis Golovin <dgolovin@redhat.com>
2019-11-12 19:01:59 -0800

    OpenShift: Create command implementation (1)
    Fix #1268.

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,7 @@ import { OdoImpl } from './odo';
 import { Build } from './k8s/build';
 import { DeploymentConfig } from './k8s/deployment';
 import { TokenStore } from './util/credentialManager';
+import * as Oc from './oc';
 
 let clusterExplorer: k8s.ClusterExplorerV1 | undefined = undefined;
 
@@ -35,6 +36,7 @@ export async function activate(context: vscode.ExtensionContext) {
     Cluster.extensionContext = Component.extensionContext = TokenStore.extensionContext = context;
     const disposable = [
         vscode.commands.registerCommand('openshift.about', (context) => execute(Cluster.about, context)),
+        vscode.commands.registerCommand('openshift.create', (context) => execute(Oc.create, context)),
         vscode.commands.registerCommand('openshift.output', (context) => execute(Cluster.showOpenShiftOutput, context)),
         vscode.commands.registerCommand('openshift.openshiftConsole', (context) => execute(Cluster.openshiftConsole, context)),
         vscode.commands.registerCommand('openshift.openshiftConsole.palette', (context) => execute(Cluster.openshiftConsole, context)),
