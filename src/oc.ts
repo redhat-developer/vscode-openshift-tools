c278d188fd9566fbc2ef3d8c5058f4a3f7074ccb
Denis Golovin <dgolovin@users.noreply.github.com>
2019-11-13 20:36:44 -0800

    OpenShift: Create command implementation (#1275)

---
+++ src/oc.ts
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import { window } from 'vscode';
+import { Cli } from './cli';
+import { ToolsConfig } from './tools';
+import { OpenShiftItem } from './openshift/openshiftItem';
+
+export class Oc {
+    public static async create() {
+        const document = window.activeTextEditor ? window.activeTextEditor.document : undefined;
+        const pleaseSave = 'Please save your changes before executing \'OpenShift: Create\' command.';
+        let message: string;
+
+        if (!document) {
+            message = '\'OpenShift: Create\' command requires .yaml or .json a file opened in editor.';
+        }
+
+        if (!message  && document.isUntitled) {
+            message = pleaseSave;
+        }
+
+        if (!message && document.isDirty) {
+            const save = 'Save';
+            const action = await window.showInformationMessage('Editor has unsaved changes', save);
+            if (action !== save) {
+                message = pleaseSave;
+            } else {
+                await document.save();
+            }
+        }
+
+        if (message) {
+            window.showWarningMessage(message);
+        } else {
+            const project = await OpenShiftItem.getOpenShiftCmdData(undefined, 'Select a Project where to create a new resource');
+            const toolLocation = await ToolsConfig.detectOrDownload('oc');
+            const result = await Cli.getInstance().execute(`${toolLocation} create -f ${document.fileName} --namespace ${project.getName()}`);
+            if (result.error) {
+                throw result.error;
+            } else {
+                return 'Resources were successfully created.';
+            }
+        }
+
+    }
+}
