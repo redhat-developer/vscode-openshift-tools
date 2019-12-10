5f823995a40b4e7250b94b7adb6401922dd8191c
Denis Golovin <dgolovin@redhat.com>
2019-12-09 22:50:22 -0800

    Add copyright message to unit-tests.ts
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- build/unit-tests.ts
+++ build/unit-tests.ts
@@ -1,5 +1,11 @@
-import * as path from 'path';
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
 
+'use strict';
+
+import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
