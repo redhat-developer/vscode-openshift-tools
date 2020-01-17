b4b8f7ce8d1a1b22444b96dad9c2072cfcb7a8fb
Denis Golovin <dgolovin@redhat.com>
2020-01-17 13:06:13 -0800

    Add eslint configuration and dependencies
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ header.js
@@ -0,0 +1,4 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
