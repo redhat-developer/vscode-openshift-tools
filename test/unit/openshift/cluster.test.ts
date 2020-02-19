2e3e36d5ef9c6d9a472fc2c53ecf2056a8b70c8b
Denis Golovin <dgolovin@redhat.com>
2020-02-18 22:28:39 -0800

    Fix compilation errors
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- test/unit/openshift/cluster.test.ts
+++ test/unit/openshift/cluster.test.ts
@@ -44,27 +44,6 @@ suite('Openshift/Cluster', () => {
         stderr: fatalErrorText,
         stdout: 'output'
     };
-    const routeObj = `{
-        "apiVersion": "v1",
-        "items": [
-            {
-                "apiVersion": "route.openshift.io/v1",
-                "kind": "Route",
-                "spec": {
-                    "host": "console-openshift-console.apps-crc.testing",
-                    "port": {
-                        "targetPort": "https"
-                    },
-                    "wildcardPolicy": "None"
-                }
-            }
-        ],
-        "kind": "List",
-        "metadata": {
-            "resourceVersion": "",
-            "selfLink": ""
-        }
-    }`;
     const testUrl = 'https://162.165.64.43:8443';
     const testUser = 'user';
     const password = 'password';
