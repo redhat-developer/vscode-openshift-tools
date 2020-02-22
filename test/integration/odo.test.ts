7d3cda8376a8a3e26f497724e6e243022fb5d93c
Denis Golovin <dgolovin@redhat.com>
2020-02-21 23:56:16 -0800

    Update unit-test.ts script to run unit and integration tests
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- test/integration/odo.test.ts
+++ test/integration/odo.test.ts
@@ -6,16 +6,29 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
 import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as path from 'path';
+import { Project } from '../../src/openshift/project';
 import * as odo from '../../src/odo';
 import { Cluster } from '../../src/openshift/cluster';
 
-suite('odo integration', () => {
+const {expect} = chai;
+chai.use(sinonChai);
+
+
+ // TODO integration tests should be running on multi-folder workspace,
+ // because when first and second workspace folders are added vscode
+ // reloads window and tests execution is going to be disrupted
+
+ suite('odo integration', () => {
     const oi: odo.Odo = odo.getInstance();
     let sb: sinon.SinonSandbox;
+    const testsRoot = path.resolve(__dirname, '..\\..\\..\\test')
 
     setup(() => {
+        // TODO generate multi-folder workspace in system temp folder before running tests
         sb = sinon.createSandbox();
-        sb.stub(vscode.window, 'showInformationMessage').resolves('Download and install');
     });
 
     teardown(() => {
@@ -37,6 +50,33 @@ suite('odo integration', () => {
         test('about()', () => {
             Cluster.about();
             assert.ok(true);
+        });
+    });
+
+    suite('create commands', ()=> {
+        test('create project', async () => {
+            const projectName = `projname${Math.round(Math.random()*10000)}`;
+            sb.stub(vscode.window, 'showInputBox').resolves(projectName);
+            await Project.create();
+            const projects = await oi.getProjects();
+            expect(projects.find((value) => value.getName() === projectName)).exist;
+        });
+
+        test('create git component', async () => {
+            const projects = await oi.getProjects();
+            const application = new odo.OpenShiftObjectImpl(projects[0], "app", odo.ContextType.APPLICATION, false, odo.OdoImpl.Instance, vscode.TreeItemCollapsibleState.Collapsed);
+            const compName = `compname${Math.round(Math.random()*10000)}`;
+            await odo.OdoImpl.Instance.createComponentFromGit(
+                application,
+                'nodejs',
+                'latest',
+                compName,
+                'https://github.com/dgolovin/nodejs-ex',
+                vscode.Uri.file(path.join(testsRoot, 'fixtures', 'workspace', 'project1')),
+                'master'
+            );
+            const comps = await oi.getComponents(application);
+            expect(comps.find((value) => value.getName() === compName)).exist;
         });
     });
 });
