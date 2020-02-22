fc46ed50fb381e2bb95f208dbf182d8b7f48fcbe
Denis Golovin <dgolovin@redhat.com>
2020-02-22 00:01:39 -0800

    Update unit-tests.ts to run unit or integration tests
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- build/run-tests.ts
+++ build/run-tests.ts
@@ -3,30 +3,18 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-/* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
 
 import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main(): Promise<void> {
-  try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-    // The path to the extension test runner script
-    // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, '../../out/test/unit/');
-
-    // Download VS Code, unzip it and run the integration test
+    const extensionTestsPath = path.resolve(
+        extensionDevelopmentPath, 'out', 'test', process.argv.slice(2)[0]);
     await runTests({
       extensionDevelopmentPath, extensionTestsPath
     });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
 }
 
 main();
