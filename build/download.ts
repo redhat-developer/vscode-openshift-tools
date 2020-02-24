a704648d71241926ba32d0b3267916516c96c867
Denis Golovin <dgolovin@redhat.com>
2020-02-24 14:11:03 -0800

    Format build scripts and add some doc comments
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- build/download.ts
+++ build/download.ts
@@ -1,4 +1,4 @@
-  /*-----------------------------------------------------------------------------------------------
+/*-----------------------------------------------------------------------------------------------
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
@@ -14,23 +14,29 @@ const pipeline = promisify(require('stream').pipeline);
 const got = require('got');
 
 export class DownloadUtil {
-
-  static async downloadFile(fromUrl: string, toFile: string, progressCb?: (current: number, increment: number) => void, throttle = 250): Promise<void> {
-    const dls = got.stream(fromUrl);
-    if (progressCb) {
-      let previous = 0;
-      // Process progress event from 'got'
-      fromEvent(dls, 'downloadProgress').pipe(throttleTime(throttle)).subscribe((progress: { percent: number }) => {
-          const current = Math.round(progress.percent * 100);
-          progressCb(current, current - previous);
-          previous = current;
-      });
-      // process end event from 'got'
-      fromEvent(dls, 'end').subscribe(() => {
-          progressCb(100, 100 - previous);
-      });
+    static async downloadFile(
+        fromUrl: string,
+        toFile: string,
+        progressCb?: (current: number, increment: number) => void,
+        throttle = 250,
+    ): Promise<void> {
+        const dls = got.stream(fromUrl);
+        if (progressCb) {
+            let previous = 0;
+            // Process progress event from 'got'
+            fromEvent(dls, 'downloadProgress')
+                .pipe(throttleTime(throttle))
+                .subscribe((progress: { percent: number }) => {
+                    const current = Math.round(progress.percent * 100);
+                    progressCb(current, current - previous);
+                    previous = current;
+                });
+            // process end event from 'got'
+            fromEvent(dls, 'end').subscribe(() => {
+                progressCb(100, 100 - previous);
+            });
+        }
+        // Pipe url to file
+        await pipeline(dls, fs.createWriteStream(toFile));
     }
-    // Pipe url to file
-    await pipeline(dls, fs.createWriteStream(toFile));
-  }
 }
--- build/run-tests.ts
+++ build/run-tests.ts
@@ -8,12 +8,21 @@
 import * as path from 'path';
 import { runTests } from 'vscode-test';
 
+/**
+ * Run mocha tests from project's tests folder. This script expects a subfolder name as a parameter to
+ * identify what kind of tests to run: unit, integration or ui.
+ */
 async function main(): Promise<void> {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
     const extensionTestsPath = path.resolve(
-        extensionDevelopmentPath, 'out', 'test', process.argv.slice(2)[0]);
+        extensionDevelopmentPath,
+        'out',
+        'test',
+        process.argv.slice(2)[0],
+    );
     await runTests({
-      extensionDevelopmentPath, extensionTestsPath
+        extensionDevelopmentPath,
+        extensionTestsPath,
     });
 }
 
