e52b9d0a45f6656091193caf4bff5fbc367333d8
Denis Golovin <dgolovin@redhat.com>
2020-03-04 22:55:35 -0800

    Simplify progress.ts execFunctionWithProgress()
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/util/progress.ts
+++ src/util/progress.ts
@@ -5,25 +5,18 @@
 
 import * as vscode from 'vscode';
 
-export interface Step {
-    command: string;
-    increment: number;
-    total?: number;
-}
-
 export class Progress {
-
-  static async execFunctionWithProgress(title: string, func: (progress: vscode.Progress<{increment: number; message: string}>) => Promise<any> ): Promise<string> {
-        return new Promise((resolve, reject) => {
-            vscode.window.withProgress({
-                    cancellable: false,
-                    location: vscode.ProgressLocation.Notification,
-                    title
-                },
-                async (progress: vscode.Progress<{increment: number; message: string}>) => {
-                    await func(progress).then(resolve).catch(reject);
-                }
-            );
-        });
+    static async execFunctionWithProgress(
+        title: string,
+        func: (progress: vscode.Progress<{ increment: number; message: string }>) => Promise<any>,
+    ): Promise<any> {
+        return vscode.window.withProgress(
+            {
+                cancellable: false,
+                location: vscode.ProgressLocation.Notification,
+                title,
+            },
+            func,
+        );
     }
 }
