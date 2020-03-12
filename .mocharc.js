a97470f05896044cc433ab61c57abb452d9f3e8e
Denis Golovin <dgolovin@redhat.com>
2020-03-12 14:07:38 -0700

    Add simple UI test to veryfy extension is successfuly activated
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  slow: 75,
+  timeout: 200000,
+};
---
+++ test/ui/activity.test.ts
@@ -0,0 +1,43 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import { QuickOpenBox, VSBrowser, WebDriver, ActivityBar, Workbench, BottomBarPanel, TerminalView, Condition, WebElement } from 'vscode-extension-tester';
+import * as chai from 'chai';
+import { create } from 'domain';
+
+const {expect} = chai;
+
+export async function wait(timeout = 2500): Promise<void> {
+    return new Promise((res) => setTimeout(res, timeout));
+}
+
+// Create a Mocha suite
+describe('My Test Suite', () => {
+  let browser: VSBrowser;
+  let driver: WebDriver
+  // initialize the browser and webdriver
+  before(() => {
+    browser = VSBrowser.instance;
+    driver = browser.driver;
+  });
+
+  // test whatever we want using webdriver, here we are just checking the page title
+  it('OpenShift views container is present', async () => {
+    const control = new ActivityBar().getViewControl('OpenShift');
+    await driver.getTitle();
+    // eslint-disable-next-line no-console
+    expect(control.getTitle()).equals('OpenShift');
+    const wb = new Workbench();
+    const terminalView: TerminalView = await new BottomBarPanel().openTerminalView();
+    wb.openCommandPrompt();
+    const input = await QuickOpenBox.create();
+    await input.setText('>OpenShift: About');
+    await input.selectQuickPick(0);
+    await input.confirm();
+    await wait(10000);
+    const text = await terminalView.getText();
+    expect(text).contains('odo v1.1.0');
+  });
+});
