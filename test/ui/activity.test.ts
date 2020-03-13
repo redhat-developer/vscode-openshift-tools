0d0c52dc69ace3e6b76812710eec8b575cdd9382
Denis Golovin <dgolovin@redhat.com>
2020-03-12 22:31:34 -0700

    Fix eslint errors
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- test/ui/activity.test.ts
+++ test/ui/activity.test.ts
@@ -4,9 +4,7 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import {
-    QuickOpenBox,
     VSBrowser,
-    WebDriver,
     ActivityBar,
     Workbench,
     BottomBarPanel,
