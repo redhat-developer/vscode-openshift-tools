85129a4705acdf1971c01b85c7e65edb6b889053
Denis Golovin <dgolovin@redhat.com>
2020-03-17 20:48:53 -0700

    Cleanup bundle-tools.ts
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- build/bundle-tools.ts
+++ build/bundle-tools.ts
@@ -16,10 +16,18 @@ import path = require('path');
 import fs = require('fs-extra');
 import configData = require('../src/tools.json');
 
-async function isDownloadRequired(filePath, sha256): Promise<boolean> {
+interface PlatformData {
+    url: string;
+    sha256sum:string;
+    dlFileName:string;
+    cmdFileName: string;
+    filePrefix: string;
+}
+
+async function isDownloadRequired(filePath: string, sha256: string): Promise<boolean> {
     let result = true;
     if (fs.existsSync(filePath)) {
-        const fileSha256 = await hasha.fromFile(filePath, {algorithm: 'sha256'});
+        const fileSha256 = await hasha.fromFile(filePath, { algorithm: 'sha256' });
         result = fileSha256 !== sha256;
     }
     return result;
@@ -28,51 +36,47 @@ async function isDownloadRequired(filePath, sha256): Promise<boolean> {
 async function downloadFileAndCreateSha256(
     toolsCacheFolder: string,
     toolsFolder: string,
-    dlFileName: string,
-    url: string,
-    sha256sum: string,
-    cmdFileName: string,
-    filePrefix?: string,
+    platform: PlatformData,
 ): Promise<void> {
     mkdirp.sync(toolsCacheFolder);
     mkdirp.sync(toolsFolder);
-    const currentFile = path.join(toolsCacheFolder, dlFileName);
-    if (await isDownloadRequired(currentFile, sha256sum)) {
-        console.log(`Downloading ${url} to ${currentFile}`);
-        await DownloadUtil.downloadFile(url, currentFile, (current) => console.log(`${current}%`));
+    const currentFile = path.join(toolsCacheFolder, platform.dlFileName);
+    if (await isDownloadRequired(currentFile, platform.sha256sum)) {
+        console.log(`Downloading ${platform.url} to ${currentFile}`);
+        await DownloadUtil.downloadFile(platform.url, currentFile, (current) =>
+            console.log(`${current}%`),
+        );
         const currentSHA256 = await hasha.fromFile(currentFile, { algorithm: 'sha256' });
-        if (currentSHA256 === sha256sum) {
+        if (currentSHA256 === platform.sha256sum) {
             console.log(`Download of ${currentFile} has finished and SHA256 is correct`);
         } else {
             throw Error(`${currentFile} is downloaded and SHA256 is not correct`);
         }
     } else {
-        console.log('Previously downloaded archive SHA256 is correct')
+        console.log('Previously downloaded archive SHA256 is correct');
     }
-    console.log(`Extracting ${currentFile} to ${path.join(toolsFolder, cmdFileName)}`);
-    await Archive.extract(currentFile, toolsFolder, cmdFileName, filePrefix);
+    console.log(`Extracting ${currentFile} to ${path.join(toolsFolder, platform.cmdFileName)}`);
+    await Archive.extract(currentFile, toolsFolder, platform.cmdFileName, platform.filePrefix);
 }
 
 async function bundleTools(): Promise<void> {
     const outFolder = path.resolve('.', 'out');
     const toolsCacheFolder = path.join(outFolder, 'tools-cache');
-    const currentPlatform = process.argv.find((arg) => arg === '--platform') ? process.platform : 'all';
+    const currentPlatform = process.argv.find((arg) => arg === '--platform')
+        ? process.platform
+        : 'all';
     console.log(currentPlatform);
     console.info(`Download tools to '${toolsCacheFolder}'`);
     for (const key in configData) {
         const tool = configData[key];
-        for (const OS in configData[key].platform) {
+        for (const OS in tool.platform) {
             if (currentPlatform === 'all' || OS === process.platform) {
                 console.log(`Bundle '${tool.description}' for ${OS}`);
                 // eslint-disable-next-line no-await-in-loop
                 await downloadFileAndCreateSha256(
                     toolsCacheFolder,
                     path.join(outFolder, 'tools', OS),
-                    configData[key].platform[OS].dlFileName,
-                    configData[key].platform[OS].url,
-                    configData[key].platform[OS].sha256sum,
-                    configData[key].platform[OS].cmdFileName,
-                    configData[key].platform[OS].filePrefix,
+                    tool.platform[OS],
                 );
             }
         }
