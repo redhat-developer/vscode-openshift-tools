00407a65d12c186fedc10d712fe747372b50338f
Denis Golovin <dgolovin@users.noreply.github.com>
2020-03-27 22:49:01 -0700

    Build component type and version request with single odo call (#1476)
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/openshift/catalog.ts
+++ src/openshift/catalog.ts
@@ -3,11 +3,13 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { Odo, OdoImpl, Command } from "../odo";
-import { Platform } from "../util/platform";
+import { Odo, OdoImpl, Command } from '../odo';
+import { Platform } from '../util/platform';
 
 export class Catalog {
     private static odo: Odo = OdoImpl.Instance;
+
+    private componentsJson: any[] = [];
 
     static listComponents(): void {
         Catalog.odo.executeInTerminal(Command.listCatalogComponents(), Platform.getUserHomePath());
@@ -15,5 +17,17 @@ export class Catalog {
 
     static listServices(): void {
         Catalog.odo.executeInTerminal(Command.listCatalogServices(), Platform.getUserHomePath());
+    }
+
+    async getComponentNames(): Promise<string[]> {
+        this.componentsJson = await Catalog.odo.getComponentTypesJson();
+        return this.componentsJson.map((value) => value.metadata.name);
+    }
+
+    getComponentVersions(componentName: string): string[] {
+        const component = this.componentsJson.find(
+            (value) => value.metadata.name === componentName,
+        );
+        return component ? component.spec.allTags : [];
     }
 }
