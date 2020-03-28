76d291b2db472b35a766b2fa3a4bcabffbd41a08
Denis Golovin <dgolovin@users.noreply.github.com>
2020-03-27 18:37:20 -0700

    Update smoke test suit test names (#1475)
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- test/ui/activity.test.ts
+++ test/ui/activity.test.ts
@@ -23,26 +23,33 @@ describe('OpenShift Connector Smoke Test', () => {
     let browser: VSBrowser;
 
     // initialize the browser and webdriver
-    before(() => {
+    before(async () => {
         browser = VSBrowser.instance;
+        await browser.waitForWorkbench();
     });
 
     // test whatever we want using webdriver, here we are just checking the page title
-    it('OpenShift Connector installed and activated', async () => {
-        await browser.waitForWorkbench();
+    it('OpenShift Connector installed and activated', ()=> {
         const control = new ActivityBar().getViewControl('OpenShift');
         // eslint-disable-next-line no-console
         expect(control.getTitle()).equals('OpenShift');
+    });
+
+    it('odo command is downloaded and functional', async () => {
         const wb = new Workbench();
         const terminalView: TerminalView = await new BottomBarPanel().openTerminalView();
-        wb.openCommandPrompt();
         wb.executeCommand('OpenShift: About');
         browser.driver.sleep(20000);
         const odoVersion = await terminalView.getText();
         expect(odoVersion).contains('odo v1.1.1');
+    })
+
+    it('oc command is downloaded and functional', async () => {
+        const wb = new Workbench();
+        const terminalView: TerminalView = await new BottomBarPanel().openTerminalView();
         wb.executeCommand('OpenShift: Print OKD Client Tool Version');
         browser.driver.sleep(20000);
         const ocVersion = await terminalView.getText();
         expect(ocVersion).contains('Client Version: 4.3.3');
-    });
+    })
 });
