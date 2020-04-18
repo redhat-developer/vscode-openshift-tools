c5d378221062588a842382cb8b413ed17c4751ee
Denis Golovin <dgolovin@redhat.com>
2020-04-18 02:35:43 -0700

    Fix errors in react based logViewer
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/view/log/app/spinner.tsx
+++ src/view/log/app/spinner.tsx
@@ -41,7 +41,7 @@ const useStyles = makeStyles((theme: Theme) =>
 const theme = createMuiTheme({
     typography: {
       h6: {
-        fontSize: 30,
+        fontSize: 12,
       },
       button: {
         fontSize: 12,
@@ -77,54 +77,39 @@ function stop() {
     vscode.postMessage({action: 'stop'});
 }
 
-export interface SpinnerProps {
-    context?: {
-        setFollow?: (set: boolean)=>void,
-        follow?: boolean
-    }
-}
-
 // Do not change any type, that would lead to props validation error
 // during the compile:views
-export default class Spinner extends React.Component<SpinnerProps> {
+export default function spinner(props: any): JSX.Element {
+    const [display ,setDisplay] = React.useState(true);
+    window.addEventListener('message', event => {
+        if (event.data.action === 'finished') {
+            setDisplay(false);
+        }
+    });
 
-    state = {
-        display: false
-    };
+    const classes = useStyles();
 
-    constructor(props: any) {
-        super(props);
-        window.addEventListener('message', event => {
-            if (event.data.action === 'finished') {
-                this.setState({display: false});
-            }
-        });
-    }
-    render () {
-        const classes = useStyles();
-        const { display }  = this.state;
-        return display ? (
-            <ThemeProvider theme={theme}>
-            <div className={classes.root}>
-                <AppBar position="static" style={{backgroundColor: '#1e1e1e'}}>
-                    <Toolbar variant="dense">
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <Loader type="Bars" color="#00BFFF" height={20} width={20} />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-                        Streaming Log
-                        </Typography>
-                        <FormGroup row>
-                            <FormControlLabel
-                                control={<AutoScrollSwitch onChange={(event)=>this.props.context.setFollow(event.target.checked)} size="small"/>}
-                                label={<Typography variant="h6" className={classes.title}>Auto Scrolling</Typography>}
-                            />
-                        </FormGroup>
-                        <Button color="inherit" className={classes.stopButton} onClick={stop}>Stop Streaming</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-            </ThemeProvider>
-        ) : null;
-    }
+    return display ? (
+        <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+            <AppBar position="static" style={{backgroundColor: '#1e1e1e'}}>
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <Loader type="Bars" color="#00BFFF" height={20} width={20} />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                    Streaming Log
+                    </Typography>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={<AutoScrollSwitch onChange={props.toggleAutoScroll} size="small"/>}
+                            label={<Typography variant="h6" className={classes.title}>Auto Scrolling</Typography>}
+                        />
+                    </FormGroup>
+                    <Button color="inherit" className={classes.stopButton} onClick={stop}>Stop Streaming</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+        </ThemeProvider>
+) : null;
 }
