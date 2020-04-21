7944cc9bf0220af57c0918df869e2d02d1b53f1e
Denis Golovin <dgolovin@redhat.com>
2020-04-20 21:52:54 -0700

    Fix unit test errors
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/view/log/LogViewLoader.ts
+++ src/view/log/LogViewLoader.ts
@@ -5,20 +5,48 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
+import { ExtenisonID } from '../../util/constants';
+import { OpenShiftObject } from '../../odo';
+import * as odo from '../../odo';
+import treeKill = require('tree-kill');
 
 export default class LogViewLoader {
-    static loadView(extensionPath: string, title: string, cmdText: string): vscode.WebviewPanel {
-        const localResourceRoot = vscode.Uri.file(path.join(extensionPath, 'out', 'logViewer'));
+
+    static get extensionPath() {
+        return vscode.extensions.getExtension(ExtenisonID).extensionPath
+    }
+
+    static async loadView(title: string, cmdFunction: (prj, app, comp) => string, target: OpenShiftObject): Promise<vscode.WebviewPanel> {
+        const localResourceRoot = vscode.Uri.file(path.join(LogViewLoader.extensionPath, 'out', 'logViewer'));
 
         const panel = vscode.window.createWebviewPanel('logView', title, vscode.ViewColumn.One, {
             enableScripts: true,
             localResourceRoots: [localResourceRoot],
             retainContextWhenHidden: true
         });
-        panel.iconPath = vscode.Uri.file(path.join(extensionPath, "images/context/cluster-node.png"));
+        panel.iconPath = vscode.Uri.file(path.join(LogViewLoader.extensionPath, "images/context/cluster-node.png"));
+
+        const cmd = cmdFunction(target.getParent().getParent().getName(), target.getParent().getName(), target.getName());
 
         // TODO: When webview is going to be ready?
-        panel.webview.html = LogViewLoader.getWebviewContent(extensionPath, cmdText);
+        panel.webview.html = LogViewLoader.getWebviewContent(LogViewLoader.extensionPath, cmd);
+
+        const process = await odo.getInstance().spawn(cmd, target.contextPath.fsPath);
+        process.stdout.on('data', (data) => {
+            panel.webview.postMessage({action: 'add', data: `${data}`.trim().split('\n')});
+        }).on('close', ()=>{
+            panel.webview.postMessage({action: 'finished'});
+        });
+        const recieveDisposable = panel.webview.onDidReceiveMessage((event) => {
+            if (event.action === 'stop') {
+                treeKill(process.pid);
+                recieveDisposable.dispose();
+            }
+        })
+        const disposable = panel.onDidDispose(()=> {
+            treeKill(process.pid);
+            disposable.dispose();
+        });
         return panel;
     }
 
