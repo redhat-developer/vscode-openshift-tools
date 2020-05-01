0e1381a9c090080780852988d7f55d300837791f
Denis Golovin <dgolovin@redhat.com>
2020-04-30 17:20:15 -0700

    Clean test code

--- test/vsix-test/smoke.test.ts
+++ test/vsix-test/smoke.test.ts
@@ -3,19 +3,9 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import * as sinon from 'sinon';
-import { commands, extensions, window } from 'vscode';
+import { commands, extensions } from 'vscode';
 
 suite('vsix install tests', () => {
-    let sb: sinon.SinonSandbox;
-
-    setup(() => {
-        sb = sinon.createSandbox();
-    });
-
-    teardown(() => {
-        sb.restore();
-    });
 
     test('activate extension', async () => {
         return commands.executeCommand('openshift.about');
@@ -23,8 +13,8 @@ suite('vsix install tests', () => {
 
     test('verify tools', async () => {
         const paths = await extensions.getExtension('redhat.vscode-openshift-connector').exports.verifyBundledBinaries();
-        window.showInformationMessage
         if (!paths.odoPath) throw Error('odo binary is missing');
         if (!paths.ocPath) throw Error('oc binary is missing');
     });
+
 });
