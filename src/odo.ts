3743e82384700efbe6ef45932ac28c15419eb527
Denis Golovin <dgolovin@redhat.com>
2020-05-05 23:21:27 -0700

    Filter out routes with undefined names
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/odo.ts
+++ src/odo.ts
@@ -855,7 +855,9 @@ export class OdoImpl implements Odo {
 
     public async _getRoutes(component: OpenShiftObject): Promise<OpenShiftObject[]> {
         const result: cliInstance.CliExitData = await this.execute(Command.getComponentUrl(), component.contextPath ? component.contextPath.fsPath : Platform.getUserHomePath(), false);
-        return this.loadItems(result).map((value) => new OpenShiftObjectImpl(component, value.metadata.name, ContextType.COMPONENT_ROUTE, false, OdoImpl.instance, TreeItemCollapsibleState.None));
+        return this.loadItems(result)
+            .filter((value)=> !!value.metadata.name)
+            .map((value) => new OpenShiftObjectImpl(component, value.metadata.name, ContextType.COMPONENT_ROUTE, false, OdoImpl.instance, TreeItemCollapsibleState.None));
     }
 
     async getStorageNames(component: OpenShiftObject): Promise<OpenShiftObject[]> {
