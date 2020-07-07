0e5aaf403c5cca1ebe38a5cf3cb0289a1dc88985
Denis Golovin <dgolovin@redhat.com>
2020-07-07 15:13:13 -0700

    cleanup eslint warnings
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ src/odo/builderImage.ts
@@ -0,0 +1,9 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+ export interface BuilderImage {
+    readonly name: string;
+    readonly tag: string;
+}
