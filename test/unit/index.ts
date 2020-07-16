d506ed64e28100b0a5dc96ad47989696918f24da
Denis Golovin <dgolovin@redhat.com>
2020-07-15 17:16:53 -0700

    nyc based code coverage reporting for unit tests
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- test/unit/index.ts
+++ test/unit/index.ts
@@ -3,27 +3,12 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 /* tslint:disable no-require-imports */
-import * as fs from 'fs';
 import * as glob from 'glob';
 import * as paths from 'path';
-import { TestRunnerOptions, CoverageRunner } from '../coverage';
 
 require('source-map-support').install();
 
 import Mocha = require('mocha');
-
-// declare var global: any;
-
-// Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
-// Since we are not running in a tty environment, we just implement the method statically
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const tty = require('tty');
-
-if (!tty.getWindowSize) {
-    tty.getWindowSize = (): number[] => {
-        return [80, 75];
-    };
-}
 
 const config: any = {
     reporter: 'mocha-jenkins-reporter',
@@ -39,20 +24,9 @@ if (process.env.BUILD_ID && process.env.BUILD_NUMBER) {
 
 const mocha = new Mocha(config);
 
-function loadCoverageRunner(testsRoot: string): CoverageRunner | undefined {
-    let coverageRunner: CoverageRunner;
-    const coverConfigPath = paths.join(testsRoot, '..', '..', '..', 'coverconfig.json');
-    if (!process.env.OST_DISABLE_COVERAGE && fs.existsSync(coverConfigPath)) {
-        coverageRunner = new CoverageRunner(JSON.parse(fs.readFileSync(coverConfigPath, 'utf-8')) as TestRunnerOptions, testsRoot);
-        coverageRunner.setupCoverage();
-    }
-    return coverageRunner;
-}
-
 export function run(): any {
      return new Promise((resolve, reject) => {
         const testsRoot = paths.resolve(__dirname);
-        const coverageRunner = loadCoverageRunner(testsRoot);
         glob('**/**.test.js', { cwd: testsRoot }, (error, files): any => {
             if (error) {
                 reject(error);
@@ -64,7 +38,7 @@ export function run(): any {
                     } else {
                         resolve();
                     }
-                }).on('end', () => coverageRunner && coverageRunner.reportCoverage());
+                });
             }
         });
     });
