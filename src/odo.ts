846c4c5f6b6682fd83b0a472e579a6b2d02f2673
Denis Golovin <dgolovin@redhat.com>
2020-09-24 23:56:22 -0700

    Implement port number input field for devfile component urls
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/odo.ts
+++ src/odo.ts
@@ -45,6 +45,7 @@ export interface OpenShiftObject extends QuickPickItem {
     path?: string;
     builderImage?: BuilderImage;
     iconPath?: Uri;
+    kind?: ComponentKind;
 }
 
 export enum ContextType {
