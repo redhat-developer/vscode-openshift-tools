bae6efa7632d4a228bac9fa56f39fadbe7065eba
Denis Golovin <dgolovin@redhat.com>
2020-12-16 10:52:41 -0800

    Update integrated terminal shellPath for windows only
    This fix changes the logic slightly. It lets vscode to decide
    what shell to use on linux and does not set it explicitly like
    it was done before. For windows it always changes shellPath to
    cmd to avoid handling powerShell command execution differences.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/util/windowUtils.ts
+++ src/util/windowUtils.ts
@@ -9,9 +9,11 @@ export class WindowUtil {
     static createTerminal(name: string, cwd?: string): Terminal {
         const options: TerminalOptions = {
             cwd,
-            name,
-            shellPath: process.platform === 'win32' ? process.env.ComSpec : '/bin/bash',
+            name
         };
+        if (process.platform === 'win32') {
+            options.shellPath = process.env.ComSpec;
+        }
         return window.createTerminal(options);
     }
 }
