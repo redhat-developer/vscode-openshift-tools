07d2de69be7ffa5b6bb37982a7146a3e2bfd89fa
Denis Golovin <dgolovin@redhat.com>
2021-01-19 21:49:54 -0800

    Move out styles to .style.tsx files
    This PR fixes #1902.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/view/cluster/app/cluster.tsx
+++ src/view/cluster/app/cluster.tsx
@@ -4,7 +4,7 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import * as React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Button,
   Card,
@@ -17,70 +17,11 @@ import {
   Typography} from '@material-ui/core';
 
 import AddClusterView from './clusterView';
+import clusterStyle from './cluster.style';
+
 const imageSrc = require('./images/logo.png');
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-      fontSize: '1.25em'
-    },
-    iconContainer: {
-      height: 60,
-      marginBottom: '3em',
-      marginTop: '2em'
-    },
-    textWhite: {
-      marginBottom: '20px!important',
-      textAlign: 'left'
-    },
-    App: {
-      textAlign: 'center'
-    },
-    rowBody: {
-      padding: '0 10em 0 10em'
-    },
-    cardContainer: {
-      display: 'flex',
-      justifyContent: 'center'
-    },
-    cardTransform: {
-      width: '27em',
-      marginRight: theme.spacing(4),
-      position: 'relative',
-      transform: 'scale(0.95)',
-      '&:hover': {
-        transform: 'scale(1)',
-        boxShadow: '5px 20px 30px rgba(0,0,0,0.2)'
-      }
-    },
-    cardHeader: {
-      backgroundColor: '#00586d!important',
-      padding: theme.spacing(2),
-      borderBottom: '0 solid transparent'
-    },
-    cardButton: {
-      display: 'block',
-      marginBottom: theme.spacing(2)
-    },
-    button: {
-      color: 'white',
-      backgroundColor: '#EE0000',
-      '&:hover': {
-        backgroundColor: '#BE0000',
-      },
-      textTransform: "none"
-    },
-    cardContent: {
-      background: 'var(--vscode-list-inactiveSelectionBackground)',
-      border: '1px solid var(--vscode-list-inactiveSelectionBackground)',
-      color: 'var(--vscode-foreground)'
-    }
-  }),
-);
+const useStyles = makeStyles(clusterStyle);
 
 const clusterTypes = [
   {
--- src/view/cluster/app/clusterView.tsx
+++ src/view/cluster/app/clusterView.tsx
@@ -4,7 +4,7 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import * as React from 'react';
-import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
 import { InsertDriveFile, GetApp, VpnKey } from '@material-ui/icons';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -37,186 +37,11 @@ import {
   Tooltip,
   Typography } from '@material-ui/core';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      textAlign: 'left',
-      '& .MuiPaper-root': {
-        backgroundColor: 'var(--vscode-editor-background)',
-        color: 'var(--vscode-foreground)'
-      },
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-      '& .MuiStepContent-root': {
-        paddingLeft: theme.spacing(5)
-      },
-      '& .MuiStepper-root': {
-        marginLeft: theme.spacing(4),
-        backgroundColor: 'var(--vscode-editor-background)'
-      },
-      '& .MuiButton-root': {
-        textTransform: 'none'
-      },
-      '& .MuiFormLabel-root': {
-        color: 'var(--vscode-input-foreground)'
-      },
-      '& .MuiStepIcon-root.MuiStepIcon-active': {
-        color: '#0066CC'
-      },
-      '& .MuiStepIcon-root.MuiStepIcon-completed': {
-        color: '#0066CC'
-      },
-      '& .MuiButton-containedPrimary': {
-        backgroundColor: '#0066CC'
-      },
-      '& .MuiStepLabel-iconContainer': {
-        paddingRight: theme.spacing(2)
-      },
-      '& .MuiStepLabel-label.MuiStepLabel-active': {
-        color: 'var(--vscode-foreground)'
-      },
-      '& .MuiStepLabel-label': {
-        color: 'var(--vscode-foreground)'
-      },
-      '& .MuiTypography-colorTextSecondary': {
-        color: 'var(--vscode-foreground)'
-      },
-      '& .MuiButton-root.Mui-disabled': {
-        color: 'var(--vscode-button-secondaryForeground)'
-      },
-      '& .MuiBadge-anchorOriginTopLeftCircle' : {
-        top: '46%',
-        left: '40%'
-      },
-      '& .MuiInputBase-root' :{
-        color: 'var(--vscode-input-foreground)'
-      },
-      '& .MuiFormHelperText-root' : {
-        color: 'var(--vscode-input-foreground)'
-      }
-    },
-    button: {
-      marginTop: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      backgroundColor: 'var(--vscode-button-background)',
-      color: 'var(--vscode-button-foreground)',
-      '&:hover' :{
-        backgroundColor: 'var(--vscode-button-hoverBackground)',
-        cursor: 'pointer'
-      },
-      '&:focus': {
-        backgroundColor: 'var(--vscode-button-hoverBackground)',
-        cursor: 'pointer'
-      },
-      '&:disabled' :{
-        opacity: 0.4
-      }
-    },
-    buttonSecondary: {
-      marginTop: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      backgroundColor: 'var(--vscode-button-secondaryBackground)',
-      color: 'var(--vscode-button-secondaryForeground)',
-      '&:hover' :{
-        backgroundColor: 'var(--vscode-button-secondaryHoverBackground)',
-        cursor: 'pointer'
-      },
-      '&:focus': {
-        backgroundColor: 'var(--vscode-button-secondaryHoverBackground)',
-        cursor: 'pointer'
-      },
-      '&:disabled' :{
-        opacity: 0.4
-      }
-    },
-    actionsContainer: {
-      marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(2)
-    },
-    resetContainer: {
-      padding: theme.spacing(3),
-    },
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-      width: '40%'
-    },
-    uploadLabel: {
-      marginTop: theme.spacing(2)
-    },
-    textContainer: {
-      color: 'var(--vscode-input-foreground)',
-      fontFamily: 'var(--vscode-editor-font-family)'
-    },
-    icon: {
-      verticalAlign: "bottom",
-      height: 20,
-      width: 20
-    },
-    details: {
-      alignItems: "center"
-    },
-    column: {
-      flexBasis: "40%"
-    },
-    helper: {
-      borderLeft: `2px solid ${theme.palette.divider}`,
-      padding: theme.spacing(1, 1)
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15)
-    },
-    margin: {
-      margin: theme.spacing(1)
-    },
-    loadingStatusText : {
-      marginBottom: 10,
-      padding: 20,
-      textAlign: 'center'
-    },
-    blockquoteText : {
-      display: 'block',
-      flexDirection: 'column',
-      margin: '15px 0',
-      padding: '8px 12px',
-      borderLeftWidth: '5',
-      borderLeftStyle: 'solid',
-      background: 'var(--vscode-textBlockQuote-background)',
-      borderColor: 'var(--vscode-textBlockQuote-border)'
-    }
-  })
-);
+import * as ClusterViewStyles from './clusterView.style';
 
-const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: '$ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
-}))(Badge);
+const useStyles = makeStyles(ClusterViewStyles.useStyles);
+const StyledBadge = withStyles(ClusterViewStyles.badgeStyles)(Badge);
+
 declare global {
   interface Window {
       acquireVsCodeApi(): any;
