902f5080e977c90b31f24aa7d949a0107d93cad8
Denis Golovin <dgolovin@redhat.com>
2021-02-02 11:33:14 -0800

    Update redhat-telemetry to 0.0.11
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -23,8 +23,7 @@ export function createTrackingEvent(name: string, properties: any = {}): Telemet
 export default async function sendTelemetry(actionName: string, properties?: any): Promise<void> {
     const service = await getTelemetryServiceInstance();
     if (actionName === 'activation') {
-        service?.sendStartupEvent();
-    } else {
-        service?.send(createTrackingEvent(actionName, properties));
+        return service?.sendStartupEvent();
     }
+    return service?.send(createTrackingEvent(actionName, properties));
 }
