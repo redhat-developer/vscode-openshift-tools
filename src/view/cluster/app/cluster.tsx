878d83b91eb5ca7b0702d232d9008afe6254e60f
Mohit Suman <mohit.skn@gmail.com>
2021-02-05 19:08:39 +0530

    add devsandbox card
    Signed-off-by: Mohit Suman <mohit.skn@gmail.com>

--- src/view/cluster/app/cluster.tsx
+++ src/view/cluster/app/cluster.tsx
@@ -15,7 +15,7 @@ import {
   ListItemText,
   Tooltip,
   Typography} from '@material-ui/core';
-  
+
 import AddClusterView from './clusterView';
 import clusterStyle from './cluster.style';
 
@@ -25,20 +25,30 @@ const clusterTypes = [
   {
     heading: "Deploy it locally on your laptop",
     description: "Install on Laptop: Red Hat CodeReady Containers.",
-    smallInfo: "A minimal, preconfigured Red Hat OpenShift 4 cluster on your laptop or desktop for development and testing.",
+    smallInfo: "Create a minimal OpenShift 4 cluster on your desktop/laptop for local development and testing.",
     imageUrl: ["https://www.openshift.com/hubfs/images/icons/Icon-Red_Hat-Hardware-Laptop-A-Black-RGB.svg"],
-    urlAlt: "Red Hat OpenShift",
+    urlAlt: "crc",
     redirectLink: "",
     buttonText: "Create/Refresh Cluster",
     tooltip: "You can create OpenShift cluster using this wizard."
   },
   {
+    heading: "Launch Developer Sandbox",
+    description: "Free access to the Developer Sandbox for Red Hat OpenShift",
+    smallInfo: "The sandbox provides you with a private OpenShift environment in a shared, multi-tenant OpenShift cluster that is pre-configured with a set of developer tools.",
+    imageUrl: ["https://assets.openshift.com/hubfs/images/logos/osh/Logo-Red_Hat-OpenShift-A-Standard-RGB.svg"],
+    urlAlt: "dev sandbox",
+    redirectLink: "https://developers.redhat.com/developer-sandbox",
+    buttonText: "Start your OpenShift experience",
+    tooltip: "Launch your Developer Sandbox for Red Hat OpenShift"
+  },
+  {
     heading: "Deploy it in your public cloud",
-    description: "Install Red Hat OpenShift 4 in your account with a supported public cloud providers.",
+    description: "Install Red Hat OpenShift 4 in your account with supported public cloud providers.",
     smallInfo: "This includes Amazon Web Services (AWS), Microsoft Azure, and Google Cloud.",
     imageUrl: ["https://www.openshift.com/hubfs/images/logos/logo_aws.svg", "https://www.openshift.com/hubfs/images/logos/logo-try-cloud.svg", "https://www.openshift.com/hubfs/images/logos/logo_google_cloud.svg"],
-    urlAlt: "Red Hat OpenShift 4",
-    redirectLink: "https://cloud.redhat.com/openshift/install#public-cloud",
+    urlAlt: "public cloud",
+    redirectLink: "https://cloud.redhat.com/openshift/create",
     buttonText: "Try it in your cloud",
     tooltip: "For complete installation, follow the official documentation."
   }
@@ -65,7 +75,7 @@ export default function Header() {
             </Typography>
           </div>
           <CardContent style= {{ height: 240 }}>
-            <Typography style={{ padding: '10px' }}>
+            <Typography style={{ padding: '10px', height: '50px' }}>
               {list.imageUrl.map((url: string, index: string | number) => (
                 <img src={url} key={index} style={{ marginLeft: '.625rem', marginRight: '.625rem' }}></img>
               ))}
@@ -107,7 +117,7 @@ export default function Header() {
       {showWizard && (<div className={classes.rowBody}>
         <Card className={classes.cardContent}>
           <Typography variant="body2" component="p" style={{ padding: 20 }}>
-            Red Hat CodeReady Containers brings a minimal OpenShift 4.0 or newer cluster to your local laptop or desktop computer.<br></br>You can use this wizard to create OpenShift clusters locally. Clusters take approximately 15 minutes to provision.
+            Red Hat CodeReady Containers brings a minimal OpenShift 4 cluster on your laptop or desktop computer.<br></br>You can use this wizard to create OpenShift cluster locally. Cluster take approximately 15 minutes to provision.
           </Typography>
           <AddClusterView />
         </Card>
