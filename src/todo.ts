bbcdedee2b75eafb4e7e4945cc5eee2ef59d6700
Denis Golovin <dgolovin@redhat.com>
2021-02-19 00:50:33 -0800

    Add todo item about projects declared in component env.yaml
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/todo.ts
+++ src/todo.ts
@@ -4,3 +4,9 @@
  *-----------------------------------------------------------------------------------------------*/
 
 // TODO: Fix workflow for creating components based on git repository
+
+// TODO: Find the way to show components from workspace. It should include components from all
+// existing and not existing projects. Several options are:
+// * Workspace Components View
+// * Go back to show multiple projects in Application View including projects that don't exist
+//   in cluster, but only in odo env.yaml
