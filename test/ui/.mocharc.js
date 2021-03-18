62f37e6b3296aec53963a40ed80eace4c38f4c51
Jan Richter <jrichter@redhat.com>
2021-03-18 11:21:07 +0100

    Add a UI test smoke suite

---
+++ test/ui/.mocharc.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    reporter: process.env.JUNIT_REPORT_PATH ? 'mocha-jenkins-reporter' : 'spec'
+};
---
+++ test/ui/suite/extension.ts
@@ -0,0 +1,38 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import { expect } from 'chai';
+import { ActivityBar, ExtensionsViewItem, ExtensionsViewSection, SideBarView } from 'vscode-extension-tester';
+import * as pjson from '../../../package.json';
+import { VIEWS } from '../common/constants';
+
+export function checkExtension() {
+    describe('Extensions view check', () => {
+        let extView: ExtensionsViewSection;
+        let item: ExtensionsViewItem;
+
+        before(async function () {
+            this.timeout(15000);
+            const btn = await new ActivityBar().getViewControl(VIEWS.extensions);
+            await btn.openView();
+            extView = await new SideBarView().getContent().getSection(VIEWS.installed) as ExtensionsViewSection;
+            item = await extView.findItem(`@installed ${pjson.displayName}`);
+        });
+
+        it('Openshift connector is installed', () => {
+            expect(item).not.undefined;
+        });
+
+        it('Openshift connector has the correct attributes', async () => {
+            const version = await item.getVersion();
+            const author = await item.getAuthor();
+            const desc = await item.getDescription();
+
+            expect(version).equals(pjson.version);
+            expect(author).equals(pjson.author);
+            expect(desc).equals(pjson.description);
+        });
+    });
+}
