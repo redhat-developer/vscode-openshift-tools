34389cab2d9131c303125c7f136ed1056ba10e4b
Denis Golovin <dgolovin@redhat.com>
2021-05-26 15:17:03 -0700

    Get signup status
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -4,14 +4,14 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import got from 'got/dist/source';
-import { authentication, window } from 'vscode';
+import { authentication, AuthenticationSession, window } from 'vscode';
 import { vsCommand } from '../vscommand';
 
 export class Auth {
 
     @vsCommand('openshift.sandbox.login')
     public static async login(): Promise<any> {
-        const session = await authentication.getSession('redhat-account-auth', ['openid'], { createIfNone: true });
+        const session: AuthenticationSession = await authentication.getSession('redhat-account-auth', ['openid'], { createIfNone: true });
         if (!session) {
             window.showWarningMessage('You need to log into Red Hat first!');
             return [];
@@ -19,9 +19,9 @@ export class Auth {
         console.log(session);
         const data = await got.get('https://registration-service-toolchain-host-operator.apps.sandbox.x8i5.p1.openshiftapps.com/api/v1/signup', {
             headers: {
-                Authorization: `Bearer ${session.accessToken}`
+                Authorization: `Bearer ${(session as any).idToken}`
             }
         });
-        console.log(data);
+        console.log(data.body);
     }
 }
