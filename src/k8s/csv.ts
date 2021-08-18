22546d24fc81204c65e003f8545a0dfc7f9e2da2
Denis Golovin <dgolovin@redhat.com>
2021-08-17 23:40:38 -0700

    Form to create operator backed service
    This PR fixes #1787.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/k8s/csv.ts
+++ src/k8s/csv.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
+import * as _ from 'lodash';
 import OpenShiftItem from '../openshift/openshiftItem';
 import { ClusterExplorerV1 } from 'vscode-kubernetes-tools-api';
 import * as common from './common';
@@ -10,6 +11,7 @@ import { ClusterServiceVersionKind, CRDDescription, CustomResourceDefinitionKind
 import { TreeItem } from 'vscode';
 import { vsCommand } from '../vscommand';
 import CreateServiceViewLoader from '../webview/create-service/createServiceViewLoader';
+import { DEFAULT_K8S_SCHEMA, getUISchema } from './utils';
 
 class CsvNode implements ClusterExplorerV1.Node, ClusterExplorerV1.ClusterExplorerExtensionNode {
 
@@ -60,9 +62,22 @@ export class ClusterServiceVersion extends OpenShiftItem {
 
     @vsCommand('clusters.openshift.csv.create')
     static async createNewService(context: any): Promise<void> {
+        const crdDescription = context.impl.crdDescription;
         const getCrdCmd = ClusterServiceVersion.command.getCrd(context.impl.crdDescription.name);
         const result: CustomResourceDefinitionKind = await common.asJson(getCrdCmd);
+
         const panel = await CreateServiceViewLoader.loadView('Create Service');
-        panel.webview.postMessage({action: 'schema', schema: JSON.stringify(result.spec.versions[0].schema.openAPIV3Schema)})
+        const openAPIV3SchemaAll = result.spec.versions[0].schema.openAPIV3Schema;
+        const openAPIV3Schema = _.defaultsDeep({}, DEFAULT_K8S_SCHEMA, _.omit(openAPIV3SchemaAll, 'properties.status'));
+
+        const uiSchema = getUISchema(
+            openAPIV3Schema,
+            crdDescription
+        );
+        panel.webview.onDidReceiveMessage(async ()=> {
+            await panel.webview.postMessage(
+                {action: 'load', openAPIV3Schema, uiSchema, crdDescription}
+            );
+        });
     }
 }
