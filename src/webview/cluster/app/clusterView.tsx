9122b5fced82baf62e1561f65e3c238d0293fa3c
Mohit Suman <mohit.skn@gmail.com>
2021-10-30 05:33:12 +0530

    fix macos and windows path for crc 1.34.0
    Signed-off-by: Mohit Suman <mohit.skn@gmail.com>

--- src/webview/cluster/app/clusterView.tsx
+++ src/webview/cluster/app/clusterView.tsx
@@ -269,8 +269,8 @@ export default function addClusterView(props) {
   const fetchDownloadBinary = () => {
     const platform = (window as any).platform;
     let crcBundle = '';
-    if (platform === 'darwin') crcBundle = 'crc-macos-amd64.tar.xz';
-    if (platform === 'win32') crcBundle = 'crc-windows-amd64.zip';
+    if (platform === 'darwin') crcBundle = 'crc-macos-amd64.pkg';
+    if (platform === 'win32') crcBundle = '	crc-windows-installer.zip';
     if (platform === 'linux') crcBundle = 'crc-linux-amd64.tar.xz';
     return `${crcDefaults.DefaultCrcUrlBase}/${crcLatest}/${crcBundle}`;
   }
