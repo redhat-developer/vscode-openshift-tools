2b37d7f187d812b5ae153e69eaa700e2a6ec2eb7
Denis Golovin <dgolovin@redhat.com>
2021-11-20 00:14:54 -0800

    Use fetch with caching to download swagger dfinition
    This PR fixes #2297.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -4,9 +4,14 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import { JSONSchema7 } from 'json-schema';
+import { Platform } from './platform';
+import * as path from 'path';
 
-const request = require('request');
 const _ = require('lodash');
+const https = require('https');
+const fetch = require('make-fetch-happen').defaults({
+    cachePath : path.join(Platform.getUserHomePath(), '.vs-openshift','cache'),
+});
 
 export type SwaggerDefinition = {
     definitions?: SwaggerDefinitions;
@@ -34,29 +39,20 @@ export function getDefinitionKey (ocrdKind: string, ocrdVersion: string, definit
   };
 
 export function getOpenAPISchemaFor(clusterUrl: string, token: string, kind: string, version: string): Promise<JSONSchema7> {
-    return new Promise((resolve, reject) => {
-        request(`${clusterUrl}/openapi/v2`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                rejectUnauthorized: false,
+    return fetch(`${clusterUrl}/openapi/v2`, {
+            headers: {
+                Authorization: `Bearer ${token}`
             },
-            (err, response, body) =>{
-                if (err) {
-                    reject(err);
-                } else {
-                    try {
-                        const swagger = JSON.parse(body);
-                        const key = getDefinitionKey(kind, version, swagger.definitions);
-                        const result = swagger.definitions[key];
-                        delete swagger.definitions[key];
-                        result.definitions = swagger.definitions;
-                        resolve(result);
-                    } catch (parsingError) {
-                        reject(parsingError);
-                    }
-                }
-            }
-        )
-    });
+            agent: new https.Agent({
+                rejectUnauthorized: false,
+            })
+        }).then(res => {
+            return res.json();
+        }).then(swagger => {
+            const key = getDefinitionKey(kind, version, swagger.definitions);
+            const result = swagger.definitions[key];
+            delete swagger.definitions[key]; // delete requested schema from definition to avoid cycles
+            result.definitions = swagger.definitions; // required to resolve oopenapischema references
+            return result;
+        });
 }
