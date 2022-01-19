6d408a328f275a610acec2eade14d2afbe3dd9ca
Denis Golovin <dgolovin@redhat.com>
2022-01-18 22:18:25 -0800

    Add sandbox api http requests
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/openshift/sandbox.ts
+++ src/openshift/sandbox.ts
@@ -4,8 +4,14 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import { workspace } from 'vscode';
+import fetch = require('make-fetch-happen');
 
- export interface SBStatus {
+export enum SBAPIEndpoint {
+    SIGNUP = '/api/v1/signup',
+    VERIFICATION = '/api/v1/signup/verification'
+}
+
+export interface SBStatus {
      ready: boolean;
      reason: 'Provisioned' | 'PendingApproval';
      verificationRequired: boolean;
@@ -28,11 +34,63 @@ export function getSandboxAPIUrl(): string {
     return workspace.getConfiguration('openshiftConnector').get('sandboxAPIHostUrl');
 }
 
-
-
 export interface SandboxAPI {
-    signup(): SBSignupResponse;
+    getSignUpStatus(): SBSignupResponse;
+    signUp(): Promise<boolean>;
     requestVerificationCode(areaCode: string, phoneNumber: string);
     validateVerificationCode(code: string);
 }
 
+export async function getSignUpStatus(token: string): Promise<SBSignupResponse | undefined> {
+    const signupResponse = await fetch(`${getSandboxAPIUrl()}${SBAPIEndpoint.SIGNUP}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            timeout: 10000
+        });
+    return signupResponse.ok ? signupResponse.json() as Promise<SBSignupResponse> : undefined;
+}
+
+export async function signUp(token: string): Promise<boolean> {
+    const signupResponse = await fetch(`${getSandboxAPIUrl()}${SBAPIEndpoint.SIGNUP}`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            timeout: 10000
+        });
+    return signupResponse.ok;
+}
+
+export async function requestVerificationCode(token: string, countryCode: string, phoneNumber: string): Promise<boolean> {
+    const verificationCodeRequestResponse = await fetch(`${getSandboxAPIUrl()}${SBAPIEndpoint.VERIFICATION}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        timeout: 10000,
+        body: JSON.stringify({
+            'country_code': countryCode,
+            'phone_number': phoneNumber
+        })
+    });
+
+    return verificationCodeRequestResponse.ok;
+}
+
+export async function validateVerificationCode(token: string, code: string): Promise<boolean> {
+    const validationRequestResponse = await fetch(`${getSandboxAPIUrl()}${SBAPIEndpoint.VERIFICATION}/${code}`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        timeout: 10000
+    });
+
+    return validationRequestResponse.ok;
+}
+
+// export function createSandboxAPI(): SandboxAPI {
+//     return
+// }
