10ffcb7071e98c132d820a0cea9e47e1d8e37402
Denis Golovin <dgolovin@redhat.com>
2022-03-08 23:23:02 -0800

    Add script for screeencast ot animated gif conversion
    Signed-off-by: Denis Golovin dgolovin@redhat.com

---
+++ build/compress.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+palette="/tmp/palette.png"
+filters="fps=10,scale=830:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen=stats_mode=diff" -y $palette
+ffmpeg -i $1 -i $palette -lavfi "$filters,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $2
