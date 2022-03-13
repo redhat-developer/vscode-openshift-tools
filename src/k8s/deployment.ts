8395ac614967fb4808e8f3c19c0eb0136986dc6c
Denis Golovin <dgolovin@redhat.com>
2022-03-12 23:52:06 -0800

    Remove unused code and add TODO comment for deployment
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/k8s/deployment.ts
+++ src/k8s/deployment.ts
@@ -10,6 +10,13 @@ export class Deployment {
 
         delete(name: string): CommandText {
             // oc delete all -l "app.kubernetes.io/component=nodejs-basic" --cascade=true
+            // it deletes everything related to component created as an example in dev console
+            // except webhooks secrets declared in deployment descriptor. Not all the hooks
+            // exist at the moment of deletion. In addition tools should get the list of
+            // the hooks and try to delete them without failing if some of them are not
+            // present in namespace
+            // TODO: Parse the deployment description, get the hooks and delete them
+            // ignoring the possible errors
             return new CommandText('oc delete',
                 'all', [
                     new CommandOption('-l', `app.kubernetes.io/component=${name}`, true, true),
