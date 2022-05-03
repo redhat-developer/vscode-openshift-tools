d10d29a974cfa5f3f9b285aacb8ac7f8837c1757
Denis Golovin <dgolovin@redhat.com>
2022-05-02 17:39:26 -0700

    Restructure test/ui folder in preparation for more UI tests
    This PR related to #2406.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

---
+++ test/ui/smoke/common/constants.ts
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+export const VIEWS = {
+    extensions: 'Extensions',
+    installed: 'Installed',
+    openshift: 'OpenShift',
+    appExplorer: 'Application Explorer',
+    components: 'Components',
+    compTypes: 'Component Types',
+    watchSessions: 'Watch Sessions',
+    debugSessions: 'Debug Sessions',
+    devFileRegistry: 'DefaultDevfileRegistry',
+};
+
+export const BUTTONS = {
+    login: 'Login',
+    kubeContext: 'Select Kubernetes Context',
+    addCluster: 'Add OpenShift Cluster',
+    newComponent: 'New Component',
+};
+
+export const INPUTS = {
+    newUrlQuickPick: 'Provide new URL...',
+    credentialsQuickPick: 'Credentials',
+    newUserQuickPick: 'Add new user...',
+    newFolderQuickPick: 'Add new context folder to workspace',
+    yes: 'Yes',
+    no: 'No',
+};
+
+export const MENUS = {
+    newProject: 'New Project',
+    delete: 'Delete',
+    push: 'Push',
+};
+
+export const COMPONENTS = {
+    nodejsDevfile: 'nodejs (devfile)',
+    devfileComponent: (name: string) => `${name} (devfile)`,
+    pushSuccess: 'Changes successfully pushed',
+};
+
+export const NOTIFICATIONS = {
+    deleteProjectWarning: (projectName: string) =>
+        `Do you want to delete Project '${projectName}'?`,
+    projectDeleteSuccess: (projectName: string) => `Project '${projectName}' successfully deleted`,
+    savePasswordPrompt: 'Do you want to save username and password?',
+    loginSuccess: (cluster: string) => `Successfully logged in to '${cluster}'`,
+};
