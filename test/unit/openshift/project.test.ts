b6314201eca30ebefc0f0cacb7d85caecfb326a7
denis <denis@localhost.localdomain>
2022-05-10 16:54:07 -0700

    Fix project name validation unit test

--- test/unit/openshift/project.test.ts
+++ test/unit/openshift/project.test.ts
@@ -108,7 +108,7 @@ suite('OpenShift/Project', () => {
             sandbox.stub(OdoImpl.Instance.subject,'next');
             await Project.create();
 
-            expect(result).equals('Not a valid Project name. Please use lower case alphanumeric characters or \'-\', start with an alphabetic character, and end with an alphanumeric character');
+            expect(result).equals('Not a valid Project name. Please enter name that starts with an alphanumeric character, use lower case alphanumeric characters or \'-\' and end with an alphanumeric character');
         });
 
         test('validator returns error message if same name of project found', async () => {
