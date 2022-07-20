5d09bad5ba5e9471eff848c66aba77412c8812bb
Ondrej Dockal <odockal@redhat.com>
2022-07-20 15:30:15 +0200

    Update UI tests to use node LTS (16) and activate UI tests in CI (#2479)
    * Update UI tests to use node LTS (16)
    
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>
    
    * Include UI tests in github workflow by default
    
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

---
+++ test/ui/public-ui-test.ts
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import { checkExtension } from './suite/extension';
+import { checkOpenshiftView } from './suite/openshift';
+// import { createComponentTest } from './suite/component';
+
+describe('Extension public-facing UI tests', () => {
+    checkExtension();
+    checkOpenshiftView();
+});
