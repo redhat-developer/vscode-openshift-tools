28a7541f49f32efab8d8ef1ec4e90332aa52298c
msivasubramaniaan <msivasub@redhat.com>
2022-08-20 04:24:21 +0530

    new design of welcome page
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

---
+++ src/webview/welcome/app/welcomePage.tsx
@@ -0,0 +1,299 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+import React from 'react';
+import { VSCodeMessage } from './vsCodeMessage';
+import { Typography } from '@mui/material';
+import HelpIcon from '@mui/icons-material/Help';
+import ChatIcon from '@mui/icons-material/Chat';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import './welcome.scss';
+
+export interface DefaultProps {
+    analytics?: import('@segment/analytics-next').Analytics;
+}
+
+export class Welcome extends React.Component<DefaultProps, {
+    imageVal: number
+}> {
+    textContentRef: React.RefObject<HTMLDivElement>;
+    sandboxRef: React.RefObject<HTMLDivElement>;
+    componentRef: React.RefObject<HTMLDivElement>;
+    remoteRef: React.RefObject<HTMLDivElement>;
+
+    constructor(props) {
+        super(props);
+        this.textContentRef = React.createRef();
+        this.sandboxRef = React.createRef();
+        this.componentRef = React.createRef();
+        this.remoteRef = React.createRef();
+        this.state = {
+            imageVal: 1
+        }
+    }
+
+    handleScroll = () => {
+        const { scrollTop } = this.textContentRef.current;
+        console.log('scrollTop:::', scrollTop);
+        if (scrollTop < 100) {
+            this.setState({
+                imageVal: 1
+            });
+            this.sandboxRef.current.style.marginTop = '0px';
+            this.componentRef.current.style.marginTop = '170px';
+            this.remoteRef.current.style.marginTop = '340px';
+            this.sandboxRef.current.style.visibility = 'visible';
+            this.componentRef.current.style.visibility = 'visible';
+            this.remoteRef.current.style.visibility = 'visible';
+        } else if (scrollTop >= 100 && scrollTop < 140) {
+            this.setState({
+                imageVal: 2
+            });
+            this.componentRef.current.style.marginTop = '-170px';
+            this.remoteRef.current.style.marginTop = '170px';
+            this.sandboxRef.current.style.visibility = 'hidden';
+            this.componentRef.current.style.visibility = 'visible';
+            this.remoteRef.current.style.visibility = 'hidden';
+        } else if (scrollTop >= 140) {
+            this.setState({
+                imageVal: 3
+            });
+            this.remoteRef.current.style.marginTop = '-400px';
+            this.sandboxRef.current.style.visibility = 'hidden';
+            this.componentRef.current.style.visibility = 'hidden';
+            this.remoteRef.current.style.visibility = 'visible';
+        }
+    };
+
+    openGetStarted = (): void => {
+        VSCodeMessage.postMessage({
+            'action': 'callGetStartedPage'
+        });
+        return;
+    }
+
+    openReleaseNotes = (): void => {
+        VSCodeMessage.postMessage({
+            'action': 'openReleaseNotes'
+        });
+        return;
+    }
+
+    openCluster = (value: string): void => {
+        VSCodeMessage.postMessage({
+            'action': 'openCluster',
+            'param': value
+        });
+        return;
+    }
+
+    openDevfileRegistry = (): void => {
+        VSCodeMessage.postMessage({
+            'action': 'openDevfileRegistry'
+        });
+        return;
+    }
+
+    footer = <footer id='footer'>
+        <div className='foot-col-1'>
+            <h4>Channels</h4>
+            <ul>
+                <li><a href='#'><TwitterIcon /></a></li>
+                <li><a href='#'><FacebookIcon /></a></li>
+                <li><a href='#'><YouTubeIcon /></a></li>
+            </ul>
+        </div>
+
+        <div className='foot-col-2'>
+            <h4>Help</h4>
+            <ul>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            <HelpIcon /> Questions
+                        </Typography>
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            <BugReportIcon /> Issues
+                        </Typography>
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            <ChatIcon />  Slack
+                        </Typography>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div className='foot-col-3'>
+            <h4>Resources</h4>
+            <ul>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            <NewReleasesIcon /> What's New
+                        </Typography>
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            <GitHubIcon /> GitHub
+                        </Typography>
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            Marketplace
+                        </Typography>
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <Typography variant='subtitle1' className='footerIcons'>
+                            License
+                        </Typography>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </footer>
+
+    render(): React.ReactNode {
+        const { imageVal } = this.state;
+        return <>
+            <header>
+                <div className='header__logo'>
+                    <img className='image__logo' src={require('../../../../images/title/logo.svg').default} />
+                    <div>
+                        <h1 className='header__title'>OpenShift Connector</h1>
+                    </div>
+                </div>
+            </header>
+            <div className='container' id='con'>
+                <div className='content__area'>
+                    <section className='section--settings'>
+                        <div className='section__content'>
+                            <div className='section__header'>
+                                <div className='setting__input setting__input--big'>
+                                    <label>Component 1</label>
+                                    <a
+                                        className='link__configure'
+                                        title='Jump to more Current Line Blame settings'
+                                        href='command:gitlens.showSettingsPage?%22current-line%22'
+                                    >
+                                        <i className='icon icon__gear'></i>
+                                    </a>
+                                </div>
+                                <p className='section__header-hint'>
+                                    description of component 1
+                                </p>
+                            </div>
+                        </div>
+                        <div className='section__preview'>
+                            <img
+                                className='brand'
+                                src={require('../../../../images/welcome/OpenShift-Branding-box.png').default}
+                                loading='lazy'
+                                width={500} />
+                        </div>
+                    </section>
+                    <section id='welcome' className='section--full'>
+                        <h2 className='section__title section__title--primary'>
+                            Welcome to <span className='highlight'>OpenShift Connector</span>
+                        </h2>
+                        <div className='section__whatsnew'>
+                            <a
+                                title='Watch the OpenShift Getting Started video'
+                                href='#' />
+                            <a
+                                className='button button--flat'
+                                title='See Whats New'
+                                onClick={this.openReleaseNotes}
+                            >See What's New in OpenShift Connector</a>
+                            <a
+                                className='button button--flat'
+                                title='Open the Get Started with OpenShift Connector walkthrough'
+                                onClick={this.openGetStarted}
+                            >Get Started Walkthrough</a>
+                        </div>
+                    </section>
+                    <section id='current-line' className='conent--section--settings'>
+                        <div className='scroll__content' ref={this.textContentRef} onScroll={() => this.handleScroll()}>
+                            <div className='section__header' ref={this.sandboxRef}>
+                                <div className='setting__input setting__input--big'>
+                                    <label>Hybrid Cloud Flexibility</label>
+                                </div>
+                                <p className='section__header-hint'>
+                                    Open hybrid cloud is Red Hat's recommended strategy for architecting, developing, and operating a hybrid mix of applications. This extension allows the developers to connect to any OpenShift cluster, be it running locally or on any hybrid cloud. Using the extension, developers can use the streamlined experience for the easy creation of clusters hosted on OpenShift
+                                </p>
+                                <ul>
+                                    <li>
+                                        <label>Local OpenShift running on the laptop</label>
+                                        <a
+                                            className='button button--flat'
+                                            title='Open CRC'
+                                            onClick={(): void => this.openCluster('crc')}
+                                        >OpenShift Local</a>
+                                    </li>
+                                    <li>
+                                        <label>Provision a free tier version of OpenShift Developer Sandbox</label>
+                                        <a
+                                            className='button button--flat'
+                                            title='Open Developer Sandnox'
+                                            onClick={(): void => this.openCluster('sandbox')}
+                                        >Developer Sandbox</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='section__header' ref={this.componentRef}>
+                                <div className='setting__input setting__input--big'>
+                                    <label>Component Creation Simplicity</label>
+                                </div>
+                                <p className='section__header-hint'>
+                                    Developers can quickly get started with application development using devfile based sample code. This allows them to built from the ground up with application development on Kubernetes in mind. Users can create, develop, debug and deploy applications on OpenShift within few clicks.
+                                </p>
+                                <label>
+                                    The extension supports Java, NodeJS, Python, .NET, Go, Quarkus, etc.
+                                    <a
+                                        className='button button--flat'
+                                        title='Component Registry View'
+                                        onClick={this.openDevfileRegistry}
+                                    >Component Registry View</a>
+                                </label>
+                            </div>
+                            <div className='section__header' ref={this.remoteRef}>
+                                <div className='setting__input setting__input--big'>
+                                    <label>Push code fast and debug on remote</label>
+                                </div>
+                                <p className='section__header-hint'>
+                                    Developers can quickly check out the code, make changes and do fast deployment on the remote cluster. Once deployed, they can Debug the changes directly in the remote environment. There is a separate view for applications running in Debug Mode.
+                                </p>
+                            </div>
+                        </div>
+                        <div className='section__preview'>
+                            {imageVal === 1 ? <img className='content__image__preview' src={require('../../../../images/welcome/cloud.svg').default} />
+                                : imageVal === 2 ? <img className='content__image__preview' src={require('../../../../images/welcome/component.svg').default} />
+                                    : <img className='content__image__preview' src={require('../../../../images/welcome/devfile.svg').default} />}
+                        </div>
+                    </section>
+                </div>
+            </div>
+            {this.footer}
+        </>;
+    }
+}
---
+++ src/webview/welcome/welcomeViewLoader.ts
@@ -0,0 +1,85 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { ExtenisonID } from '../../util/constants';
+
+let panel: vscode.WebviewPanel;
+
+async function welcomeViewerMessageListener(event: any): Promise<any> {
+    switch (event?.action) {
+        case 'callGetStartedPage':
+            vscode.commands.executeCommand('openshift.getStarted');
+            break;
+        case 'openReleaseNotes':
+            await vscode.commands.executeCommand('vscode.open', 'https://github.com/redhat-developer/vscode-openshift-tools/releases');
+            break;
+        case 'openCluster':
+            await vscode.commands.executeCommand('openshift.explorer.addCluster',event.param);
+            break;
+        case 'openDevfileRegistry':
+            await vscode.commands.executeCommand('openshift.componentTypesView.registry.openInView');
+            break;
+        default:
+            panel.webview.postMessage(
+                {
+                    error: 'Invalid command'
+                }
+            );
+            break;
+    }
+}
+export default class WelcomeViewLoader {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    static get extensionPath() {
+        return vscode.extensions.getExtension(ExtenisonID).extensionPath
+    }
+
+    // eslint-disable-next-line @typescript-eslint/require-await
+    static async loadView(title: string): Promise<vscode.WebviewPanel> {
+        const localResourceRoot = vscode.Uri.file(path.join(WelcomeViewLoader.extensionPath, 'out', 'welcomeViewer'));
+        const images = vscode.Uri.file(path.join(WelcomeViewLoader.extensionPath, 'images'));
+        if (panel) {
+            // If we already have a panel, show it in the target column
+            panel.reveal(vscode.ViewColumn.One);
+        } else {
+            panel = vscode.window.createWebviewPanel('welcomeView', title, vscode.ViewColumn.One, {
+                enableScripts: true,
+                localResourceRoots: [images, localResourceRoot],
+                retainContextWhenHidden: true
+            });
+            panel.iconPath = vscode.Uri.file(path.join(WelcomeViewLoader.extensionPath, 'images/context/devfile.png'));
+            panel.webview.html = WelcomeViewLoader.getWebviewContent(WelcomeViewLoader.extensionPath, panel);
+            panel.onDidDispose(() => {
+                panel = undefined;
+            });
+            panel.webview.onDidReceiveMessage(welcomeViewerMessageListener);
+        }
+        return panel;
+    }
+
+    private static getWebviewContent(extensionPath: string, p: vscode.WebviewPanel): string {
+        // Local path to main script run in the webview
+        const reactAppRootOnDisk = path.join(extensionPath, 'out', 'welcomeViewer');
+        const reactAppPathOnDisk = vscode.Uri.file(
+            path.join(reactAppRootOnDisk, 'welcomeViewer.js'),
+        );
+        const reactAppUri = p.webview.asWebviewUri(reactAppPathOnDisk);
+        const htmlString: Buffer = fs.readFileSync(path.join(reactAppRootOnDisk, 'index.html'));
+        const meta = `<meta http-equiv="Content-Security-Policy"
+            content="connect-src *;
+            default-src 'none';
+            img-src ${p.webview.cspSource} https: 'self' data:;
+            script-src ${p.webview.cspSource} 'unsafe-eval' 'unsafe-inline' vscode-resource:;
+            style-src 'self' vscode-resource: 'unsafe-inline';">`;
+        return `${htmlString}`
+            .replace('%COMMAND%', '')
+            .replace('%PLATFORM%', process.platform)
+            .replace('welcomeViewer.js', `${reactAppUri}`)
+            .replace('%BASE_URL%', `${reactAppUri}`)
+            .replace('<!-- meta http-equiv="Content-Security-Policy" -->', meta);
+    }
+}
