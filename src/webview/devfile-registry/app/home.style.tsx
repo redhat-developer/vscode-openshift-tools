00c7f06b379f74e8536cc576ba9448b6eef3a369
msivasubramaniaan <msivasub@redhat.com>
2022-09-05 21:57:06 +0530

    used monokai syntax highlighting and other cosmetic changes
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/webview/devfile-registry/app/home.style.tsx
+++ src/webview/devfile-registry/app/home.style.tsx
@@ -8,7 +8,7 @@ import { Theme, createStyles } from '@material-ui/core/styles';
 export default (_theme: Theme) =>
     createStyles({
         devfileGalleryGrid: {
-            margin: '0rem',
+            margin: '1.5rem 0rem',
             padding: '0rem 1.5rem',
             color: 'var(--vscode-foreground)',
             backgroundColor: 'var(--vscode-editor-background)',
