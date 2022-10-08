92d7e5cac4a06d7c2b46b399c720da817d8de11f
Denis Golovin <dgolovin@redhat.com>
2022-10-07 23:37:14 -0700

    Restore Open Console Dashboard command for curren ontext
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/todo.ts
+++ src/todo.ts
@@ -2,3 +2,9 @@
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
+// TODO: Migrate 'Open Dashboard' to context item in Openshift Application View
+// TODO: MIgrate 'Set Project' to context item
+// TODO: Create Project should (if possible) check permissinons to create project
+// TODO: Bring back show log in webview
+// TODO: Debug
+// TODO: Documentation update: README.md, Walkthrough and demo gif
