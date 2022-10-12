450c5a839c6a4e786e6ae0a941ef08fd5c19813f
Denis Golovin <dgolovin@redhat.com>
2022-10-11 23:27:09 -0700

    Update odo to v3.0.0 release
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/todo.ts
+++ src/todo.ts
@@ -3,12 +3,11 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-// TODO: Bring back show log in webview
 // TODO: Debug
 // TODO: Deploy (after next release)
 // TODO: Documentation update: README.md, Walkthrough and demo gif
+// Optional
 // TODO: Create report for extension versions at use
-
 
 // OpenShift Application Explorer
 // TODO: Handle no ~/.kube/config use case and show item 'Login to a cluster'
