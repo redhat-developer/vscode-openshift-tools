ea7b3ec312a57363825596f3382eee442f74e6a4
Denis Golovin <dgolovin@redhat.com>
2022-10-26 23:36:46 -0700

    Migrate debug command to work with odo v3
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- src/explorer.ts
+++ src/explorer.ts
@@ -140,7 +140,7 @@ export class OpenShiftExplorer implements TreeDataProvider<ExplorerItem>, Dispos
                 }
             }
             return {
-                contextValue: 'openshift.k8sObject.',
+                contextValue: 'openshift.k8sObject',
                 label: element.metadata.name,
                 collapsibleState: TreeItemCollapsibleState.None,
                 iconPath: element.kind === 'Deployment' ? path.resolve(__dirname, '../../images/context/dark/deployment.svg') : path.resolve(__dirname, '../../images/context/dark/deployment-config.svg')
