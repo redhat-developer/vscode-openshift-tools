f279e53a89f009c62d6636f6935eaf9a8ff8d011
Denis Golovin <dgolovin@redhat.com>
2022-11-11 12:08:42 -0800

    S
    This PR fixes #????.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -10,9 +10,9 @@ import cp = require('child_process');
 import path = require('path');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import * as etest from '@vscode/test-electron';
+import * as testElectron from '@vscode/test-electron';
 
-etest.downloadAndUnzipVSCode().then((executable: string) => {
+void testElectron.downloadAndUnzipVSCode().then((executable: string) => {
     let vsCodeExecutable;
     if (platform() === 'darwin') {
         console.log(executable);
@@ -32,9 +32,7 @@ etest.downloadAndUnzipVSCode().then((executable: string) => {
     const vsCodeTest = path.resolve(path.join(extensionRootPath, '.vscode-test'));
     const userDataDir = path.join(vsCodeTest, 'user-data');
     const extDir = path.join(vsCodeTest, 'extensions');
-
     const vsixPath = vsixName.includes('.vsix') ? path.join(extensionRootPath, vsixName) : vsixName;
     console.log('Installin extension: ', vsixPath );
-    console.log(`${vsCodeExecutable} --install-extension ${vsixPath} --user-data-dir ${userDataDir} --extensions-dir ${extDir}`);
     cp.execSync(`${vsCodeExecutable} --install-extension ${vsixPath} --user-data-dir ${userDataDir} --extensions-dir ${extDir}`);
 });
