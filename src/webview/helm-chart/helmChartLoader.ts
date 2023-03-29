f20a5c4a750f4cc18cf60b273240cd06d648c53a
msivasubramaniaan <msivasub@redhat.com>
2023-03-29 23:57:01 +0530

    removed make-fetch
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/webview/helm-chart/helmChartLoader.ts
+++ src/webview/helm-chart/helmChartLoader.ts
@@ -6,7 +6,6 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as YAML from 'yaml'
-import fetch = require('make-fetch-happen');
 import { ExtensionID } from '../../util/constants';
 import { vsCommand } from '../../vscommand';
 import { ComponentTypesView } from '../../registriesView';
