c29b87665816f36ea8c9ad9d9db64b5625cfdbb7
Muthurajan Sivasubramanian <93245779+msivasubramaniaan@users.noreply.github.com>
2023-05-22 23:49:00 +0530

    added circular progress (#2885)
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/webview/common/loading.tsx
+++ src/webview/common/loading.tsx
@@ -2,34 +2,40 @@
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
-import { Box, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import LinearProgress from '@mui/material/LinearProgress';
 import { makeStyles } from '@mui/styles';
 import * as React from 'react';
 import loaderStyle from './loading.style';
 
 interface LoadProps extends React.AllHTMLAttributes<HTMLDivElement> {
-    title: string
+    title: string,
+    type?: string
 }
 
 const useLoadingStyle = makeStyles(loaderStyle);
 
 export const LoadScreen: React.FC<LoadProps> = ({
-    title
+    title,
+    type
 }: LoadProps) => {
     const loadingStyle = useLoadingStyle();
+    const isCircular = type && type === 'circle';
     const isInstall = title.indexOf('Install') !== -1;
     return (
-        <div className={isInstall ? '' : loadingStyle.loading}>
-            <div style={{ width : isInstall ? 'auto' : '30rem' }}>
+        <div className={isInstall ? '' : loadingStyle.loading} style={{ float: 'right', marginRight: '0.5rem' }}>
+            <div style={{ width: isInstall ? 'auto' : '30rem', display: 'flex', flexDirection: isCircular ? 'row' : 'column' }}>
                 <Box sx={{ color: '#EE0000' }}>
-                    <LinearProgress color='inherit' sx={{ height: isInstall ? '0.5rem' : '1rem' }} />
+                    {type && type === 'circle' ?
+                        <CircularProgress color='inherit' size={20} /> :
+                        <LinearProgress color='inherit' sx={{ height: isInstall ? '0.5rem' : '1rem' }} />
+                    }
                 </Box>
                 <Typography
                     variant='caption'
                     component='div'
                     color='inherit'
-                    style={{ marginTop: '0.5rem', fontSize: '1.25em' }}
+                    style={{ marginTop: isCircular ? '3px' : '0.5rem', marginLeft: isCircular ? '5px' : '0', fontSize: '1em' }}
                 >{title}</Typography>
             </div>
         </div>
