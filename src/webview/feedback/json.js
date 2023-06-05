847f73661330c42fa2dab2f9b1e22b6ba1fd072b
Muthurajan Sivasubramanian <93245779+msivasubramaniaan@users.noreply.github.com>
2023-06-05 14:41:00 +0530

    Update src/webview/feedback/json.js
    Co-authored-by: Mohit Suman <mohit.skn@gmail.com>

--- src/webview/feedback/json.js
+++ src/webview/feedback/json.js
@@ -77,7 +77,7 @@ export const json = {
                     'name': 'contact',
                     'hideNumber': true,
                     'title': 'Share your contact information if you\'d like us to answer you:',
-                    'placeholder': 'Enter email address, phone number or leave blank for anonymous feedback'
+                    'placeholder': 'Provide email address or leave blank for anonymous feedback'
                 },
             ]
         }
