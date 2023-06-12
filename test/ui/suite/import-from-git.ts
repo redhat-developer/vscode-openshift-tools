eee53173f4e5bfa4353a2c0c940ee6d9c3805d96
msivasubramaniaan <msivasub@redhat.com>
2023-06-12 20:35:59 +0530

    increased time out
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- test/ui/suite/import-from-git.ts
+++ test/ui/suite/import-from-git.ts
@@ -16,7 +16,7 @@ export function testImportFromGit() {
         const tempDir: string = path.join(__dirname, 'temp');
 
         before(async function () {
-            this.timeout(20000);
+            //this.timeout(20000);
             const view: SideBarView = await (await (await new ActivityBar().wait(5_000)).getViewControl(VIEWS.openshift)).openView();
             editorView = await ((await new Workbench().wait(5_000)).getEditorView().wait(5_000));
             await new Promise(res => setTimeout(res, 5000));
@@ -44,7 +44,7 @@ export function testImportFromGit() {
         });
 
         it('Import project from git', async function () {
-            this.timeout(30_000);
+            this.timeout(40_000);
             await editorView.closeAllEditors();
             const buttons: WelcomeContentButton[] = await welcome.getButtons();
             let importButton: WelcomeContentButton;
