237a1b0467ba337f2209fce62a6accfe5eb432d2
Lukas Grossmann <lgrossma@redhat.com>
2023-10-31 13:23:55 +0100

    Adding create component from git
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/common/ui/webview/newComponentWebViewEditor.ts
+++ test/ui/common/ui/webview/newComponentWebViewEditor.ts
@@ -8,7 +8,7 @@ import { WebViewForm } from './WebViewForm';
 //TODO: Add support for create from git page and from local codebase page
 
 /**
- * @author lgrossma@redhat.com
+ * @author Lukas Grossmann <lgrossma@redhat.com>
  * Class represents WebView of Create Component form
  */
 
@@ -23,11 +23,11 @@ export class CreateComponentWebView extends WebViewForm {
     }
 
     private async getCreateFromGitButton(webView: WebView): Promise<WebElement> {
-        return await webView.findWebElement(By.xpath('//*[@id="root"]/div/div/div[1]/div[2]/div[3]/button'));
+        return await webView.findWebElement(By.xpath('//div[..//h6[contains(text(),"From Existing Remote Git Repository")]]/button'));
     }
 
     private async getCreateFromLocalButton(webView: WebView): Promise<WebElement> {
-        return await webView.findWebElement(By.xpath('//*[@id="root"]/div/div/div[1]/div[1]/div[3]/button'));
+        return await webView.findWebElement(By.xpath('//div[..//h6[contains(text(),"From Existing Local Codebase")]]/button'));
     }
 
     public async createComponentFromTemplate(): Promise<void> {
@@ -39,21 +39,24 @@ export class CreateComponentWebView extends WebViewForm {
 
     public async createComponentFromGit(): Promise<void> {
         await this.enterWebView(async (webView) => {
-            await (await this.getCreateFromGitButton(webView)).click();
+            const button = await this.getCreateFromGitButton(webView);
+            await button.click();
         });
     }
 
     public async createComponentFromLocalCodebase(): Promise<void> {
         await this.enterWebView(async (webView) => {
-            await (await this.getCreateFromLocalButton(webView)).click();
+            const button = await this.getCreateFromLocalButton(webView);
+            await button.click();
         });
     }
 }
 
 /**
  * Class represents page that shows up after selecting a devfile
+ * @author Lukas Grossmann <lgrossma@redhat.com>
 */
-export class TemplateProjectPage extends WebViewForm{
+export class SetNameAndFolderPage extends WebViewForm {
 
     public constructor(name: string) {
         super(name);
@@ -91,4 +94,60 @@ export class TemplateProjectPage extends WebViewForm{
     private async getCreateComponentButton(webView: WebView): Promise<WebElement> {
         return await webView.findWebElement(By.xpath('//span[contains(text(), "Create Component")]'));
     }
+}
+
+/**
+ * Class represents page that shows up after selecting to create a component from Git
+ * @author Lukas Grossmann <lgrossma@redhat.com>
+ */
+export class GitProjectPage extends WebViewForm {
+
+    public constructor() {
+        super('Create Component');
+    }
+
+    public async insertGitLink(link: string): Promise<void> {
+        await this.enterWebView(async (webView) => {
+            const linkField = await this.getGitRepositoryLinkField(webView);
+            await linkField.sendKeys(link)
+        });
+    }
+
+    public async clickNextButton(): Promise<void> {
+        await this.enterWebView(async (webView) => {
+            const button = await this.getNextButton(webView);
+            await button.click();
+        })
+    }
+
+    public async clickContinueButton(): Promise<void> {
+        await this.enterWebView(async (webView) => {
+            const button = await this.getContinueButton(webView);
+            await button.click()
+        });
+    }
+
+    public async clickSelectDifferentDevfileButton(): Promise<void> {
+        await this.enterWebView(async (webView) => {
+            const button = await this.getSelectDifferentDevfileButton(webView);
+            await button.click()
+        });
+    }
+
+    private async getGitRepositoryLinkField(webView: WebView): Promise<WebElement> {
+        return await webView.findWebElement(By.xpath('//div[./label[contains(text(), "Link to Git Repository")]]//input'));
+    }
+
+    private async getNextButton(webView: WebView): Promise<WebElement> {
+        return await webView.findWebElement(By.xpath('//button[contains(text(), "NEXT")]'));
+    }
+
+    private async getContinueButton(webView: WebView): Promise<WebElement> {
+        return await webView.findWebElement(By.xpath('//button[contains(text(), "CONTINUE WITH THIS DEVFILE")]'));
+    }
+
+    private async getSelectDifferentDevfileButton(webView: WebView): Promise<WebElement> {
+        return await webView.findWebElement(By.xpath('//button[contains(text(), "SELECT A DIFFERENT DEVFILE")]'));
+    }
+
 }
