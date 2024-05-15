93365505cd5557a19c658c4a78679af6ada63b52
Lukas Grossmann <lgrossma@redhat.com>
2024-05-15 11:55:40 +0200

    Fix eslint errors
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/suite/command-about.ts
+++ test/ui/suite/command-about.ts
@@ -35,7 +35,7 @@ export function checkAboutCommand() {
         it('Terminal shows according information', async function() {
             this.timeout(60_000);
             await terminalInstance.click();
-            
+
             const os = process.platform;
             const key = os === 'darwin' ? Key.COMMAND : Key.CONTROL;
 
