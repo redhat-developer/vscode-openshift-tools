76d870a799d4826b2a664a4d19b5b0a95bb15af9
Lukas Grossmann <lgrossma@redhat.com>
2024-05-15 13:01:52 +0200

    remove logs
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/suite/command-about.ts
+++ test/ui/suite/command-about.ts
@@ -34,25 +34,14 @@ export function checkAboutCommand() {
         // Pending on https://github.com/redhat-developer/vscode-extension-tester/pull/855
         it('Terminal shows according information', async function() {
             this.timeout(60_000);
-            console.log('a')
             await terminalInstance.click();
-            console.log('b')
-
-            //const os = process.platform;
-            console.log('c')
-            console.log('d')
 
             await terminalInstance.sendKeys(`${Key.CONTROL}${Key.SHIFT}a`);
-            console.log('e')
             await terminalInstance.sendKeys(`${Key.CONTROL}${Key.SHIFT}c`);
-            console.log('f')
             await webviewView.switchBack();
-            console.log('g')
 
             const cb = await import('clipboardy');
-            console.log('h')
             const clipboard = await cb.read();
-            console.log('i')
             expect(clipboard).to.contain(odoVersion);
         });
     });
