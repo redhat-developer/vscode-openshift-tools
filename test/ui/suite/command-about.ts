ca2d064d18987d091876ce308061b1e552b786c2
Lukas Grossmann <86789336+lgrossma@users.noreply.github.com>
2024-05-16 14:25:48 +0200

    Fix About command test (#4125)
    * Fix About command test
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * Add dependency to package.json
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * Fix eslint errors
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * add logs
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * add logs
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * add logs
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * add logs
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * remove logs
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    * Delete old comments, delete unused lines
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>
    
    ---------
    
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/suite/command-about.ts
+++ test/ui/suite/command-about.ts
@@ -3,33 +3,43 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { expect } from 'chai';
-import { TerminalView } from 'vscode-extension-tester';
+import { By, EditorView, Key, WebElement, WebviewView, Workbench } from 'vscode-extension-tester';
 import { activateCommand } from '../common/command-activator';
+import { expect } from 'chai';
 
 export function checkAboutCommand() {
     describe('About Command', () => {
         const command = '>OpenShift: About';
-        const expectedTerminalName = 'OpenShift: Show odo Version';
         const odoVersion = 'odo v3';
 
+        let webviewView: WebviewView;
+        let terminalInstance: WebElement;
+
         before(async () => {
+            await new EditorView().closeAllEditors();
             await activateCommand(command);
         })
 
-        // Pending on https://github.com/redhat-developer/vscode-extension-tester/pull/855
-        it.skip('New terminal opens', async function() {
-            this.timeout(60000);
-            await new Promise(res => setTimeout(res, 6000));
-            const terminalName = await new TerminalView().getCurrentChannel();
-            expect(terminalName).to.include(expectedTerminalName);
+        it('New terminal opens', async function() {
+            this.timeout(60_000);
+            await new Promise(res => setTimeout(res, 3_000));
+            await new Workbench().executeCommand('Openshift Terminal: Focus on OpenShift Terminal View');
+            webviewView = new WebviewView();
+            await webviewView.switchToFrame(6_500);
+            terminalInstance = await webviewView.findWebElement(By.xpath('//textarea[@aria-label = "Terminal input"]'));
         });
 
-        // Pending on https://github.com/redhat-developer/vscode-extension-tester/pull/855
-        it.skip('Terminal shows according information', async function() {
-            this.timeout(60000);
-            const terminalText = await new TerminalView().getText();
-            expect(terminalText).to.include(odoVersion);
+        it('Terminal shows according information', async function() {
+            this.timeout(60_000);
+            await terminalInstance.click();
+
+            await terminalInstance.sendKeys(`${Key.CONTROL}${Key.SHIFT}a`);
+            await terminalInstance.sendKeys(`${Key.CONTROL}${Key.SHIFT}c`);
+            await webviewView.switchBack();
+
+            const cb = await import('clipboardy');
+            const clipboard = await cb.read();
+            expect(clipboard).to.contain(odoVersion);
         });
     });
 }
