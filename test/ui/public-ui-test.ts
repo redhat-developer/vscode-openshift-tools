95afdd691faa8a4190c57724829a1f62e536801f
Lukas Grossmann <lgrossma@redhat.com>
2024-05-20 19:50:34 +0200

    adding log for checking kubeconfig content
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/public-ui-test.ts
+++ test/ui/public-ui-test.ts
@@ -5,6 +5,7 @@
 
 import { checkAboutCommand } from './suite/command-about';
 import path = require('path');
+import * as fs from 'fs-extra';
 import { testCreateComponent } from './suite/createComponent';
 import { testDevfileRegistries } from './suite/devfileRegistries';
 import { checkExtension } from './suite/extension';
@@ -19,25 +20,33 @@ require('source-map-support').install();
 describe('Extension public-facing UI tests', function() {
     const contextFolder = path.join(__dirname, 'context');
 
-    before(async function () {
-        await backupKubeConfig();
+    describe('Non-cluster tests', function() {
+        before(async function () {
+            await backupKubeConfig();
+        });
+    
+        checkExtension();
+        checkOpenshiftView();
+        testAddCluster();
+        checkAboutCommand();
+        testDevfileRegistries();
+        checkFocusOnCommands();
+        testCreateComponent(contextFolder);
+        testCreateServerlessFunction(contextFolder);
     });
 
-    checkExtension();
-    checkOpenshiftView();
-    testAddCluster();
-    checkAboutCommand();
-    testDevfileRegistries();
-    checkFocusOnCommands();
-    testCreateComponent(contextFolder);
-    testCreateServerlessFunction(contextFolder);
+    describe('Extension public-facing UI tests with Kind cluster', function() {
+        before(async function() {
+            await loadKubeConfigFromBackup();
+            const kubeConfig = path.join(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'], '.kube', 'config');
+            const fileContent = fs.readFileSync(kubeConfig);
+            console.log(fileContent.toString());
+        });
+
+        checkFocusOnCommands();
+    
+        //tests requiring clusters incoming
+    });
 });
 
-describe('Extension public-facing UI tests with Kind cluster', function() {
-    before(async function() {
-        await loadKubeConfigFromBackup();
-    });
-    checkFocusOnCommands();
 
-    //tests requiring clusters incoming
-})
