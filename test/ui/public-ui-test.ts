c3f4a62cdba1ab2d680c6989b59154aa4b1f01ae
Lukas Grossmann <lgrossma@redhat.com>
2024-05-20 16:55:23 +0200

    Add possibility to run UI tests with cluster
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/public-ui-test.ts
+++ test/ui/public-ui-test.ts
@@ -12,18 +12,38 @@ import { checkFocusOnCommands } from './suite/focusOn';
 import { checkOpenshiftView } from './suite/openshift';
 import { testCreateServerlessFunction } from './suite/serverlessFunction';
 import { testAddCluster } from './suite/addCluster';
+import { backupKubeConfig, loadKubeConfigFromBackup } from './common/kubeConfigUtils';
 
 require('source-map-support').install();
 
-describe('Extension public-facing UI tests', function() {
+describe('Extension public-facing UI tests', function () {
     const contextFolder = path.join(__dirname, 'context');
+    let clusterIsSet = false;
 
-    checkExtension();
-    checkOpenshiftView();
-    testAddCluster();
-    checkAboutCommand();
-    testDevfileRegistries();
-    checkFocusOnCommands();
-    testCreateComponent(contextFolder);
-    testCreateServerlessFunction(contextFolder);
+    describe('Non-cluster tests', function () {
+        before(async function () {
+            await backupKubeConfig();
+        });
+
+        checkExtension();
+        checkOpenshiftView();
+        testAddCluster();
+        checkAboutCommand(clusterIsSet);
+        testDevfileRegistries();
+        checkFocusOnCommands();
+        testCreateComponent(contextFolder);
+        testCreateServerlessFunction(contextFolder);
+    });
+
+    describe('Extension public-facing UI tests with Kind cluster', function () {
+        clusterIsSet = true;
+
+        before(async function () {
+            await loadKubeConfigFromBackup();
+        });
+
+        checkAboutCommand(clusterIsSet);
+
+        //tests requiring clusters incoming
+    });
 });
--- test/ui/suite/command-about.ts
+++ test/ui/suite/command-about.ts
@@ -7,10 +7,12 @@ import { By, EditorView, Key, WebElement, WebviewView, Workbench } from 'vscode-
 import { activateCommand } from '../common/command-activator';
 import { expect } from 'chai';
 
-export function checkAboutCommand() {
+export function checkAboutCommand(clusterIsSet: boolean) {
     describe('About Command', () => {
         const command = '>OpenShift: About';
         const odoVersion = 'odo v3';
+        const noClusterMessage = 'unable to fetch the cluster server version';
+        const clusterServer = 'https://127.0.0.1';
 
         let webviewView: WebviewView;
         let terminalInstance: WebElement;
@@ -18,18 +20,22 @@ export function checkAboutCommand() {
         before(async () => {
             await new EditorView().closeAllEditors();
             await activateCommand(command);
-        })
-
-        it('New terminal opens', async function() {
-            this.timeout(60_000);
-            await new Promise(res => setTimeout(res, 3_000));
-            await new Workbench().executeCommand('Openshift Terminal: Focus on OpenShift Terminal View');
-            webviewView = new WebviewView();
-            await webviewView.switchToFrame(6_500);
-            terminalInstance = await webviewView.findWebElement(By.xpath('//textarea[@aria-label = "Terminal input"]'));
         });
 
-        it('Terminal shows according information', async function() {
+        it('New terminal opens', async function () {
+            this.timeout(60_000);
+            await new Promise((res) => setTimeout(res, 3_000));
+            await new Workbench().executeCommand(
+                'Openshift Terminal: Focus on OpenShift Terminal View',
+            );
+            webviewView = new WebviewView();
+            await webviewView.switchToFrame(6_500);
+            terminalInstance = await webviewView.findWebElement(
+                By.xpath('//textarea[@aria-label = "Terminal input"]'),
+            );
+        });
+
+        it('Terminal shows according information', async function () {
             this.timeout(60_000);
             await terminalInstance.click();
 
@@ -40,6 +46,11 @@ export function checkAboutCommand() {
             const cb = await import('clipboardy');
             const clipboard = await cb.read();
             expect(clipboard).to.contain(odoVersion);
+            if (!clusterIsSet) {
+                expect(clipboard).to.contain(noClusterMessage);
+            } else {
+                expect(clipboard).to.contain(clusterServer);
+            }
         });
     });
 }
