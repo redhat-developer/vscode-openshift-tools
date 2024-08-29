6792418839ec0fea58653f48a7dfeb5864db7558
msivasubramaniaan <msivasub@redhat.com>
2024-08-29 21:14:34 +0530

    removed console log
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/webview/create-deployment/pages/builderConfig.tsx
+++ src/webview/create-deployment/pages/builderConfig.tsx
@@ -39,8 +39,6 @@ function createDeployment(
     gitURL: string,
     builderImage: BuilderImage
 ) {
-    // eslint-disable-next-line no-console
-    console.log('createDeployment called');
     window.vscodeApi.postMessage({
         action: 'createDeployment',
         data: {
