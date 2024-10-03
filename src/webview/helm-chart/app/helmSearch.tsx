3f174dacb3b8995303705d69bedcd8ecb7408a8e
msivasubramaniaan <msivasub@redhat.com>
2024-10-03 19:17:00 +0530

    added tree view filtering
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/webview/helm-chart/app/helmSearch.tsx
+++ src/webview/helm-chart/app/helmSearch.tsx
@@ -4,8 +4,11 @@
  *-----------------------------------------------------------------------------------------------*/
 
 import React from 'react';
-import { Alert, Checkbox, Divider, FormControlLabel, FormGroup, IconButton, InputAdornment, Modal, Pagination, Stack, TextField, Theme, Tooltip, Typography } from '@mui/material';
+import { Alert, Box, Checkbox, Divider, FormControlLabel, FormGroup, IconButton, InputAdornment, Modal, Pagination, Stack, TextField, Theme, Tooltip, Typography } from '@mui/material';
 import { Close, Search } from '@mui/icons-material';
+import { TreeViewBaseItem } from '@mui/x-tree-view/models';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+import { useTreeViewApiRef } from '@mui/x-tree-view/hooks/useTreeViewApiRef'; // Import the API hook
 import { HelmListItem } from './helmListItem';
 import { ChartResponse, HelmRepo } from '../../../helm/helmChartType';
 import { VSCodeMessage } from '../vsCodeMessage';
@@ -180,6 +183,51 @@ export function HelmSearch(props: HelmSearchProps) {
     const [selectedHelmChart, setselectedHelmChart] = React.useState<ChartResponse>();
     const [currentPage, setCurrentPage] = React.useState(1);
     const [searchText, setSearchText] = React.useState('');
+    const [treeFindStrs, setTreeFindStrs] = React.useState<string[]>([]);
+    const [isFindInChartName, setFindInChartName] = React.useState(false);
+    const [isOtherTreeElementSelected, setOtherTreeElementSelected] = React.useState(false);
+
+    const treeViewItems: TreeViewBaseItem[] = [
+        {
+            id: 'allitems',
+            label: 'All Items',
+            children: [
+                {
+                    id: 'ci-cd',
+                    label: 'CI/CD',
+                    children: [
+                        { id: 'jenkins', label: 'Jenkins' }
+                    ]
+                },
+                {
+                    id: 'databases',
+                    label: 'Databases',
+                    children: [
+                        { id: 'mariadb', label: 'MariaDB' },
+                        { id: 'mongodb', label: 'MongoDB' },
+                        { id: 'mysql', label: 'MySQL' },
+                        { id: 'postgres', label: 'Postgres' },
+
+                    ]
+                },
+                {
+                    id: 'languages',
+                    label: 'Languages',
+                    children: [
+                        { id: 'dotnet', label: '.NET' },
+                        { id: 'go', label: 'GO' },
+                        { id: 'java', label: 'Java' },
+                        { id: 'javascript', label: 'JavaScript' },
+                        { id: 'php', label: 'PHP' },
+                        { id: 'python', label: 'Python' },
+                        { id: 'ruby', label: 'Ruby' }
+                    ]
+                }
+            ]
+        },
+    ];
+
+    const treeViewRef = useTreeViewApiRef();
 
     function ascName(oldChart: ChartResponse, newChart: ChartResponse) {
         const oldChartName = oldChart.displayName || oldChart.chartName;
@@ -222,7 +270,7 @@ export function HelmSearch(props: HelmSearchProps) {
             enabledArray.push({
                 name: helmRepo.name,
                 url: helmRepo.url,
-                enabled: true,
+                enabled: false,
             });
         }
         setHelmChartEnabled((_) => enabledArray);
@@ -243,6 +291,27 @@ export function HelmSearch(props: HelmSearchProps) {
         setCurrentPage((_) => 1);
     }, [helmChartEnabled, providerTypeEnabled, searchText]);
 
+    React.useEffect(() => {
+        // Wait for the component to mount
+        const selectFirstNode = () => {
+            if (!isOtherTreeElementSelected) {
+                // Programmatically focus and click the first item when the component mounts
+                const api = treeViewRef.current;
+                if (api) {
+                    // Focus the first node manually by using the nodeId of the first item
+                    const firstNodeId = treeViewItems[0].id; // Accessing the first item's id
+                    api.focusItem(undefined, firstNodeId); // Focus the first node
+                    api.selectItem({
+                        event: undefined, itemId: firstNodeId,
+                        keepExistingSelection: false, shouldBeSelected: true
+                    }); //select the first node
+                }
+            }
+        };
+
+        selectFirstNode();
+    }, [treeViewItems, isOtherTreeElementSelected]);
+
     const activeProviderTypes = providerTypeEnabled //
         .filter((entry) => entry.enabled) //
         .map((entry) => entry.type);
@@ -252,11 +321,10 @@ export function HelmSearch(props: HelmSearchProps) {
         .map((entry) => entry.name);
 
     function getFilteredCharts(): ChartResponse[] {
-        const filteredCharts = [];
-        const helmResponse = helmCharts.filter((helmChart: ChartResponse) =>
-            activeRepos.includes(helmChart.repoName))
-            .filter((helmChart: ChartResponse) => isProviderTypesToBeIncluded(helmChart))
-            .filter(function (helmChart: ChartResponse) {
+        const filteredCharts: ChartResponse[] = [];
+        let helmResponse: ChartResponse[] = [];
+        if (treeFindStrs.length === 0) {
+            helmResponse = helmCharts.filter((helmChart: ChartResponse) => {
                 const searchTerms = searchText.split(/\s+/);
                 return every(
                     searchTerms.map(
@@ -270,13 +338,71 @@ export function HelmSearch(props: HelmSearchProps) {
                     ),
                 );
             });
+        } else {
+            helmResponse = helmCharts.filter((helmChart: ChartResponse) => {
+                return isFindInChartName ? findInChartName(helmChart, treeFindStrs) || findInKeywords(helmChart, treeFindStrs)
+                    :
+                    findInKeywords(helmChart, treeFindStrs);
+            });
+        }
+
+        if (activeRepos.length > 0) {
+            helmResponse = helmResponse.filter((helmChart) => activeRepos.includes(helmChart.repoName));
+        }
+
+        if (activeProviderTypes.length > 0) {
+            helmResponse = helmResponse.filter((helmChart) => isProviderTypesToBeIncluded(helmChart));
+        }
+
         filteredCharts.push(...helmResponse);
         return filteredCharts.sort(ascName);
+    }
+
+    function findInChartName(chart: ChartResponse, findStr: string[]): boolean {
+        return chart.displayName?.toLowerCase().includes(findStr[0]) || chart.chartName.includes(findStr[0]);
+    }
+
+    function findInKeywords(chart: ChartResponse, findStrs: string[]): boolean {
+        return chart.chartVersions[0].keywords &&
+            chart.chartVersions[0].keywords.some((keyword) => findStrs.includes(keyword));
+
     }
 
     function isProviderTypesToBeIncluded(chart: ChartResponse): boolean {
         return activeProviderTypes.length === 0 || //
             activeProviderTypes.includes(chart.chartVersions[0].annotations && chart.chartVersions[0].annotations['charts.openshift.io/providerType']);
+    }
+
+    function handleTreeViewClick(_event: React.MouseEvent<Element, MouseEvent>, itemId: string): void {
+        setOtherTreeElementSelected((_flag) => true);
+        let languagesTreeNodes: TreeViewBaseItem[] = [];
+        switch (itemId) {
+            case 'allitems':
+                setTreeFindStrs((_oldVal) => []);
+                setFindInChartName((_oldVal) => false);
+                break;
+            case 'ci-cd':
+                setTreeFindStrs((_oldVal) => ['jenkins']);
+                setFindInChartName((_oldVal) => true);
+                break;
+            case 'databases':
+                setTreeFindStrs((_oldVal) => ['database']);
+                setFindInChartName((_oldVal) => false);
+                break;
+            case 'languages':
+                languagesTreeNodes = treeViewItems[0].children.filter((treeViewItem) => treeViewItem.id === 'languages').map((treeItem) => treeItem.children)[0];
+                setTreeFindStrs((_oldVal) => languagesTreeNodes.map((childItem: TreeViewBaseItem) => childItem.id));
+                setFindInChartName((_oldVal) => false);
+                break;
+            default:
+                setTreeFindStrs((_oldVal) => [itemId]);
+                if (itemId === 'jenkins') {
+                    setFindInChartName((_oldVal) => true);
+                } else {
+                    setFindInChartName((_oldVal) => false);
+                }
+                break;
+        }
     }
 
     return (
@@ -287,12 +413,29 @@ export function HelmSearch(props: HelmSearchProps) {
                         <LoadScreen title='Retrieving Helm Charts' /> :
                         <Stack direction="row" spacing={1} width={'100%'}>
                             {
-                                (helmRepos.length > 1 || providerTypes.length > 1) &&
+                                (helmCharts.length >= 1) &&
                                 <>
                                     <Stack direction='column' sx={{ height: '100vh', overflow: 'scroll', maxWidth: '30%' }} spacing={0}>
                                         <Typography variant="body2" marginBottom={1}>
                                             Filter by
                                         </Typography>
+                                        {
+                                            <>
+                                                <Box>
+                                                    <RichTreeView items={treeViewItems}
+                                                        sx={{
+                                                            '& .MuiTreeItem-label': {
+                                                                fontSize: 'var(--vscode-font-size) !important',
+                                                                fontFamily: 'var(--vscode-font-family) !important',
+                                                                fontWeight: 'lighter !important'
+                                                            }
+                                                        }}
+                                                        apiRef={treeViewRef}
+                                                        onItemClick={(event, itemId) => handleTreeViewClick(event, itemId)} />
+                                                </Box>
+                                                <Divider orientation='horizontal' sx={{ width: '100%', marginTop: 2 }} />
+                                            </>
+                                        }
                                         {helmRepos.length > 1 && (
                                             <>
                                                 <Typography variant='body2' marginTop={1} marginBottom={1}>
@@ -339,30 +482,30 @@ export function HelmSearch(props: HelmSearchProps) {
                                         <Alert severity='info'>No Helm repos are configured. Please configure a Helm repo to view its charts.</Alert>
                                     </Stack>
                                     :
-                                <Stack
-                                    id='devfileList'
-                                    direction='column'
-                                    sx={{ height: 'calc(100vh - 100px)', overflow: 'scroll' }}
-                                    divider={<Divider />}
-                                    width='100%'
-                                >
-                                    {getFilteredCharts()
-                                        .slice(
-                                            (currentPage - 1) * ITEMS_PER_PAGE,
-                                            Math.min(currentPage * ITEMS_PER_PAGE, getFilteredCharts().length)
-                                        )
-                                        .map((helmChart) => {
-                                            return (
-                                                <HelmListItem
-                                                    key={`${helmChart.chartName}-${helmChart.displayName}`}
-                                                    helmChart={helmChart}
-                                                    selectedVersion={helmChart.chartVersions[0]}
-                                                    buttonCallback={() => {
-                                                        setselectedHelmChart(helmChart);
-                                                    }} />
-                                            );
-                                        })}
-                                </Stack>}
+                                    <Stack
+                                        id='devfileList'
+                                        direction='column'
+                                        sx={{ height: 'calc(100vh - 100px)', overflow: 'scroll' }}
+                                        divider={<Divider />}
+                                        width='100%'
+                                    >
+                                        {getFilteredCharts()
+                                            .slice(
+                                                (currentPage - 1) * ITEMS_PER_PAGE,
+                                                Math.min(currentPage * ITEMS_PER_PAGE, getFilteredCharts().length)
+                                            )
+                                            .map((helmChart) => {
+                                                return (
+                                                    <HelmListItem
+                                                        key={`${helmChart.chartName}-${helmChart.displayName}`}
+                                                        helmChart={helmChart}
+                                                        selectedVersion={helmChart.chartVersions[0]}
+                                                        buttonCallback={() => {
+                                                            setselectedHelmChart(helmChart);
+                                                        }} />
+                                                );
+                                            })}
+                                    </Stack>}
                             </Stack>
                         </Stack>
                 }
