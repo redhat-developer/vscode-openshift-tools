bb4db96364d1ce60d1b54c7ed132e34ccf03dcbc
Victor Rubezhny <vrubezhny@redhat.com>
2025-07-17 23:00:14 +0200

    [build] Fix release GH workflow
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- build/update-readme.cjs
+++ build/update-readme.cjs
@@ -3,12 +3,12 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { readFileSync, writeFileSync } from 'fs-extra';
+const fs = require('fs-extra');
 
-const readme = readFileSync('./README.md');
+const readme = fs.readFileSync('./README.md');
 
 const lines = `${readme}`.split('\n');
 
 const index = lines.findIndex((line) => line.includes('## Overview'));
 lines.splice(0, index + 1);
-writeFileSync('./README.md', lines.join('\n'));
+fs.writeFileSync('./README.md', lines.join('\n'));
