8989e65991f3ea7936940693143475c0f8d02e76
msivasubramaniaan <msivasub@redhat.com>
2026-07-29 17:23:30 +0530

    added UI and functionality for adding binding service

---
+++ src/webview/add-service-binding/app/index.tsx
@@ -0,0 +1,16 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import { createRoot } from 'react-dom/client';
+import * as React from 'react';
+import { AddServiceBindingForm } from './addServiceBindingForm';
+import { WebviewErrorBoundary } from '../../common/webviewErrorBoundary'
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <WebviewErrorBoundary webviewName='addServiceBinding'>
+    <AddServiceBindingForm />
+  </WebviewErrorBoundary>,
+);
